fbae29b0ff0dafe31c29d60ff725648838e386d2
Frank Li <frankli1@fb.com>
2017-07-10 16:38:35 -0700

    Run Prettier

--- examples/siteConfig.js
+++ examples/siteConfig.js
@@ -64,16 +64,17 @@ const siteConfig = {
       "rgba(46, 133, 85, 0.03)" /* primaryColor in rgba form, with 0.03 alpha */
   },
   tagline: "My Tagline",
-  recruitingLink: "https://crowdin.com/project/test-site" /* translation site "help translate" link */,
+  recruitingLink:
+    "https://crowdin.com/project/test-site" /* translation site "help translate" link */,
   /* remove this section to disable search bar */
   algolia: {
-    apiKey: "0f9f28b9ab9efae89810921a351753b5", /* use your search-only api key */
+    apiKey:
+      "0f9f28b9ab9efae89810921a351753b5" /* use your search-only api key */,
     indexName: "github"
-  },
+  }
   /* remove this to disable google analytics tracking */
   /* gaTrackingId: "" */
 };
-
 
 /* DO NOT EDIT BELOW THIS LINE */
 
