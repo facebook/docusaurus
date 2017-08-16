f60c62c538e867a8297bbfd505af9193990970b5
Frank Li <frankli1@fb.com>
2017-08-16 15:26:00 -0700

    Fix missing comma in siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -35,7 +35,7 @@ const siteConfig = {
   url: "https://docusaurus.io",
   baseUrl: "/",
   projectName: "docusaurus",
-  cname: "docusaurus.io"
+  cname: "docusaurus.io",
   users,
   editUrl:
     "https://github.com/facebookexperimental/docusaurus/edit/master/docs/",
