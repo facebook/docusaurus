f0ff3f408f99dcf2eb8f772a6d7d09ba9d4e1f76
Joel Marcey <joelm@fb.com>
2017-12-13 08:44:46 -0800

    Add testimomnial quotes

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -219,21 +219,21 @@ class Index extends React.Component {
                 contents={[
                   {
                     content:
-                      "TESTIMONIAL 1",
+                      "<i>I’ve helped open source many projects at Facebook and every one needed a website. They all had very similar constraints: the documentation should be written in markdown and be deployed via GitHub pages. None of the existing solutions were great, so I hacked my own and then forked it whenever we needed a new website. I’m so glad that Docusaurus now exists so that I don’t have to spend a week each time spinning up a new one.</i>",
                     image: `${siteConfig.baseUrl}img/christopher-chedeau.jpg`,
                     imageAlign: "top",
                     title: 'Christopher "vjeux" Chedeau <br/><font size="2">Lead Prettier Developer</font>'
                   },
                   {
                     content:
-                      "TESTIMONIAL 2",
+                      "<i>Open source contributions to the React Native docs have skyrocketed after our move to Docusaurus. The docs are now hosted on a small repo in plain markdown, with none of the clutter that a typical static site generator would require. Thanks Slash!</i>",
                     image: `${siteConfig.baseUrl}img/hector-ramos.png`,
                     imageAlign: "top",
                     title: 'Hector Ramos <br/><font size="2">Lead React Native Advocate</font>'
                   },
                   {
                     content:
-                      "TESTIMONIAL 3",
+                      "<i>Docusaurus has been a great choice for the ReasonML family of projects. It makes our documentation consistent, i18n-friendly, easy to maintain, and friendly for new contributors.</i>",
                     image: `${siteConfig.baseUrl}img/ricky-vetter.jpg`,
                     imageAlign: "top",
                     title: 'Ricky Vetter <br/><font size="2">Lead Reason-React Developer</font>'
