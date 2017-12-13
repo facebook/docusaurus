9f360a5863e5f7582ab014775cd97f60fb1e8487
Joel Marcey <joelm@fb.com>
2017-12-13 10:52:11 -0800

    Add About Slash page
    It will show up in the header as "About/" :)

---
+++ website/pages/en/about-slash.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require("react");
+
+const CompLibrary = require("../../core/CompLibrary.js");
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+const siteConfig = require(process.cwd() + "/siteConfig.js");
+
+class AboutSlash extends React.Component {
+  render() {
+    return (
+      <div className="pageContainer">
+        <Container className="mainContainer documentContainer postContainer">
+          <h1>About Slash</h1>
+          <img src={`${siteConfig.baseUrl}img/docusaurus.svg`} />
+          <p>
+            Slash is the official mascot of Docusaurus. You will find different variations of her throughout the <a href="https://docusaurus.io">website</a>, whether she is moving fast on her scooter or writing documentation at her standing desk. At Facebook, we have actual Slash plushies -- and you never know, you may see these plushies at various events and conferences in the future.
+          </p>
+        </Container>
+        <Container className="mainContainer">
+          <h2>Birth of Slash</h2>
+          <img src={`${siteConfig.baseUrl}img/slash-birth.png`} />
+          <p>
+            The team sat in a conference room trying to come up with a name for the project. Dinosaurs became a theme, finally landing on Docusaurus, combining documentation with those many dinosaurus that end in "saurus". Of course, we needed a logo for our new project. Eric sat down and designed a logo that was quite beyond the norm of our normal open source project logos, but yet was just so awesome, we had to use it. We needed a name for this cute Docusaur. "Marky" for markdown? "Docky" for documentation? No, "Slash" for the normal way someone starts code documentation in many programming languages <code>//</code> or <code>/*</code> or <code>///</code>. And Slash was born.
+          </p>
+        </Container>
+        <br/>
+      </div>
+    );
+  }
+}
+
+AboutSlash.defaultProps = {
+  language: "en"
+};
+
+module.exports = AboutSlash;
