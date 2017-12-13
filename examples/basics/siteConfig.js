fd835116ee38169c1480fbfb9b7509711841aa56
Eric Nakagawa <ericnakagawa@gmail.com>
2017-12-13 15:59:43 -0800

    Add dummy siteConfig.js for deploying, fixed indendation on publishing file (#285)

--- examples/basics/siteConfig.js
+++ examples/basics/siteConfig.js
@@ -42,6 +42,8 @@ const siteConfig = {
     'Copyright © ' +
     new Date().getFullYear() +
     ' Your Name or Your Company Name',
+  // organizationName: 'deltice', // or set an env variable ORGANIZATION_NAME
+  // projectName: 'test-site', // or set an env variable PROJECT_NAME
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
     theme: 'default',
