1db69285e559c13cbc205359249aa2d2bbd5a5c4
Héctor Ramos <165856+hramos@users.noreply.github.com>
2017-12-18 11:47:57 -0800

    Split showcase into FB and non-FB (#315)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,104 +8,112 @@
 /* List of projects/orgs using your project for the users page */
 const users = [
   {
-    caption: "Prettier",
-    image: "/img/prettier.png",
-    infoLink: "https://www.prettier.io",
-    pinned: true
+    caption: 'Prettier',
+    image: '/img/prettier.png',
+    infoLink: 'https://www.prettier.io',
+    fbOpenSource: false,
+    pinned: true,
   },
   {
-    caption: "FastText",
-    image: "/img/fasttext.png",
-    infoLink: "https://fasttext.cc",
-    pinned: true
+    caption: 'FastText',
+    image: '/img/fasttext.png',
+    infoLink: 'https://fasttext.cc',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "Jest",
-    image: "/img/jest.png",
-    infoLink: "https://facebook.github.io/jest/",
-    pinned: true
+    caption: 'Jest',
+    image: '/img/jest.png',
+    infoLink: 'https://facebook.github.io/jest/',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "Reason React",
-    image: "/img/reason-react.svg",
-    infoLink: "https://reasonml.github.io/reason-react/",
-    pinned: true
+    caption: 'Reason React',
+    image: '/img/reason-react.svg',
+    infoLink: 'https://reasonml.github.io/reason-react/',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "MakeItOpen",
-    image: "/img/makeitopen.png",
-    infoLink: "http://makeitopen.com/",
-    pinned: true
+    caption: 'MakeItOpen',
+    image: '/img/makeitopen.png',
+    infoLink: 'http://makeitopen.com/',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "React Native",
-    image: "/img/react-native.svg",
-    infoLink: "https://facebook.github.io/react-native",
-    pinned: true
+    caption: 'React Native',
+    image: '/img/react-native.svg',
+    infoLink: 'https://facebook.github.io/react-native',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "Relay",
-    image: "/img/relay.svg",
-    infoLink: "https://facebook.github.io/relay/",
-    pinned: true
+    caption: 'Relay',
+    image: '/img/relay.svg',
+    infoLink: 'https://facebook.github.io/relay/',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "Bucklescript",
-    image: "/img/bucklescript.svg",
-    infoLink: "https://bucklescript.github.io/",
-    pinned: true
+    caption: 'Bucklescript',
+    image: '/img/bucklescript.svg',
+    infoLink: 'https://bucklescript.github.io/',
+    fbOpenSource: true,
+    pinned: true,
   },
   {
-    caption: "Docusaurus",
-    image: "/img/docusaurus.svg",
-    infoLink: "https://www.docusaurus.io",
-    pinned: true
-  }
+    caption: 'Docusaurus',
+    image: '/img/docusaurus.svg',
+    infoLink: 'https://www.docusaurus.io',
+    fbOpenSource: true,
+    pinned: true,
+  },
 ];
 
 const siteConfig = {
-  title: "Docusaurus",
-  tagline: "Easy to Maintain Open Source Documentation Websites",
-  url: "https://docusaurus.io",
-  baseUrl: "/",
-  organizationName: "facebook",
-  projectName: "Docusaurus",
-  cname: "docusaurus.io",
+  title: 'Docusaurus',
+  tagline: 'Easy to Maintain Open Source Documentation Websites',
+  url: 'https://docusaurus.io',
+  baseUrl: '/',
+  organizationName: 'facebook',
+  projectName: 'Docusaurus',
+  cname: 'docusaurus.io',
   noIndex: false,
   users,
-  editUrl:
-    "https://github.com/facebook/docusaurus/edit/master/docs/",
+  editUrl: 'https://github.com/facebook/docusaurus/edit/master/docs/',
   headerLinks: [
-    { doc: "installation", label: "Docs" },
-    { page: "help", label: "Help" },
-    { page: "about-slash", label: "About /"},
-    { blog: true, label: "Blog" },
+    {doc: 'installation', label: 'Docs'},
+    {page: 'help', label: 'Help'},
+    {page: 'about-slash', label: 'About /'},
+    {blog: true, label: 'Blog'},
     {
-      href: "https://github.com/facebook/docusaurus",
-      label: "GitHub"
-    }
+      href: 'https://github.com/facebook/docusaurus',
+      label: 'GitHub',
+    },
   ],
-  headerIcon: "img/docusaurus.svg",
-  footerIcon: "img/docusaurus_monochrome.svg",
-  favicon: "img/docusaurus.ico",
+  headerIcon: 'img/docusaurus.svg',
+  footerIcon: 'img/docusaurus_monochrome.svg',
+  favicon: 'img/docusaurus.ico',
   algolia: {
-    apiKey: "3eb9507824b8be89e7a199ecaa1a9d2c",
-    indexName: "docusaurus"
+    apiKey: '3eb9507824b8be89e7a199ecaa1a9d2c',
+    indexName: 'docusaurus',
   },
   colors: {
-    primaryColor: "#2E8555",
-    secondaryColor: "#205C3B"
+    primaryColor: '#2E8555',
+    secondaryColor: '#205C3B',
   },
-  translationRecruitingLink: "https://crowdin.com/project/docusaurus",
-  copyright: "Copyright © " + new Date().getFullYear() + " Facebook Inc.",
+  translationRecruitingLink: 'https://crowdin.com/project/docusaurus',
+  copyright: 'Copyright © ' + new Date().getFullYear() + ' Facebook Inc.',
   highlight: {
-    theme: "solarized-dark"
+    theme: 'solarized-dark',
   },
-  scripts: ["https://buttons.github.io/buttons.js"],
-  gaTrackingId: "UA-44373548-31",
-  facebookAppId: "1615782811974223",
-  twitter: "true",
-  ogImage: "img/docusaurus.png",
+  scripts: ['https://buttons.github.io/buttons.js'],
+  gaTrackingId: 'UA-44373548-31',
+  facebookAppId: '1615782811974223',
+  twitter: 'true',
+  ogImage: 'img/docusaurus.png',
 };
 
 module.exports = siteConfig;
