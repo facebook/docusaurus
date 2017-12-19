1cdffe2e4179680d721060bdcc7fc19fb50e3f61
Richard Zhang <richardzcode@gmail.com>
2017-12-18 19:38:22 -0800

    prettier recommends

--- examples/basics/core/Footer.js
+++ examples/basics/core/Footer.js
@@ -10,12 +10,12 @@ const React = require('react');
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + 'docs/' + (language? language + '/' : '') + doc;
+    return baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language? language + '/' : '') + doc;
+    return baseUrl + (language ? language + '/' : '') + doc;
   }
 
   render() {
