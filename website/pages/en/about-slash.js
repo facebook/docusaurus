de381e8f03a1ecf30a55152077ad0dbc5641a53b
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-12-27 07:25:43 -0800

    Fix<slash>: Fix typo (#355)

--- website/pages/en/about-slash.js
+++ website/pages/en/about-slash.js
@@ -28,7 +28,7 @@ class AboutSlash extends React.Component {
           <h2>Birth of Slash</h2>
           <img src={`${siteConfig.baseUrl}img/slash-birth.png`} />
           <p>
-            The team sat in a conference room trying to come up with a name for the project. Dinosaurs became a theme, finally landing on Docusaurus, combining documentation with those many dinosaurus that end in "saurus". Of course, we needed a logo for our new project. Eric sat down and designed a logo that was quite beyond the norm of our normal open source project logos, but yet was just so awesome, we had to use it. We needed a name for this cute Docusaur. "Marky" for markdown? "Docky" for documentation? No, "Slash" for the normal way someone starts code documentation in many programming languages <code>//</code> or <code>/*</code> or <code>///</code>. And Slash was born.
+            The team sat in a conference room trying to come up with a name for the project. Dinosaurs became a theme, finally landing on Docusaurus, combining documentation with those many dinosaurs that end in "saurus". Of course, we needed a logo for our new project. Eric sat down and designed a logo that was quite beyond the norm of our normal open source project logos, but yet was just so awesome, we had to use it. We needed a name for this cute Docusaur. "Marky" for markdown? "Docky" for documentation? No, "Slash" for the normal way someone starts code documentation in many programming languages <code>//</code> or <code>/*</code> or <code>///</code>. And Slash was born.
           </p>
         </Container>
         <br/>
