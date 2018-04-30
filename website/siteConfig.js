80f4686c16db73d76d95f850af7b293b3c18ceca
Yangshun Tay <tay.yang.shun@gmail.com>
2018-04-29 23:30:32 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/* List of projects/orgs using your project for the users page */
+// List of projects/orgs using your project for the users page.
 const users = [
   {
     caption: "Almin",
