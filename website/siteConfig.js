304c99010cb605651567bbe88ee94e1714d7fef1
Erin Teo Yi Ling <erinteo@fb.com>
2018-05-05 16:41:26 -0700

    Remove defaultVersionShown from siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -201,7 +201,6 @@ const siteConfig = {
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
   onPageNav: 'separate',
-  defaultVersionShown: '1.0.12',
 };
 
 module.exports = siteConfig;
