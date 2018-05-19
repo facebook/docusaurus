c9518b5663366b4e6fe60898c35cd0103c3acae7
Yangshun Tay <tay.yang.shun@gmail.com>
2018-05-19 09:21:12 -0700

    Tweak

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -56,7 +56,7 @@ const siteConfig = {
   ],
   gaTrackingId: 'UA-44373548-31',
   facebookAppId: '1615782811974223',
-  facebookComment: true,
+  facebookComments: true,
   twitter: 'true',
   twitterUsername: 'docusaurus',
   ogImage: 'img/docusaurus.png',
