1dd1bdc811a00f05594292e240ef6e2ca3afb9e4
Agastya Darma Lakasna <agastyadrm@naver.com>
2018-05-24 21:50:43 +0700

    Modifying build-file test case to read hashed CSS

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -51,9 +51,7 @@ const siteConfig = {
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-blocks-buttons.js',
   ],
-  stylesheets: [
-    '/css/code-blocks-buttons.css',
-  ],
+  stylesheets: ['/css/code-blocks-buttons.css'],
   gaTrackingId: 'UA-44373548-31',
   facebookAppId: '199138890728411',
   facebookComments: true,
