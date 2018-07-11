ed4f2c16946f952402a614855abc821f415e8552
Yangshun Tay <tay.yang.shun@gmail.com>
2018-07-10 22:11:02 -0700

    ESLintify Part 3

--- website/static/js/code-blocks-buttons.js
+++ website/static/js/code-blocks-buttons.js
@@ -1,19 +1,23 @@
-/* global ClipboardJS */
-
-window.addEventListener('load', () => {
+// Turn off ESLint for this file because it's sent down to users as-is.
+/* eslint-disable */
+window.addEventListener('load', function() {
   function button(label, ariaLabel, icon, className) {
     const btn = document.createElement('button');
     btn.classList.add('btnIcon', className);
     btn.setAttribute('type', 'button');
     btn.setAttribute('aria-label', ariaLabel);
     btn.innerHTML =
-      `<div class="btnIcon__body">${icon}<strong class="btnIcon__label">${label}</strong>` +
-      `</div>`;
+      '<div class="btnIcon__body">' +
+      icon +
+      '<strong class="btnIcon__label">' +
+      label +
+      '</strong>' +
+      '</div>';
     return btn;
   }
 
   function addButtons(codeBlockSelector, btn) {
-    document.querySelectorAll(codeBlockSelector).forEach(code => {
+    document.querySelectorAll(codeBlockSelector).forEach(function(code) {
       code.parentNode.appendChild(btn.cloneNode(true));
     });
   }
@@ -27,18 +31,16 @@ window.addEventListener('load', () => {
   );
 
   const clipboard = new ClipboardJS('.btnClipboard', {
-    // Not gonna use the shorthand as this file is sent down to browsers without transpiling.
-    /* eslint-disable object-shorthand */
     target: function(trigger) {
       return trigger.parentNode.querySelector('code');
     },
   });
 
-  clipboard.on('success', event => {
+  clipboard.on('success', function(event) {
     event.clearSelection();
     const textEl = event.trigger.querySelector('.btnIcon__label');
     textEl.textContent = 'Copied';
-    setTimeout(() => {
+    setTimeout(function() {
       textEl.textContent = 'Copy';
     }, 2000);
   });
