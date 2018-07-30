e2e81805081612dbea376963cb96b71f735365f6
endiliey <endiliey@gmail.com>
2018-07-30 18:58:08 +0800

    chore: add prerelease script

---
+++ admin/prerelease.md
@@ -0,0 +1,15 @@
+# Prereleasing New Version of Docusaurus
+
+[![asciicast](https://asciinema.org/a/n8rm53yfNURlIreGJkdSTgfAi.png)](https://asciinema.org/a/n8rm53yfNURlIreGJkdSTgfAi)
+
+# Steps
+
+1. Ensure that `origin` remote is your Docusaurus fork. Example
+
+```bash
+$ git remote -v
+origin	https://github.com/endiliey/Docusaurus.git (fetch)
+origin	https://github.com/endiliey/Docusaurus.git (push)
+```
+
+1. Run `bash scripts/prerelease.sh` to create a pull request for new version release
---
+++ scripts/prerelease.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Select a option to release (input a serial number)："
+echo
+
+select VERSION in patch minor major "Specific Version"
+  do
+    echo
+    if [[ $REPLY =~ ^[1-4]$ ]]; then
+      if [[ $REPLY == 4 ]]; then
+        read -p "Enter a specific version: " -r VERSION
+        echo
+        if [[ -z $REPLY ]]; then
+          VERSION=$REPLY
+        fi
+      fi
+
+      read -p "Release $VERSION - are you sure? (y/n) " -n 1 -r
+      echo
+
+      if [[ $REPLY =~ ^[Yy]$ || -z $REPLY ]]; then
+        # bump version
+        yarn version --new-version $VERSION --no-git-tag-version
+        NEW_VERSION=$(node -p "require('./package.json').version")
+
+        # create new branch
+        git checkout -b $NEW_VERSION master
+
+        # cut docusaurus docs version
+        cd website && yarn run version $NEW_VERSION
+        
+        # Create PR
+        echo "Creating Pull Request for Release ${NEW_VERSION}"
+        git add .
+        git commit -m "v$NEW_VERSION"
+        git push origin $NEW_VERSION
+        echo "✅ Release PR created"
+      else
+        echo Cancelled
+      fi
+      break
+    else
+      echo Invalid \"${REPLY}\"
+      echo "To continue, please input a serial number(1-4) of an option."
+      echo
+    fi
+  done
