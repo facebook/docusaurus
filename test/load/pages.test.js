2141e6ea90594b7602f0fbc73d1afe0f50459584
endiliey <endiliey@gmail.com>
2018-09-07 13:53:14 +0800

    test: loadPages

--- test/load/pages.test.js
+++ test/load/pages.test.js
@@ -3,7 +3,11 @@ import path from 'path';
 
 describe('loadPages', () => {
   test('valid pages', async () => {
-    const pagesDir = path.join(__dirname, '__fixtures__', 'simple-pages');
+    const pagesDir = path.join(
+      __dirname,
+      '__fixtures__',
+      'simple-pages'
+    );
     const pagesData = await loadPages(pagesDir);
     expect(pagesData).toMatchSnapshot();
     expect(pagesData).not.toBeNull();
