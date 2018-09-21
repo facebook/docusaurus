3b7e6583159082a5ec22f57176923ea40bf393b8
endiliey <endiliey@gmail.com>
2018-09-22 00:32:17 +0800

    refactor: rename 'prerender' to 'preload' for correctness

--- v2/lib/core/preload.js
+++ v2/lib/core/preload.js
@@ -4,7 +4,7 @@ import {matchRoutes} from 'react-router-config';
  * This helps us to make sure all the async component for that particular route
  * is loaded before rendering. This is to avoid loading screens on first page load
  */
-export default function prerender(routeConfig, providedLocation) {
+export default function preload(routeConfig, providedLocation) {
   const matches = matchRoutes(routeConfig, providedLocation);
   return Promise.all(
     matches.map(match => {
