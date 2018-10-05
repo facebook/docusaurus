efc38b094ddebc22a03ab0b77fdc3094eebab461
Yangshun Tay <tay.yang.shun@gmail.com>
2018-10-04 23:58:50 -0700

    style(v2): add static footer component

--- v2/lib/theme/Footer/index.js
+++ v2/lib/theme/Footer/index.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import styles from './styles.css';
 
-function Footer(props) {
-  const {pagesMetadatas, docsMetadatas, location} = props;
-  const docsFlatMetadatas = Object.values(docsMetadatas);
-  const routeLinks = [...pagesMetadatas, ...docsFlatMetadatas].map(
-    data =>
-      data.permalink !== location.pathname && (
-        <li key={data.permalink}>
-          <Link to={data.permalink}>{data.permalink}</Link>
-        </li>
-      ),
-  );
-
+function Footer() {
   return (
-    <div className={styles.footer}>
-      <ul className={styles.routeLinks}>{routeLinks}</ul>
-    </div>
+    <footer className={styles.footer}>
+      <section className={styles.footerRow}>
+        <div className={styles.footerColumn}>
+          <h3 className={styles.footerColumnTitle}>Docs</h3>
+          <ul className={styles.footerList}>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Getting Started
+              </a>
+            </li>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Versioning
+              </a>
+            </li>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Localization
+              </a>
+            </li>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Adding Search
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.footerColumn}>
+          <h3 className={styles.footerColumnTitle}>Community</h3>
+          <ul className={styles.footerList}>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                User Showcase
+              </a>
+            </li>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Stack Overflow
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.footerColumn}>
+          <h3 className={styles.footerColumnTitle}>Social</h3>
+          <ul className={styles.footerList}>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                GitHub
+              </a>
+            </li>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Facebook
+              </a>
+            </li>
+            <li className={styles.footerListItem}>
+              <a className={styles.footerLink} href="/">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className={styles.copyright}>
+        <span>Copyright © {new Date().getFullYear()} Facebook Inc.</span>
+      </section>
+    </footer>
   );
 }
 
