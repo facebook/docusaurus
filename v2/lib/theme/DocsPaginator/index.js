7160aa1c81688cb3b196daf1a59c5d424c28fe35
Yangshun Tay <tay.yang.shun@gmail.com>
2018-10-13 23:20:31 -0700

    style(v2): skin paginator component

--- v2/lib/theme/DocsPaginator/index.js
+++ v2/lib/theme/DocsPaginator/index.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import styles from './styles.css';
+
 export default class DocsPaginator extends React.Component {
   render() {
     const {docsMetadatas, metadata} = this.props;
     return (
-      <div>
-        {metadata.previous &&
-          docsMetadatas[metadata.previous] && (
-            <Link to={docsMetadatas[metadata.previous].permalink}>
-              <span>← {metadata.previous_title}</span>
-            </Link>
-          )}
-        {metadata.next &&
-          docsMetadatas[metadata.next] && (
-            <Link to={docsMetadatas[metadata.next].permalink}>
-              <span>{metadata.next_title} →</span>
-            </Link>
-          )}
+      <div className={styles.paginatorContainer}>
+        <div>
+          {metadata.previous &&
+            docsMetadatas[metadata.previous] && (
+              <Link
+                className={styles.paginatorLink}
+                to={docsMetadatas[metadata.previous].permalink}>
+                <svg className={styles.arrow} viewBox="0 0 24 24">
+                  <g>
+                    <line x1="19" y1="12" x2="5" y2="12" />
+                    <polyline points="12 19 5 12 12 5" />
+                  </g>
+                </svg>{' '}
+                <span className={styles.label}>{metadata.previous_title}</span>
+              </Link>
+            )}
+        </div>
+        <div className={styles.paginatorRightContainer}>
+          {metadata.next &&
+            docsMetadatas[metadata.next] && (
+              <Link
+                className={styles.paginatorLink}
+                to={docsMetadatas[metadata.next].permalink}>
+                <span className={styles.label}>{metadata.next_title}</span>{' '}
+                <svg className={styles.arrow} viewBox="0 0 24 24">
+                  <g>
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                    <polyline points="12 5 19 12 12 19" />
+                  </g>
+                </svg>
+              </Link>
+            )}
+        </div>
       </div>
     );
   }
