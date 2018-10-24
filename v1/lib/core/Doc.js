dc887576cdead8c47735a017f2a912f631fdb30f
Fienny Angelina <fifi.angelina@yahoo.com>
2018-10-24 22:45:34 +0800

    Try to find a way to recognize the codeblock tabs, its title, and where it ends - I try using DOCUSAURUS_CODE_TABS to mark the start - Use TAB_TITLE to mark the title of the tab - END_TAB to mark the end of that tab - END_DOCUSAURUS_CODE_TABS to mark the end of the whole code blocks then parse using regex and render accordingly

--- v1/lib/core/Doc.js
+++ v1/lib/core/Doc.js
@@ -7,6 +7,7 @@
 
 const React = require('react');
 const MarkdownBlock = require('./MarkdownBlock.js');
+const CodeTabsMarkdownBlock = require('./CodeTabsMarkdownBlock.js');
 
 const translate = require('../server/translate.js').translate;
 
@@ -17,8 +18,46 @@ const translateThisDoc = translate(
   'Translate this Doc|recruitment message asking to translate the docs',
 );
 
+const splitTabsToTitleAndContent = content => {
+  const tabs = content.match(/TAB_TITLE(.*?)END_TAB/gms);
+  const titleAndContentRegex = /^TAB_TITLE=([^\n]+)(.*?)END_TAB$/s;
+  if (tabs && tabs.length) {
+    return tabs.map(tab => {
+      const temp = tab.match(titleAndContentRegex);
+      return {title: temp[1], content: temp[2]};
+    });
+  }
+  return null;
+};
 // inner doc component for article itself
 class Doc extends React.Component {
+  renderContent() {
+    const {content} = this.props;
+    let inCodeTabs = false;
+    const contents = content.split(
+      /(DOCUSAURUS_CODE_TABS\n)(.*?)(\nEND_DOCUSAURUS_CODE_TABS)/gms,
+    );
+    const renderResult = contents.map(c => {
+      if (c === 'DOCUSAURUS_CODE_TABS\n') {
+        inCodeTabs = true;
+        return null;
+      }
+      if (c === '\nEND_DOCUSAURUS_CODE_TABS') {
+        inCodeTabs = false;
+        return null;
+      }
+      if (inCodeTabs) {
+        return (
+          <CodeTabsMarkdownBlock>
+            {splitTabsToTitleAndContent(c)}
+          </CodeTabsMarkdownBlock>
+        );
+      }
+      return <MarkdownBlock>{c}</MarkdownBlock>;
+    });
+    return renderResult;
+  }
+
   render() {
     let docSource = this.props.source;
 
@@ -59,6 +98,8 @@ class Doc extends React.Component {
       );
     }
 
+    // const a = this.props.content.split(/(DOCUSAURUS_CODE_TABS)(.*?)(END_DOCUSAURUS_CODE_TABS)/gms)
+    // console.log(a.length, a);
     return (
       <div className="post">
         <header className="postHeader">
@@ -67,9 +108,7 @@ class Doc extends React.Component {
             <h1 className="postHeaderTitle">{this.props.title}</h1>
           )}
         </header>
-        <article>
-          <MarkdownBlock>{this.props.content}</MarkdownBlock>
-        </article>
+        <article>{this.renderContent()}</article>
       </div>
     );
   }
