13b23edd12bae9a9c67ef1c226167909f36c2c76
Yangshun Tay <tay.yang.shun@gmail.com>
2018-10-25 17:30:38 -0700

    chore(v1): add Facebook copyright headers (#1068)

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+##
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 DOCS_VERSION_COMMAND="run version"
 
 echo "Select an option for release："
