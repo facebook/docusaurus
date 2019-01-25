b5dd31adf15c43668f2cd964bbe8dea9572fc099
Website Deployment Script <docusaurus-bot@users.noreply.github.com>
2019-01-25 22:23:24 +0000

    Deploy website
    Deploy website version based on cbcbf5a8c2ffd3d54c112e3f1957368d75d8f3cf

--- js/codetabs.js
+++ js/codetabs.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // Turn off ESLint for this file because it's sent down to users as-is.
 /* eslint-disable */
 window.addEventListener('load', function() {
