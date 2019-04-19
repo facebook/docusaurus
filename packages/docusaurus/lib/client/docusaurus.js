dfa3258e2d298638329753126b8c4f30cd66dde3
endiliey <endiliey@gmail.com>
2019-04-19 19:30:35 +0800

    prefetch feedback page on home page

--- packages/docusaurus/lib/client/docusaurus.js
+++ packages/docusaurus/lib/client/docusaurus.js
@@ -4,12 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import {matchRoutes} from 'react-router-config';
 import routesChunkNames from '@generated/routesChunkNames';
+import routes from '@generated/routes'; // eslint-disable-line
 import prefetchHelper from './prefetch';
 
 const docusaurus = {
   prefetch: routePath => {
-    console.log(routePath);
     // Skip prefetching if we know user is on slow or constrained connection
     if (`connection` in navigator) {
       if ((navigator.connection.effectiveType || ``).includes(`2g`)) {
@@ -21,7 +22,6 @@ const docusaurus = {
     }
 
     // Find all chunk names needed for that particular route
-    // TODO: make it more efficient and declarative
     const chunkNamesNeeded = [];
     function dfs(target) {
       if (!target) {
@@ -42,17 +42,15 @@ const docusaurus = {
       }
       chunkNamesNeeded.push(target);
     }
-    dfs(routesChunkNames[routePath]);
+    const matches = matchRoutes(routes, routePath);
+    matches.map(match => dfs(routesChunkNames[match.route.path]));
 
-    // Get all important webpack chunk assets we need to prefetch
+    // Prefetch all the chunks assets file needed
     const chunkAssetsNeeded = chunkNamesNeeded.reduce((arr, chunkName) => {
       const chunkAssets = window.__chunkMapping[chunkName];
       return arr.concat(chunkAssets);
     }, []);
-
-    // Prefetch all the chunks assets file needed
     chunkAssetsNeeded.map(prefetchHelper);
-
     return true;
   },
 };
