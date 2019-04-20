b99ede440b0e27d596941c7d483c24682b461093
Endilie Yacop Sucipto <endiliey@gmail.com>
2019-04-21 00:28:13 +0700

    feat(v2): prefetch links on viewport with intersection observer (#1375)
    * feat(v2): prefetch links on viewport with intersection observer
    
    * nits
    
    * remove console log
    
    * better useEffect
    
    * disconnect IO when unmount
    
    * comment

--- packages/docusaurus/lib/client/exports/Link.js
+++ packages/docusaurus/lib/client/exports/Link.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import Perimeter from 'react-perimeter';
 import {NavLink} from 'react-router-dom';
 
@@ -15,6 +15,51 @@ function Link(props) {
   const {to, href, preloadProximity = 20} = props;
   const targetLink = to || href;
   const isInternal = internalRegex.test(targetLink);
+
+  const IOSupported =
+    typeof window !== 'undefined' && 'IntersectionObserver' in window;
+
+  let io;
+  const handleIntersection = (el, cb) => {
+    io = new window.IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (el === entry.target) {
+          // If element is in viewport, stop listening/observing & run callback.
+          // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+          if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            io.unobserve(el);
+            io.disconnect();
+            cb();
+          }
+        }
+      });
+    });
+    // Add element to the observer
+    io.observe(el);
+  };
+
+  const handleRef = ref => {
+    if (IOSupported && ref && isInternal) {
+      // If IO supported and element reference found, setup Observer functionality
+      handleIntersection(ref, () => {
+        window.__docusaurus.prefetch(targetLink);
+      });
+    }
+  };
+
+  useEffect(() => {
+    // If IO is not supported. We prefetch by default (only once)
+    if (!IOSupported && isInternal) {
+      window.__docusaurus.prefetch(targetLink);
+    }
+    // when unmount, stops intersection observer from watching
+    return () => {
+      if (IOSupported && io) {
+        io.disconnect();
+      }
+    };
+  }, [targetLink, IOSupported, isInternal]);
+
   return !targetLink || !isInternal ? (
     // eslint-disable-next-line jsx-a11y/anchor-has-content
     <a {...props} href={targetLink} />
@@ -23,7 +68,7 @@ function Link(props) {
       padding={preloadProximity}
       onBreach={() => window.__docusaurus.preload(targetLink)}
       once>
-      <NavLink {...props} to={targetLink} />
+      <NavLink {...props} innerRef={handleRef} to={targetLink} />
     </Perimeter>
   );
 }
