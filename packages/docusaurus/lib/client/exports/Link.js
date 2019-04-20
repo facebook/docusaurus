b31b2f388c394c6b3445833edc354135ab1a2af6
endiliey <endiliey@gmail.com>
2019-04-21 01:17:58 +0800

    disconnect IO when unmount

--- packages/docusaurus/lib/client/exports/Link.js
+++ packages/docusaurus/lib/client/exports/Link.js
@@ -11,26 +11,6 @@ import {NavLink} from 'react-router-dom';
 
 const internalRegex = /^\/(?!\/)/;
 
-// Set up IntersectionObserver
-const handleIntersection = (el, cb) => {
-  const io = new window.IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (el === entry.target) {
-        // Check if element is within viewport, remove listener, destroy observer, and run link callback.
-        // MSEdge doesn't currently support isIntersecting, so also test for an intersectionRatio > 0
-        // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-        if (entry.isIntersecting || entry.intersectionRatio > 0) {
-          io.unobserve(el);
-          io.disconnect();
-          cb();
-        }
-      }
-    });
-  });
-  // Add element to the observer
-  io.observe(el);
-};
-
 function Link(props) {
   const {to, href, preloadProximity = 20} = props;
   const targetLink = to || href;
@@ -39,13 +19,31 @@ function Link(props) {
   const IOSupported =
     typeof window !== 'undefined' && 'IntersectionObserver' in window;
 
+  let io;
+  const handleIntersection = (el, cb) => {
+    io = new window.IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (el === entry.target) {
+          // Check if element is within viewport, remove listener, destroy observer, and run link callback.
+          // MSEdge doesn't currently support isIntersecting, so also test for an intersectionRatio > 0
+          // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+          if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            io.unobserve(el);
+            io.disconnect();
+            cb();
+          }
+        }
+      });
+    });
+    // Add element to the observer
+    io.observe(el);
+  };
+
   const handleRef = ref => {
-    if (IOSupported && ref) {
+    if (IOSupported && ref && isInternal) {
       // If IO supported and element reference found, setup Observer functionality
       handleIntersection(ref, () => {
-        if (isInternal) {
-          window.__docusaurus.prefetch(targetLink);
-        }
+        window.__docusaurus.prefetch(targetLink);
       });
     }
   };
@@ -55,7 +53,13 @@ function Link(props) {
     if (!IOSupported && isInternal) {
       window.__docusaurus.prefetch(targetLink);
     }
-  }, []);
+    // when unmount, stops intersection observer from watching
+    return () => {
+      if (IOSupported && io) {
+        io.disconnect();
+      }
+    };
+  }, [targetLink, IOSupported, isInternal]);
 
   return !targetLink || !isInternal ? (
     // eslint-disable-next-line jsx-a11y/anchor-has-content
