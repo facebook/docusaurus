9aa66ddac6173c7f3327fcccd944e780013fa4ed
Endilie Yacop Sucipto <endiliey@gmail.com>
2019-04-21 00:51:25 +0700

    chore(v2): temporary hack to persist infima CSS (#1376)

--- packages/docusaurus/lib/client/App.js
+++ packages/docusaurus/lib/client/App.js
@@ -9,6 +9,7 @@ import React, {useState} from 'react';
 import {renderRoutes} from 'react-router-config';
 import ReactListenerProvider from 'react-listener-provider';
 
+import Head from '@docusaurus/Head'; // eslint-disable-line
 import routes from '@generated/routes'; // eslint-disable-line
 import metadata from '@generated/metadata'; // eslint-disable-line
 import siteConfig from '@generated/docusaurus.config'; //eslint-disable-line
@@ -19,6 +20,22 @@ function App() {
   return (
     <DocusaurusContext.Provider
       value={{siteConfig, ...metadata, ...context, setContext}}>
+      {/* TODO: this link stylesheet to infima is temporary */}
+      <Head>
+        <link
+          crossOrigin="anonymous"
+          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+          preload
+          rel="stylesheet"
+        />
+        <link
+          href="https://infima-dev.netlify.com/css/default/default.min.css"
+          preload
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
       <ReactListenerProvider>{renderRoutes(routes)}</ReactListenerProvider>
     </DocusaurusContext.Provider>
   );
