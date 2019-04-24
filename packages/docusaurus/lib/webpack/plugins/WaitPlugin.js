3978d8c1f15a0471b7ec40a623a444ed0d660853
endiliey <endiliey@gmail.com>
2019-04-24 22:29:25 +0800

    refactor -> less code

--- packages/docusaurus/lib/webpack/plugins/WaitPlugin.js
+++ packages/docusaurus/lib/webpack/plugins/WaitPlugin.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-const fs = require('fs');
+
 const chokidar = require('chokidar');
 
 class WaitPlugin {
@@ -16,25 +16,15 @@ class WaitPlugin {
     // Before finishing the compilation step
     compiler.hooks.make.tapAsync('WaitPlugin', (compilation, callback) => {
       const {filepath} = this;
-
       const watcher = chokidar.watch(filepath, {
         awaitWriteFinish: true,
         disableGlobbing: true,
         persistent: false,
       });
 
-      const checkCondition = () => {
-        if (Array.isArray(filepath)) {
-          return filepath.every(file => fs.existsSync(file));
-        }
-        return fs.existsSync(filepath);
-      };
-
       watcher.on('add', () => {
-        if (checkCondition()) {
-          watcher.close();
-          callback();
-        }
+        watcher.close();
+        callback();
       });
     });
   }
