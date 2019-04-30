edcc2ab0c4f85cdcec43f7cd904fdcbc4acb2245
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-04-30 13:53:54 +0530

    Minor fix

--- packages/docusaurus/bin/docusaurus.js
+++ packages/docusaurus/bin/docusaurus.js
@@ -106,7 +106,7 @@ program
           System: ['OS', 'CPU'],
           Binaries: ['Node', 'Yarn', 'npm'],
           Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
-          npmPackages: ['/**/{@docusaurus/*/}'],
+          npmPackages: ['@docusaurus/*/'],
         },
         {
           showNotFound: true,
