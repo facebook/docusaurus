5030952a8fe6ac65c4576ec662e1bbecf7f997bd
James George <jamesgeorge998001@gmail.com>
2019-05-01 19:54:45 +0530

    feat(v2-cli): Added new environment info command (#1410)
    * Adds new info command
    
    Shows up essential debugging information about the environment
    
    * Minor fix
    
    * Remove npmGlobalPackages from envinfo
    
    * Fix up glob pattern
    
    * Minor fix
    
    * Update glob pattern
    
    * fix envinfo glob pattern and params
    
    * prettier
    
    * simplify glob

--- packages/docusaurus/bin/docusaurus.js
+++ packages/docusaurus/bin/docusaurus.js
@@ -8,6 +8,7 @@
  */
 
 const chalk = require('chalk');
+const envinfo = require('envinfo');
 const semver = require('semver');
 const path = require('path');
 const program = require('commander');
@@ -92,6 +93,22 @@ program
       hotOnly,
       cacheLoader,
     });
+  });
+
+program
+  .command('info')
+  .description('Shows debugging information about the local environment')
+  .action(() => {
+    console.log(chalk.bold('\nEnvironment Info:'));
+    envinfo
+      .run({
+        System: ['OS', 'CPU'],
+        Binaries: ['Node', 'Yarn', 'npm'],
+        Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+        npmPackages: '?(@)docusaurus{*,*/**}',
+        npmGlobalPackages: '?(@)docusaurus{*,*/**}',
+      })
+      .then(console.log);
   });
 
 program.arguments('<command>').action(cmd => {
