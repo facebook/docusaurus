b89c8974f2dbf1a42350de59e6151f2bb279be52
endiliey <endiliey@gmail.com>
2019-05-08 19:54:35 +0800

    chore(v2): add flow setup

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@
  */
 
 module.exports = {
-  presets: ['@babel/env', '@babel/react'],
+  presets: ['@babel/env', '@babel/react', '@babel/preset-flow'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
