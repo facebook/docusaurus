22bf23965bfb4b16c3fb4241242acd7a283e0636
Endi <endiliey@gmail.com>
2019-05-15 20:54:41 +0700

    fix(v2): fix wrong dependency problem (#1460)

--- packages/docusaurus/src/server/load/loadSetup.js
+++ packages/docusaurus/src/server/load/loadSetup.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import '@babel/polyfill';
 import path from 'path';
 import load from '../index';
 
--- packages/docusaurus/src/webpack/__tests__/utils.test.js
+++ packages/docusaurus/src/webpack/__tests__/utils.test.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import '@babel/polyfill';
 import {validate} from 'webpack';
 import path from 'path';
 
