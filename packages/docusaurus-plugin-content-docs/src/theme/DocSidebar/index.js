8a8a1de0355ad1c6a31e57647763a1c3b2ffc44a
Yangshun Tay <tay.yang.shun@gmail.com>
2019-06-05 10:31:11 -0700

    feat(v2): separate css modules from Infima

--- packages/docusaurus-plugin-content-docs/src/theme/DocSidebar/index.js
+++ packages/docusaurus-plugin-content-docs/src/theme/DocSidebar/index.js
@@ -79,47 +79,49 @@ function DocSidebar(props) {
   };
 
   return (
-    <div
-      className={classnames('menu', 'menu--responsive', styles.sidebar, {
-        'menu--show': showResponsiveSidebar,
-      })}>
-      <button
-        className="button button--secondary button--sm menu__button"
-        type="button"
-        onClick={() => {
-          setShowResponsiveSidebar(!showResponsiveSidebar);
-        }}>
-        {showResponsiveSidebar ? (
-          <span
-            className={classnames(
-              styles.sidebarMenuIcon,
-              styles.sidebarMenuCloseIcon,
-            )}>
-            &times;
-          </span>
-        ) : (
-          <svg
-            className={styles.sidebarMenuIcon}
-            xmlns="http://www.w3.org/2000/svg"
-            height={MOBILE_TOGGLE_SIZE}
-            width={MOBILE_TOGGLE_SIZE}
-            viewBox="0 0 32 32"
-            role="img"
-            focusable="false">
-            <title>Menu</title>
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              strokeWidth="2"
-              d="M4 7h22M4 15h22M4 23h22"
-            />
-          </svg>
-        )}
-      </button>
-      <ul className="menu__list">
-        {thisSidebar.map(item => renderItem(item, {root: true}))}
-      </ul>
+    <div className={styles.sidebar}>
+      <div
+        className={classnames('menu', 'menu--responsive', {
+          'menu--show': showResponsiveSidebar,
+        })}>
+        <button
+          className="button button--secondary button--sm menu__button"
+          type="button"
+          onClick={() => {
+            setShowResponsiveSidebar(!showResponsiveSidebar);
+          }}>
+          {showResponsiveSidebar ? (
+            <span
+              className={classnames(
+                styles.sidebarMenuIcon,
+                styles.sidebarMenuCloseIcon,
+              )}>
+              &times;
+            </span>
+          ) : (
+            <svg
+              className={styles.sidebarMenuIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              height={MOBILE_TOGGLE_SIZE}
+              width={MOBILE_TOGGLE_SIZE}
+              viewBox="0 0 32 32"
+              role="img"
+              focusable="false">
+              <title>Menu</title>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                strokeWidth="2"
+                d="M4 7h22M4 15h22M4 23h22"
+              />
+            </svg>
+          )}
+        </button>
+        <ul className="menu__list">
+          {thisSidebar.map(item => renderItem(item, {root: true}))}
+        </ul>
+      </div>
     </div>
   );
 }
