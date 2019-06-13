5763760008b8e93205e87ab704dfe63bb0ed41b3
Sakti Kumar Chourasia <schourasia2013@gmail.com>
2019-06-14 01:56:09 +0530

    added autoprefixer code with postcss loader

--- packages/docusaurus/src/webpack/utils.ts
+++ packages/docusaurus/src/webpack/utils.ts
@@ -39,6 +39,26 @@ export function getStyleLoaders(
       loader: require.resolve('css-loader'),
       options: cssOptions,
     },
+    {
+      // Options for PostCSS as we reference these options twice
+      // Adds vendor prefixing based on your specified browser support in
+      // package.json
+      loader: require.resolve('postcss-loader'),
+      options: {
+        // Necessary for external CSS imports to work
+        // https://github.com/facebook/create-react-app/issues/2677
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+        ],
+      },
+    },
   ].filter(Boolean) as Loader[];
   return loaders;
 }
