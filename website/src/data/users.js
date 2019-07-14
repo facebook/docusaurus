281f02279f411c763ee97d2470bbff4460d060d3
Yangshun Tay <tay.yang.shun@gmail.com>
2019-07-13 21:44:40 -0700

    feat(v2): add showcase page (#1660)
    * feat(v2): add showcase page
    
    * misc(v2): shift users out of pages to prevent it from being treated as a page
    
    * misc(v2): update title

---
+++ website/src/data/users.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const users = [
+  // Please add in alphabetical order of title.
+  {
+    title: 'Channel.js',
+    description: 'The missing constructor for creating safe async iterators',
+    preview: '/img/showcase/channeljs.png',
+    website: 'https://channel.js.org',
+    source: 'https://github.com/channeljs/channel',
+    fbOpenSource: false,
+    pinned: false,
+  },
+  {
+    title: 'Component Kit',
+    description: 'A declarative UI framework for iOS',
+    preview: '/img/showcase/componentkit.png',
+    website: 'https://componentkit.org',
+    source: 'https://github.com/facebook/componentkit',
+    fbOpenSource: true,
+    pinned: true,
+  },
+  {
+    title: 'Flux',
+    description: 'Application architecture for building user interfaces',
+    preview: '/img/showcase/flux.png',
+    website: 'https://facebook.github.io/flux/',
+    source: 'https://github.com/facebook/flux',
+    fbOpenSource: true,
+    pinned: true,
+  },
+  {
+    title: 'Hermes',
+    description: 'JavaScript engine optimized for React Native',
+    preview: '/img/showcase/hermes.png',
+    website: 'https://hermesengine.dev',
+    source: 'https://github.com/facebook/hermes',
+    fbOpenSource: true,
+    pinned: true,
+  },
+  {
+    title: 'uniforms',
+    description: 'A set of React libraries for building forms',
+    preview: '/img/showcase/uniforms.png',
+    website: 'https://uniforms.tools/',
+    source: 'https://github.com/vazco/uniforms',
+    fbOpenSource: false,
+    pinned: false,
+  },
+];
+
+export default users;
