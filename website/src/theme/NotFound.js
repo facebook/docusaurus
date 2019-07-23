458e10c9ae92602a3aad9fdc229d417cd4454315
endiliey <endiliey@gmail.com>
2019-07-24 00:01:24 +0700

    chore(v2): make feedback page accessible, not 404

---
+++ website/src/theme/NotFound.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Layout from '@theme/Layout';
+import Feedback from '../pages/feedback';
+
+function NotFound({location}) {
+  if (/^\/feedback/.test(location.pathname)) {
+    return <Feedback />;
+  }
+
+  return (
+    <Layout title="Page Not Found">
+      <div className="container margin-vert--xl">
+        <div className="row">
+          <div className="col col--6 col--offset-3">
+            <h1 className="hero__title">Page Not Found</h1>
+            <p>We could not find what you were looking for.</p>
+            <p>
+              Please contact the owner of the site that linked you to the
+              original URL and let them know their link is broken.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default NotFound;
