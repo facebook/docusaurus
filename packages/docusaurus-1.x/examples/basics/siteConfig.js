09aa7f54369672d44f55418a540620332f20c262
Neal B <Info@nealbuerger.com>
2019-08-20 21:24:32 +0200

    fix(prettier): fixed accidental formatting

--- packages/docusaurus-1.x/examples/basics/siteConfig.js
+++ packages/docusaurus-1.x/examples/basics/siteConfig.js
@@ -41,10 +41,10 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'doc1', label: 'Docs' },
-    { doc: 'doc4', label: 'API' },
-    { page: 'help', label: 'Help' },
-    { blog: true, label: 'Blog' },
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
