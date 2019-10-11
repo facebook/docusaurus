ca4d942e093ae71919d05f00a3638606183b129c
Endi <endiliey@gmail.com>
2019-10-12 01:25:08 +0800

    Update packages/docusaurus-plugin-content-docs/src/__tests__/lastUpdate.test.ts

--- packages/docusaurus-plugin-content-docs/src/__tests__/lastUpdate.test.ts
+++ packages/docusaurus-plugin-content-docs/src/__tests__/lastUpdate.test.ts
@@ -9,7 +9,6 @@ import fs from 'fs';
 import path from 'path';
 
 import lastUpdate from '../lastUpdate';
-import {SpawnSyncReturns} from 'child_process';
 
 describe('lastUpdate', () => {
   test('existing test file in repository with Git timestamp', () => {
