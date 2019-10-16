f4c0c85669b6f8f6b6011fccebbb4b18645a90d8
endiliey <endiliey@gmail.com>
2019-10-16 13:26:17 +0700

    chore(v2): add license header for clean-webpack-plugin

--- packages/docusaurus/src/webpack/plugins/CleanWebpackPlugin.ts
+++ packages/docusaurus/src/webpack/plugins/CleanWebpackPlugin.ts
@@ -5,6 +5,29 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2015 John Agan
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// Forked from https://github.com/johnagan/clean-webpack-plugin
+// Modified to optimize performance for Docusaurus specific use case
+
 import {Compiler, Stats} from 'webpack';
 import path from 'path';
 import {sync as delSync} from 'del';
