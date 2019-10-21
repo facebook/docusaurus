20c8df136c3a40bf8b660b0aed88bee2e5228255
endiliey <endiliey@gmail.com>
2019-10-21 13:49:33 +0700

    feat(v2): new ui progress bar

---
+++ packages/docusaurus/src/webpack/plugins/LogPlugin.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const WebpackBar = require('webpackbar');
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+
+function showError(arr) {
+  console.log(`\n\n${arr.join('')}`);
+}
+
+class LogPlugin extends WebpackBar {
+  apply(compiler) {
+    super.apply(compiler);
+
+    compiler.hooks.done.tap('WebpackNiceLog', stats => {
+      const messages = formatWebpackMessages(stats.toJson('errors-only', true));
+      if (messages.errors.length) {
+        showError(messages.errors);
+      }
+    });
+  }
+}
+
+module.exports = LogPlugin;
--- packages/docusaurus/src/webpack/server.ts
+++ packages/docusaurus/src/webpack/server.ts
@@ -9,16 +9,15 @@ import path from 'path';
 import StaticSiteGeneratorPlugin from 'static-site-generator-webpack-plugin';
 import {Configuration} from 'webpack';
 import merge from 'webpack-merge';
-import WebpackNiceLog from 'webpack-nicelog';
 
 import {Props} from '@docusaurus/types';
 import {createBaseConfig} from './base';
 import WaitPlugin from './plugins/WaitPlugin';
+import LogPlugin from './plugins/LogPlugin';
 
 export function createServerConfig(props: Props): Configuration {
   const {baseUrl, routesPaths, outDir} = props;
   const config = createBaseConfig(props, true);
-  const isProd = process.env.NODE_ENV === 'production';
 
   const routesLocation = {};
   // Array of paths to be rendered. Relative to output directory
@@ -57,10 +56,9 @@ export function createServerConfig(props: Props): Configuration {
       }),
 
       // Show compilation progress bar.
-      new WebpackNiceLog({
+      new LogPlugin({
         name: 'Server',
         color: 'yellow',
-        skipBuildTime: isProd,
       }),
     ],
   });
