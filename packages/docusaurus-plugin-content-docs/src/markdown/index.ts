8b9c8182e66ada75dc8d7fb91a111fe13adc34a6
endiliey <endiliey@gmail.com>
2019-10-21 12:21:03 +0700

    nits

--- packages/docusaurus-plugin-content-docs/src/markdown/index.ts
+++ packages/docusaurus-plugin-content-docs/src/markdown/index.ts
@@ -6,7 +6,7 @@
  */
 
 import path from 'path';
-const {getOptions} = require('loader-utils');
+import {getOptions} from 'loader-utils';
 import {resolve} from 'url';
 import {loader} from 'webpack';
 
