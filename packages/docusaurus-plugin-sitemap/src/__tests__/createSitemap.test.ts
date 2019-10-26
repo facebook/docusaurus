736690ab771dcc8a2bc2353da0b61fa1288a2a31
moozzyk <moozzyk@gmail.com>
2019-10-26 08:44:40 -0700

    Addressing review comments

--- packages/docusaurus-plugin-sitemap/src/__tests__/createSitemap.test.ts
+++ packages/docusaurus-plugin-sitemap/src/__tests__/createSitemap.test.ts
@@ -6,15 +6,21 @@
  */
 
 import createSitemap from '../createSitemap';
+import {DocusaurusConfig} from '@docusaurus/types';
+import DEFAULT_OPTIONS from '../index';
 
 describe('createSitemap', () => {
   test('simple site', () => {
     const sitemap = createSitemap(
       {
         url: 'https://example.com',
-      } as any,
+      } as DocusaurusConfig,
       ['/', '/test'],
-      {} as any,
+      {
+        cacheTime: 600,
+        changefreq: 'daily',
+        priority: 0.7,
+      },
     );
     expect(sitemap.toString()).toContain(
       `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">`,
