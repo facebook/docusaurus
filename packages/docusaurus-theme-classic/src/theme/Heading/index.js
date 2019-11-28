47ea13b59a42187e826531d3a6b096bea77760fa
Alexey Pyltsyn <lex61rus@gmail.com>
2019-11-28 22:38:06 +0300

    Lint

--- packages/docusaurus-theme-classic/src/theme/Heading/index.js
+++ packages/docusaurus-theme-classic/src/theme/Heading/index.js
@@ -18,7 +18,12 @@ const Heading = Tag => ({id, ...props}) => {
   return (
     <Tag {...props}>
       <a aria-hidden="true" tabIndex="-1" className="anchor" id={id} />
-      <a aria-hidden="true" tabIndex="-1" className="hash-link" href={`#${id}`} title="Direct link to section">
+      <a
+        aria-hidden="true"
+        tabIndex="-1"
+        className="hash-link"
+        href={`#${id}`}
+        title="Direct link to section">
         #
       </a>
       {props.children}
