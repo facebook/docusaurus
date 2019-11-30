0b41cdb86cb2c3edd2e7a2a1378e99e14b20b689
Yangshun Tay <tay.yang.shun@gmail.com>
2019-11-30 15:24:24 -0800

    Update types.ts

--- packages/docusaurus-plugin-ideal-image/src/types.ts
+++ packages/docusaurus-plugin-ideal-image/src/types.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export interface PluginOptions {
   /**
    * Filename template for output files.
