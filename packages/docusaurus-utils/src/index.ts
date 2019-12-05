004e9301fb17d100e2d76b08649aa4518f529872
endiliey <endiliey@gmail.com>
2019-12-06 00:13:41 +0700

    perf(v2): improve sequential build time by not overwriting file if possible

--- packages/docusaurus-utils/src/index.ts
+++ packages/docusaurus-utils/src/index.ts
@@ -27,7 +27,18 @@ export async function generate(
     return;
   }
 
-  const lastHash = fileHash.get(filepath);
+  let lastHash = fileHash.get(filepath);
+
+  // If file already exist but its not in runtime cache hash yet,
+  // we try to calculate the content hash and then compare
+  // This is to avoid unnecessary overwrite and we can reuse old file
+  if (!lastHash && fs.existsSync(filepath)) {
+    const lastContent = await fs.readFile(filepath, 'utf8');
+    lastHash = createHash('md5')
+      .update(lastContent)
+      .digest('hex');
+  }
+
   const currentHash = createHash('md5')
     .update(content)
     .digest('hex');
