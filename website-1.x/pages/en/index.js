20f8dfce3a2172abc3e24bb7dca50476ebc94245
t11s <hello@thecosmic.io>
2019-12-17 08:00:52 -0800

    Add space between "out" and "Docusaurus"

--- website-1.x/pages/en/index.js
+++ website-1.x/pages/en/index.js
@@ -63,7 +63,7 @@ class Index extends React.Component {
         <div className="announcement">
           <div className="announcement-inner">
             If you don't need advanced features such as versioning or
-            translations, try out
+            translations, try out 
             <a href="https://v2.docusaurus.io">Docusaurus 2</a> instead!
             Contribute to its roadmap by suggesting features or{' '}
             <a href="https://v2.docusaurus.io/feedback/">
