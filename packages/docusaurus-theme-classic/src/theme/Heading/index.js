155539421b40cd14f4b2592827fb96ac3c56fac8
Alexey Pyltsyn <lex61rus@gmail.com>
2020-01-23 19:06:51 +0300

    fix(v2): unify anchor behavior (#2162)
    * fix(v2): unify anchor behavior
    
    * fix(v2): use activeElement
    
    * fix(v2): add support for sticky header
    
    * Fix highlighted anchor if non-hidable navbar enabled

--- packages/docusaurus-theme-classic/src/theme/Heading/index.js
+++ packages/docusaurus-theme-classic/src/theme/Heading/index.js
@@ -8,27 +8,45 @@
 /* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid */
 
 import React from 'react';
+import classnames from 'classnames';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import './styles.css';
+import styles from './styles.module.css';
 
-const Heading = Tag => ({id, ...props}) => {
-  if (!id) {
-    return <Tag {...props} />;
-  }
-  return (
-    <Tag {...props}>
-      <a aria-hidden="true" tabIndex="-1" className="anchor" id={id} />
-      <a
-        aria-hidden="true"
-        tabIndex="-1"
-        className="hash-link"
-        href={`#${id}`}
-        title="Direct link to heading">
-        #
-      </a>
-      {props.children}
-    </Tag>
-  );
-};
+const Heading = Tag =>
+  function TargetComponent({id, ...props}) {
+    const {
+      siteConfig: {
+        themeConfig: {navbar: {hideOnScroll = false} = {}} = {},
+      } = {},
+    } = useDocusaurusContext();
+
+    if (!id) {
+      return <Tag {...props} />;
+    }
+
+    return (
+      <Tag {...props}>
+        <a
+          aria-hidden="true"
+          tabIndex="-1"
+          className={classnames('anchor', {
+            [styles.enhancedAnchor]: !hideOnScroll,
+          })}
+          id={id}
+        />
+        <a
+          aria-hidden="true"
+          tabIndex="-1"
+          className="hash-link"
+          href={`#${id}`}
+          title="Direct link to heading">
+          #
+        </a>
+        {props.children}
+      </Tag>
+    );
+  };
 
 export default Heading;
