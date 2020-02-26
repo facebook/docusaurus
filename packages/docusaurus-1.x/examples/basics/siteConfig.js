4dd4f09e45b330b143686e5b554b2a1f9de42cc2
Joe Previte <jprevite@fb.com>
2020-02-26 14:31:38 -0800

    fix: check repoUrl before rendering edit link in users

--- packages/docusaurus-1.x/examples/basics/siteConfig.js
+++ packages/docusaurus-1.x/examples/basics/siteConfig.js
@@ -104,7 +104,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  // repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
