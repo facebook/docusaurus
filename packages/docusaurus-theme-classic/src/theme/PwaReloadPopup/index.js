32346c0e56d735920cbb4d006a59ab8e3dff7f74
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2020-02-26 00:33:21 -0800

    implement @theme/PwaReloadPopup

---
+++ packages/docusaurus-theme-classic/src/theme/PwaReloadPopup/index.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2020-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, {useState} from 'react';
+import classnames from 'classnames';
+
+import styles from './styles.module.css';
+
+export default function PwaReloadPopup({onRefresh}) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    isVisible && (
+      <div className={classnames('alert', 'alert--secondary', styles.popup)}>
+        {/* TODO: put in i18n */}
+        <p>New version available</p>
+        <div className={styles.buttonContainer}>
+          <button
+            className="button button--link"
+            type="button"
+            onClick={() => {
+              setIsVisible(false);
+              onRefresh();
+            }}>
+            {/* TODO: put in i18n */}
+            Refresh
+          </button>
+
+          <button
+            aria-label="Close"
+            className="close"
+            type="button"
+            onClick={() => setIsVisible(false)}>
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+      </div>
+    )
+  );
+}
