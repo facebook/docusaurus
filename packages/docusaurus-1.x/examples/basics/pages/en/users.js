590de952344dcdae28dcc1a2f908c33ac89d9b2d
Joe Previte <jprevite@fb.com>
2020-02-26 14:59:04 -0800

    fix(v1): check repo url before using on Users page (#2331)
    * fix: check repoUrl before rendering edit link in users
    
    * refactor: prettier

--- packages/docusaurus-1.x/examples/basics/pages/en/users.js
+++ packages/docusaurus-1.x/examples/basics/pages/en/users.js
@@ -18,7 +18,6 @@ class Users extends React.Component {
       return null;
     }
 
-    const editUrl = `${siteConfig.repoUrl}/edit/master/website/siteConfig.js`;
     const showcase = siteConfig.users.map(user => (
       <a href={user.infoLink} key={user.infoLink}>
         <img src={user.image} alt={user.caption} title={user.caption} />
@@ -34,10 +33,16 @@ class Users extends React.Component {
               <p>This project is used by many folks</p>
             </div>
             <div className="logos">{showcase}</div>
-            <p>Are you using this project?</p>
-            <a href={editUrl} className="button">
-              Add your company
-            </a>
+            {siteConfig.repoUrl && (
+              <>
+                <p>Are you using this project?</p>
+                <a
+                  href={`${siteConfig.repoUrl}/edit/master/website/siteConfig.js`}
+                  className="button">
+                  Add your company
+                </a>
+              </>
+            )}
           </div>
         </Container>
       </div>
