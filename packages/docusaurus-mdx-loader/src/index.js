907dd1b266ce0da23726a763c0522b4126b8d5b2
Alexey Pyltsyn <lex61rus@gmail.com>
2020-03-19 00:18:05 +0300

    fix(v2): remove HTML from heading slug

--- packages/docusaurus-mdx-loader/src/index.js
+++ packages/docusaurus-mdx-loader/src/index.js
@@ -9,9 +9,9 @@ const {getOptions} = require('loader-utils');
 const {readFile} = require('fs-extra');
 const mdx = require('@mdx-js/mdx');
 const emoji = require('remark-emoji');
-const slug = require('remark-slug');
 const matter = require('gray-matter');
 const stringifyObject = require('stringify-object');
+const slug = require('./remark/slug');
 const rightToc = require('./remark/rightToc');
 
 const DEFAULT_OPTIONS = {
