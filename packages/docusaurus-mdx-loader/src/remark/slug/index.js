9cf3c66917ced27ba76e999cec2b3df0d3d888b2
Alexey Pyltsyn <lex61rus@gmail.com>
2020-03-21 10:22:55 +0300

    fix(v2): remove HTML from heading slug (#2426)
    * fix(v2): remove HTML from heading slug
    
    * Fix tests for rightToc

---
+++ packages/docusaurus-mdx-loader/src/remark/slug/index.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* Based on remark-slug (https://github.com/remarkjs/remark-slug) */
+
+const visit = require('unist-util-visit');
+const toString = require('mdast-util-to-string');
+const slugs = require('github-slugger')();
+
+function slug() {
+  const transformer = ast => {
+    slugs.reset();
+
+    function visitor(headingNode) {
+      const data = headingNode.data || (headingNode.data = {}); // eslint-disable-line
+      const properties = data.hProperties || (data.hProperties = {});
+      let {id} = properties;
+
+      if (id) {
+        id = slugs.slug(id, true);
+      } else {
+        const headingTextNodes =
+          headingNode.children.find(
+            ({type}) => !['html', 'jsx'].includes(type),
+          ) || headingNode;
+        id = slugs.slug(toString(headingTextNodes));
+      }
+
+      data.id = id;
+      properties.id = id;
+    }
+
+    visit(ast, 'heading', visitor);
+  };
+
+  return transformer;
+}
+
+module.exports = slug;
