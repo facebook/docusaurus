adb263c0c687b7a918334e19b786ea4be851cd2a
Mohamed Rashid <ge_rashid@hotmail.co.uk>
2020-03-25 19:27:29 +0000

    fix(v1): follow same indentation as the rest of the anchor tags

--- packages/docusaurus-1.x/examples/basics/core/Footer.js
+++ packages/docusaurus-1.x/examples/basics/core/Footer.js
@@ -34,14 +34,18 @@ class Footer extends React.Component {
             <a href={this.docUrl('doc1.html')}>
               Getting Started (or other categories)
             </a>
-            <a href={this.docUrl('doc2.html')}>Guides (or other categories)</a>
+            <a href={this.docUrl('doc2.html')}>
+              Guides (or other categories)
+            </a>
             <a href={this.docUrl('doc3.html')}>
               API Reference (or other categories)
             </a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={`${this.props.config.baseUrl}users`}>User Showcase</a>
+            <a href={`${this.props.config.baseUrl}users`}>
+              User Showcase
+            </a>
             <a
               href="https://stackoverflow.com/questions/tagged/"
               target="_blank"
