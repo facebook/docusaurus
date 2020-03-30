0de13769c280b2514652ae2582c4876a25bd11c5
Alexey Pyltsyn <lex61rus@gmail.com>
2020-03-30 15:25:29 +0300

    refactor(v2): remove unnecessary  X-UA-Compatible meta tag

--- packages/docusaurus-theme-classic/src/theme/Layout/index.js
+++ packages/docusaurus-theme-classic/src/theme/Layout/index.js
@@ -53,7 +53,6 @@ function Layout(props) {
           {/* TODO: Do not assume that it is in english language */}
           <html lang="en" />
 
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           {metaTitle && <title>{metaTitle}</title>}
           {metaTitle && <meta property="og:title" content={metaTitle} />}
           {favicon && <link rel="shortcut icon" href={faviconUrl} />}
