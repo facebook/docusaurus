0e948284392074e8ea8bf2ef506fb1900f5c7ae0
Can Gencer <cgencer@gmail.com>
2020-04-01 15:01:40 +0200

    Add Hazelcast Jet to users list

--- website-1.x/data/users.js
+++ website-1.x/data/users.js
@@ -302,6 +302,13 @@ module.exports = [
     pinned: false,
   },
   {
+    caption: 'Hazelcast Jet',
+    image: '/img/users/hazelcast-jet.svg',
+    infoLink: 'https://jet-start.sh',
+    fbOpenSource: false,
+    pinned: false,
+  },
+  {
     caption: 'Hemera',
     image: '/img/users/hemera.png',
     infoLink: 'https://hemerajs.github.io/hemera/',
