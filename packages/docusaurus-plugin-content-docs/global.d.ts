6280b65e94b30bd5eda6125777a12869d0e1faa7
Alexey Pyltsyn <lex61rus@gmail.com>
2020-04-05 17:33:21 +0300

    chore(v2): remove redundant file

--- packages/docusaurus-plugin-content-docs/global.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-
-// @mdx-js/mdx currently doesn't have any @types
-declare module '@mdx-js/mdx' {
-  export default function mdx(content: string, options: any): JSX.Element;
-}
