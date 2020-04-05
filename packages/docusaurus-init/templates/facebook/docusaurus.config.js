fbdcabe5c49e2d05af385e7a72ae00a24ec13665
Yangshun Tay <tay.yang.shun@gmail.com>
2020-04-05 23:13:15 +0800

    Update docusaurus.config.js

--- packages/docusaurus-init/templates/facebook/docusaurus.config.js
+++ packages/docusaurus-init/templates/facebook/docusaurus.config.js
@@ -100,11 +100,13 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
+          // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
