c09835a76261c1f3ea668005759d12d2d4bea5b7
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2020-04-05 12:16:40 -0300

    feat(v2): add showReadingTime and editUrl to the templates (#2541)
    * Add mdx plugins
    
    * Apply suggestions from code review
    
    Co-Authored-By: Alexey Pyltsyn <lex61rus@gmail.com>
    
    * Update website/docs/markdown-features.mdx
    
    Co-Authored-By: Alexey Pyltsyn <lex61rus@gmail.com>
    
    * Update markdown-features.mdx
    
    * Update templates with new fields
    
    * Rebase
    
    * fix(v2): tests
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    Co-authored-by: Yangshun Tay <tay.yang.shun@gmail.com>
    Co-authored-by: Alexey Pyltsyn <lex61rus@gmail.com>

--- packages/docusaurus-init/templates/facebook/docusaurus.config.js
+++ packages/docusaurus-init/templates/facebook/docusaurus.config.js
@@ -100,8 +100,15 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
