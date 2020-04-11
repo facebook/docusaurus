21df049c2bbc5ede60b798968c1351d99b5e088d
Mohamed Rashid <ge_rashid@hotmail.co.uk>
2020-04-11 01:34:46 +0100

    refactor(v2): exit when CI is true and there are warnings

--- packages/docusaurus/src/commands/build.ts
+++ packages/docusaurus/src/commands/build.ts
@@ -39,6 +39,20 @@ function compile(config: Configuration[]): Promise<any> {
           console.warn(warning);
         });
       }
+      if (
+        process.env.CI &&
+        (typeof process.env.CI !== 'string' || process.env.CI.toLowerCase() !== 'false') &&
+        stats.toJson('errors-warnings').warnings.length
+      ) {
+        console.log(
+          chalk.yellow(
+            '\nTreating warnings as errors because process.env.CI = true.\n' +
+            'Most CI servers set it automatically.\n' +
+            stats.toJson('errors-warnings').warnings
+          )
+        );
+        return reject(new Error(stats.toJson('errors-warnings').warnings.join('\n\n')));
+      }
       resolve();
     });
   });
