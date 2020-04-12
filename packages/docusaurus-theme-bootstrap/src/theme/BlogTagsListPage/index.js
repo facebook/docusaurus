baff4188da26decdfd26a0e2af37a21f056a01c1
fanny <fanny.vieira@ccc.ufcg.edu.br>
2020-04-12 11:47:16 -0300

    feat(v2): add tags

--- packages/docusaurus-theme-bootstrap/src/theme/BlogTagsListPage/index.js
+++ packages/docusaurus-theme-bootstrap/src/theme/BlogTagsListPage/index.js
@@ -4,5 +4,27 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import React from 'react';
 
-export default () => {};
+function BlogTagsListPage(props) {
+  const {tags} = props;
+  const renderAllTags = () => (
+    <>
+      {Object.keys(tags).map((tag) => (
+        <li key={tag} className="btn btn-primary m-2">
+          {tags[tag].name}{' '}
+          <span className="badge badge-light">{tags[tag].count}</span>
+        </li>
+      ))}
+    </>
+  );
+
+  return (
+    <div>
+      <h1 className="text-primary">Tags</h1>
+      <ul>{renderAllTags()}</ul>
+    </div>
+  );
+}
+
+export default BlogTagsListPage;
