bf4c7f7823c33f76969ac9af882af28e2a499eb2
Yangshun Tay <tay.yang.shun@gmail.com>
2020-04-13 02:04:40 +0800

    Update types.d.ts

--- packages/docusaurus/src/client/types.d.ts
+++ packages/docusaurus/src/client/types.d.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 declare module '@generated/client-modules' {
   const clientModules: readonly any[];
   export default clientModules;
