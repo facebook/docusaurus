7700b743a89b5016671ba0a18a2d551c849f4672
fanny <fanny.vieira@ccc.ufcg.edu.br>
2020-04-14 20:34:52 -0300

    feat(v2): Add blog post paginator

---
+++ packages/docusaurus-theme-bootstrap/src/theme/BlogPostPaginator/index.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Link from '@docusaurus/Link';
+
+function BlogPostPaginator(props) {
+  const {nextItem, prevItem} = props;
+
+  return (
+    <nav aria-label="Blog post page navigation" className="my-3">
+      <ul className="pagination justify-content-between">
+        <li className="page-item">
+          {prevItem && (
+            <Link className="page-link rounded-pill" to={prevItem.permalink}>
+              &laquo; {prevItem.title}
+            </Link>
+          )}
+        </li>
+        <li className="page-item">
+          {nextItem && (
+            <Link className="page-link rounded-pill" to={nextItem.permalink}>
+              {nextItem.title} &raquo;
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default BlogPostPaginator;
