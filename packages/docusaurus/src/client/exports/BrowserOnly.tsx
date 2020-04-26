4bd4ce667fd7cb105bc0e593dd5da92d56509b21
Yangshun Tay <tay.yang.shun@gmail.com>
2020-04-26 20:36:42 +0800

    Update BrowserOnly.tsx

--- packages/docusaurus/src/client/exports/BrowserOnly.tsx
+++ packages/docusaurus/src/client/exports/BrowserOnly.tsx
@@ -9,11 +9,11 @@ import React from 'react';
 import ExecutionEnvironment from './ExecutionEnvironment';
 
 function BrowserOnly({children, fallback}) {
-  if (ExecutionEnvironment.canUseDOM && children != null) {
-    return <>{children()}</>;
+  if (!ExecutionEnvironment.canUseDOM || children == null) {  
+    return fallback || null;
   }
-
-  return fallback || null;
+  
+  return <>{children()}</>;
 }
 
 export default BrowserOnly;
