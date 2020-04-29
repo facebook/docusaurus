30177230570456c371688ed6f65eff02dcd22eda
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2020-04-29 14:07:08 -0300

    feat(v2): bootstrap pagination (#2695)
    * feat(v2): Add pagination
    
    * çhore(v2): remove console.log
    
    * feat(v2): remove changes in template

--- packages/docusaurus-theme-bootstrap/src/theme/BlogPostPage/index.js
+++ packages/docusaurus-theme-bootstrap/src/theme/BlogPostPage/index.js
@@ -7,23 +7,28 @@
 
 import React from 'react';
 import BlogPostItem from '@theme/BlogPostItem';
+import BlogPostPaginator from '@theme/BlogPostPaginator';
 
 function BlogPostPage(props) {
   const {content: BlogPostContents} = props;
   const {frontMatter, metadata} = BlogPostContents;
+  const {nextItem, prevItem} = metadata;
 
   return (
     <div className="container-fluid my-5">
       <div className="row row-cols-1 row-cols-sm-1">
         <div
           key={metadata.permalink}
-          className="col col-md-6 offset-md-3 col-xs-6 mb-5">
+          className="col col-xl-4 offset-xl-4 col-xs-6 mb-5">
           <BlogPostItem
             frontMatter={frontMatter}
             metadata={metadata}
             isBlogPostPage>
             <BlogPostContents />
           </BlogPostItem>
+          {(nextItem || prevItem) && (
+            <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />
+          )}
         </div>
       </div>
     </div>
