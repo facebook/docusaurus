20543c903464ffea1fbbc6b3a0f744add0bc30f7
Alexey Pyltsyn <lex61rus@gmail.com>
2020-05-17 20:43:16 +0300

    chore(v2): mention about docs home page in init templates

--- packages/docusaurus-init/templates/classic/docusaurus.config.js
+++ packages/docusaurus-init/templates/classic/docusaurus.config.js
@@ -83,6 +83,8 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
