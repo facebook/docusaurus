76df524d85d5823278dc6780db6a7cf25335878c
slorber <lorber.sebastien@gmail.com>
2020-05-28 15:01:51 +0200

    feat(v2): add components to react-live scope (#2807)

--- packages/docusaurus-theme-live-codeblock/src/theme/CodeBlock/index.js
+++ packages/docusaurus-theme-live-codeblock/src/theme/CodeBlock/index.js
@@ -9,18 +9,33 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import usePrismTheme from '@theme/hooks/usePrismTheme';
 import Playground from '@theme/Playground';
+import ReactLiveScope from '@theme/ReactLiveScope';
 import CodeBlock from '@theme-init/CodeBlock';
+import {useMDXComponents} from '@mdx-js/react';
+
+// Returns components that are available to use by the react-live playground
+// It's not possible to import anything in the live playground,
+// we need to provide the available imports manually
+// See https://github.com/facebook/docusaurus/issues/2807
+const useReactLiveScope = () => {
+  const mdxComponents = useMDXComponents();
+  return {
+    ...mdxComponents,
+    ...ReactLiveScope,
+  };
+};
 
 const withLiveEditor = (Component) => {
   const WrappedComponent = (props) => {
     const {isClient} = useDocusaurusContext();
     const prismTheme = usePrismTheme();
+    const reactLiveScope = useReactLiveScope();
 
     if (props.live) {
       return (
         <Playground
           key={isClient}
-          scope={{...React}}
+          scope={reactLiveScope}
           theme={prismTheme}
           {...props}
         />
