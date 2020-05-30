72b319a29395e248eb1343bdc021d6804457e0ef
tetunori <tetunori@gmail.com>
2020-05-30 15:22:27 +0900

    fix: use `useBaseUrl()`

--- packages/docusaurus-theme-search-algolia/src/theme/hooks/useSearchQuery.js
+++ packages/docusaurus-theme-search-algolia/src/theme/hooks/useSearchQuery.js
@@ -7,14 +7,14 @@
 
 import {useHistory, useLocation} from '@docusaurus/router';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const SEARCH_PARAM_QUERY = 'q';
 
 function useSearchQuery() {
   const history = useHistory();
   const location = useLocation();
-  const {siteConfig: {baseUrl} = {}} = useDocusaurusContext();
+  const baseUrl = useBaseUrl('');
 
   return {
     searchValue:
