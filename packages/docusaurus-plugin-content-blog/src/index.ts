053c5073f8898397a76ba3983a44910aa74f59a6
moonrailgun <moonrailgun@gmail.com>
2020-06-15 14:23:24 +0800

    perf(v2): convert synchronous filewrite to asynchronous in feed file generate
    This looks like should return a Promise list , other than a sync io operation

--- packages/docusaurus-plugin-content-blog/src/index.ts
+++ packages/docusaurus-plugin-content-blog/src/index.ts
@@ -448,11 +448,9 @@ export default function pluginContentBlog(
             `${feedType}.xml`,
           );
           const feedContent = feedType === 'rss' ? feed.rss2() : feed.atom1();
-          try {
-            fs.writeFileSync(feedPath, feedContent);
-          } catch (err) {
+          return fs.outputFile(feedPath, feedContent).catch((err) => {
             throw new Error(`Generating ${feedType} feed failed: ${err}`);
-          }
+          });
         }),
       );
     },
