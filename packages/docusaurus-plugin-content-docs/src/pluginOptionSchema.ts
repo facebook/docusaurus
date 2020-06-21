f218b55e24cc6c752f53306d4581ee90ab300478
teikjun <teikjunhci@gmail.com>
2020-06-21 17:36:04 +0800

    fix(v2): fix plugin-content-docs schema

--- packages/docusaurus-plugin-content-docs/src/pluginOptionSchema.ts
+++ packages/docusaurus-plugin-content-docs/src/pluginOptionSchema.ts
@@ -9,14 +9,15 @@ export const REVERSED_DOCS_HOME_PAGE_ID = '_index';
 
 export const PluginOptionSchema = Joi.object({
   path: Joi.string().default('docs'), // Path to data on filesystem, relative to site dir.
+  editUrl: Joi.string().uri(), // website URL for user to propose edits
   routeBasePath: Joi.string().default('docs'), // URL Route.
   homePageId: Joi.string().default(REVERSED_DOCS_HOME_PAGE_ID), // Document id for docs home page.
   include: Joi.array().items(Joi.string()).default(['**/*.{md,mdx}']), // Extensions to include
   sidebarPath: Joi.string().default(''), // Path to sidebar configuration for showing a list of markdown pages.
   docLayoutComponent: Joi.string().default('@theme/DocPage'),
   docItemComponent: Joi.string().default('@theme/DocItem'),
-  remarkPlugins: Joi.array().items(Joi.object()).default([]),
-  rehypePlugins: Joi.array().items(Joi.object()).default([]),
+  remarkPlugins: Joi.array().default([]),
+  rehypePlugins: Joi.array().default([]),
   showLastUpdateTime: Joi.bool().default(false),
   showLastUpdateAuthor: Joi.bool().default(false),
   admonitions: Joi.object().default({}),
