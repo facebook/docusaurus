20ebac26870cd16e6e2184c8b0de0e22dc2322c4
slorber <lorber.sebastien@gmail.com>
2020-07-15 19:25:08 +0200

    fix broken link on template

--- packages/docusaurus-init/templates/bootstrap/docusaurus.config.js
+++ packages/docusaurus-init/templates/bootstrap/docusaurus.config.js
@@ -3,6 +3,7 @@ module.exports = {
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
+  onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
