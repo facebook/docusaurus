ffd64490dc3595044dc98c313391b397ac85fc27
slorber <lorber.sebastien@gmail.com>
2020-08-04 19:02:59 +0200

    redirect old community docs to new urls

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -69,6 +69,20 @@ module.exports = {
             return [`${path}/introduction`];
           }
         },
+        redirects: [
+          {
+            from: ['/docs/support', '/docs/next/support'],
+            to: '/community/support',
+          },
+          {
+            from: ['/docs/team', '/docs/next/team'],
+            to: '/community/team',
+          },
+          {
+            from: ['/docs/resources', '/docs/next/resources'],
+            to: '/community/resources',
+          },
+        ],
       },
     ],
     [
