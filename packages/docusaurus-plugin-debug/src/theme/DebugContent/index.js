e062fca229641cb75ffda81576ef96631f6bc40d
slorber <lorber.sebastien@gmail.com>
2020-08-06 21:44:01 +0200

    improve content rendering a bit

--- packages/docusaurus-plugin-debug/src/theme/DebugContent/index.js
+++ packages/docusaurus-plugin-debug/src/theme/DebugContent/index.js
@@ -5,12 +5,62 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import DebugLayout from '../DebugLayout';
 
+const PluginInstanceContent = ({pluginId, pluginInstanceContent}) => (
+  <section style={{marginTop: 30}}>
+    <h4>{`>> ${pluginId}`}</h4>
+    <div style={{marginTop: 10}}>
+      {JSON.stringify(pluginInstanceContent, null, 2)}
+    </div>
+  </section>
+);
+
+const PluginContent = ({pluginName, pluginContent}) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <section style={{marginTop: 60}}>
+      <h3 onClick={() => setVisible((v) => !v)} style={{cursor: 'pointer'}}>
+        {pluginName}
+      </h3>
+      {visible && (
+        <div>
+          {Object.entries(pluginContent).map(
+            ([pluginId, pluginInstanceContent]) => {
+              return (
+                <PluginInstanceContent
+                  key={pluginName}
+                  pluginId={pluginId}
+                  pluginInstanceContent={pluginInstanceContent}
+                />
+              );
+            },
+          )}
+        </div>
+      )}
+    </section>
+  );
+};
+
 function DebugContent({allContent}) {
-  return <DebugLayout>{JSON.stringify(allContent, null, 2)}</DebugLayout>;
+  return (
+    <DebugLayout>
+      <h2>Plugin content</h2>
+      <div>
+        {Object.entries(allContent).map(([pluginName, pluginContent]) => {
+          return (
+            <PluginContent
+              key={pluginName}
+              pluginName={pluginName}
+              pluginContent={pluginContent}
+            />
+          );
+        })}
+      </div>
+    </DebugLayout>
+  );
 }
 
 export default DebugContent;
