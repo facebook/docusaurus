b145dde57f38e7d25c166ebaeb33dd4557769c2e
ako92 <alihk12@mail.com>
2020-08-08 20:05:57 +0430

    fix:type problem fixed

--- packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -64,15 +64,16 @@ function NavLink({
 
 function NavItemDesktop({items, position, className, ...props}) {
   const dropDownRef = React.useRef<HTMLDivElement>(null);
-  const dropDownMenuRef = React.useRef<HTMLUListElement>(null); // TODO should find better solution for this. anything else will get a error when retrieve children.
+  const dropDownMenuRef = React.useRef<HTMLUListElement>(null);
   const [showDropDown, setShowDropDown] = useState(false);
   useOnClickOutside(dropDownRef, () => toggle(false));
   function toggle(state: boolean) {
     if (state) {
       const firstNavLinkOfULElement =
         dropDownMenuRef?.current?.firstChild?.firstChild;
+
       if (firstNavLinkOfULElement) {
-        firstNavLinkOfULElement.focus();
+        (firstNavLinkOfULElement as HTMLElement).focus();
       }
     }
     setShowDropDown(state);
