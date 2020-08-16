4144cb4e170681c3a0ac67e1d51b107d2f9094e2
Drewbi <drewalexander986@gmail.com>
2020-08-17 01:45:39 +0800

    Add style and collapse depth to json viewer

--- packages/docusaurus-plugin-debug/src/theme/DebugJsonView/index.js
+++ packages/docusaurus-plugin-debug/src/theme/DebugJsonView/index.js
@@ -8,6 +8,7 @@
 import React from 'react';
 
 import BrowserOnly from '@docusaurus/BrowserOnly';
+// import styles from './styles.module.css'
 
 // avoids "react-json-view" to display  "root"
 const RootName = false;
@@ -25,17 +26,28 @@ const BrowserOnlyReactJson = (props) => {
   );
 };
 
-function DebugJsonView({src}) {
+function DebugJsonView({src, collapseDepth}) {
   return (
     <BrowserOnlyReactJson
       src={src}
+      style={{
+        marginTop: '10px',
+        padding: '10px',
+        borderRadius: '4px',
+        backgroundColor: '#242526'
+      }}
       name={RootName}
+      theme="paraiso"
       shouldCollapse={(field) => {
         // By default, we collapse the json for performance reasons
         // See https://github.com/mac-s-g/react-json-view/issues/235
-        // only the "root" is not collapsed
-        return field.name !== RootName;
+        // Non-root elements that are larger than 50 fields are collapsed
+        return field.name !== RootName && Object.keys(field.src).length > 50;
       }}
+      collapsed={collapseDepth}
+      groupArraysAfterLength="5"
+      enableClipboard={false}
+      displayDataTypes={false}
     />
   );
 }
