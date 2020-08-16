dd99e4241ed5a45e1b0d8b78bd14f8fe5ec88a1b
Drewbi <drewalexander986@gmail.com>
2020-08-17 01:45:53 +0800

    Add style to debug layout

--- packages/docusaurus-plugin-debug/src/theme/DebugLayout/index.js
+++ packages/docusaurus-plugin-debug/src/theme/DebugLayout/index.js
@@ -7,14 +7,12 @@
 
 import React from 'react';
 import Link from '@docusaurus/Link';
-// import styles from './styles.module.css';
+import styles from './styles.module.css';
 
 const DebugNavLink = ({to, children}) => (
   <Link
-    style={{margin: 10}}
-    className="button button--primary"
+    className={styles.navlink}
     isNavLink
-    activeClassName="button--active"
     to={to}
     exact>
     {children}
@@ -24,14 +22,14 @@ const DebugNavLink = ({to, children}) => (
 function DebugLayout({children}) {
   return (
     <div>
-      <nav style={{width: '100%', padding: 10, border: 'solid'}}>
+      <nav className={styles.nav}>
         <DebugNavLink to="/__docusaurus/debug">Config</DebugNavLink>
         <DebugNavLink to="/__docusaurus/debug/metadata">Metadata</DebugNavLink>
         <DebugNavLink to="/__docusaurus/debug/registry">Registry</DebugNavLink>
         <DebugNavLink to="/__docusaurus/debug/routes">Routes</DebugNavLink>
         <DebugNavLink to="/__docusaurus/debug/content">Content</DebugNavLink>
       </nav>
-      <main style={{padding: 20}}>{children}</main>
+      <main className={styles.container}>{children}</main>
     </div>
   );
 }
