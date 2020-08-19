13c737e3f6ab28550b98dcca3a3113e756598ae6
slorber <lorber.sebastien@gmail.com>
2020-08-19 17:36:12 +0200

    move pluginIds.test.ts

--- packages/docusaurus/src/server/plugins/__tests__/pluginIds.test.ts
+++ packages/docusaurus/src/server/plugins/__tests__/pluginIds.test.ts
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {ensureUniquePluginInstanceIds} from '../../plugins/pluginIds';
+import {ensureUniquePluginInstanceIds} from '../pluginIds';
 
-import {InitPlugin} from '../../plugins/init';
+import {InitPlugin} from '../init';
 
 function createTestPlugin(name: string, id?: string): InitPlugin {
   // @ts-expect-error: good enough for tests
