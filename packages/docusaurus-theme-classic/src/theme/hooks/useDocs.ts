681d4eedd573dcfe367c024aeaabb805971ce67e
slorber <lorber.sebastien@gmail.com>
2020-09-01 17:11:14 +0200

    try to fix useDocs

--- packages/docusaurus-theme-classic/src/theme/hooks/useDocs.ts
+++ packages/docusaurus-theme-classic/src/theme/hooks/useDocs.ts
@@ -7,7 +7,15 @@
 
 // See https://github.com/facebook/docusaurus/issues/3360
 // TODO find a better solution, this shouldn't be needed
-export {};
+
+// TODO this is  not ideal and produce a warning!
+// see https://github.com/webpack/webpack/issues/7713#issuecomment-467888437
+try {
+  module.exports = require('@theme-init/hooks/useDocs');
+} catch (e) {
+  module.exports = {};
+}
+
 /*
 throw new Error(
   "The docs plugin is not used, so you can't require the useDocs hooks. ",
