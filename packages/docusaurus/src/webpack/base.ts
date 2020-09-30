6a88e138f69c74ee8f3f2c735f3da81f69d6f733
Andrey Rublev <box@anru.me>
2020-09-30 14:45:28 +0700

    chore: fixed props being passed to @svgr/webpack loader
    Without comma option `-prettier-svgo` is parsed as `'prettier-svgo': false`

--- packages/docusaurus/src/webpack/base.ts
+++ packages/docusaurus/src/webpack/base.ts
@@ -218,7 +218,7 @@ export function createBaseConfig(
         },
         {
           test: /\.svg$/,
-          use: '@svgr/webpack?-prettier-svgo,+titleProp,+ref![path]',
+          use: '@svgr/webpack?-prettier,-svgo,+titleProp,+ref![path]',
         },
       ],
     },
