9632b6e980d17d025b072a241729b05b57d49874
Alexey Pyltsyn <lex61rus@gmail.com>
2020-10-12 18:12:03 +0300

    refactor(v2): clarify versions page

--- website/src/pages/versions.js
+++ website/src/pages/versions.js
@@ -20,8 +20,9 @@ function Version() {
   const pastVersions = versions.filter(
     (version) => version !== latestVersion && version.name !== 'current',
   );
-
+  const stableVersion = pastVersions.shift();
   const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
+
   return (
     <Layout
       title="Versions"
@@ -30,17 +31,19 @@ function Version() {
       <main className="container margin-vert--lg">
         <h1>Docusaurus documentation versions</h1>
         <div className="margin-bottom--lg">
-          <h3 id="latest">Latest version (Stable)</h3>
-          <p>Here you can find the latest documentation.</p>
+          <h3 id="next">Current version (Stable)</h3>
+          <p>
+            Here you can find the documentation for current released version.
+          </p>
           <table>
             <tbody>
               <tr>
-                <th>{latestVersion.label}</th>
+                <th>{stableVersion.name}</th>
                 <td>
-                  <Link to={latestVersion.path}>Documentation</Link>
+                  <Link to={stableVersion.path}>Documentation</Link>
                 </td>
                 <td>
-                  <a href={`${repoUrl}/releases/tag/v${latestVersion.name}`}>
+                  <a href={`${repoUrl}/releases/tag/v${stableVersion.name}`}>
                     Release Notes
                   </a>
                 </td>
@@ -48,6 +51,25 @@ function Version() {
             </tbody>
           </table>
         </div>
+
+        <div className="margin-bottom--lg">
+          <h3 id="latest">Next version (Unreleased)</h3>
+          <p>
+            Here you can find the documentation for work-in-process unreleased
+            version.
+          </p>
+          <table>
+            <tbody>
+              <tr>
+                <th>{latestVersion.label}</th>
+                <td>
+                  <Link to={latestVersion.path}>Documentation</Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
         {currentVersion !== latestVersion && (
           <div className="margin-bottom--lg">
             <h3 id="next">Next version (Unreleased)</h3>
@@ -69,7 +91,7 @@ function Version() {
         )}
         {pastVersions.length > 0 && (
           <div className="margin-bottom--lg">
-            <h3 id="archive">Past Versions</h3>
+            <h3 id="archive">Past versions (Not maintained anymore)</h3>
             <p>
               Here you can find documentation for previous versions of
               Docusaurus.
