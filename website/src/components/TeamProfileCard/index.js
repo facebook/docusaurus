2a3da27ef677a20815c42eb0804463b332953822
Advait73 <62119114+Advait73@users.noreply.github.com>
2020-10-16 19:15:23 +0200

    Rephrased TeamProfileCard as Class instead of Function

--- website/src/components/TeamProfileCard/index.js
+++ website/src/components/TeamProfileCard/index.js
@@ -5,45 +5,50 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { Component} from 'react'
 
-export default function TeamProfileCard({
-  className,
-  name,
-  children,
-  githubUrl,
-  twitterUrl,
-}) {
-  return (
-    <div className={className}>
-      <div className="card card--full-height">
-        <div className="card__header">
-          <div className="avatar avatar--vertical">
-            <img
-              className="avatar__photo avatar__photo--xl"
-              src={githubUrl + '.png'}
-            />
-            <div className="avatar__intro">
-              <h3 className="avatar__name">{name}</h3>
+class TeamProfileCard extends Component{
+    constructor(){
+        className,
+        name,
+        children,
+        githubUrl,
+        twitterUrl
+    }
+    render(){
+        
+        return (
+            <div className={className}>
+              <div className="card card--full-height">
+                <div className="card__header">
+                  <div className="avatar avatar--vertical">
+                    <img
+                      className="avatar__photo avatar__photo--xl"
+                      src={githubUrl + '.png'}
+                    />
+                    <div className="avatar__intro">
+                      <h3 className="avatar__name">{name}</h3>
+                    </div>
+                  </div>
+                </div>
+                <div className="card__body">{children}</div>
+                <div className="card__footer">
+                  <div className="button-group button-group--block">
+                    {githubUrl && (
+                      <a className="button button--secondary" href={githubUrl}>
+                        GitHub
+                      </a>
+                    )}
+                    {twitterUrl && (
+                      <a className="button button--secondary" href={twitterUrl}>
+                        Twitter
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="card__body">{children}</div>
-        <div className="card__footer">
-          <div className="button-group button-group--block">
-            {githubUrl && (
-              <a className="button button--secondary" href={githubUrl}>
-                GitHub
-              </a>
-            )}
-            {twitterUrl && (
-              <a className="button button--secondary" href={twitterUrl}>
-                Twitter
-              </a>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+          );
+    }
 }
+export default TeamProfileCard
