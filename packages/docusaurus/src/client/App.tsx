04e91f6ff2af442cc684bb49e8b422ea0a62094a
Jainam C Shah <jainamcshah98@gmail.com>
2020-10-19 20:10:40 +0530

    Add CSSLoadingWarningMessage to App in client

--- packages/docusaurus/src/client/App.tsx
+++ packages/docusaurus/src/client/App.tsx
@@ -6,6 +6,7 @@
  */
 
 import React, {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
 
 import routes from '@generated/routes';
 import siteConfig from '@generated/docusaurus.config';
@@ -13,9 +14,51 @@ import globalData from '@generated/globalData';
 import siteMetadata from '@generated/site-metadata';
 import renderRoutes from './exports/renderRoutes';
 import DocusaurusContext from './exports/context';
+import Head from './exports/Head';
 import PendingNavigation from './PendingNavigation';
 
 import './client-lifecycles-dispatcher';
+import './hideCSSLoadingWarning.css';
+
+function CSSLoadingWarningMessage() {
+  const {baseUrl} = siteConfig;
+  const {pathname} = useLocation();
+
+  if (pathname === baseUrl) {
+    return (
+      <>
+        <Head
+          script={[
+            {
+              type: 'text/javascript',
+              innerHTML: `
+              var baseUrlSuggestion = document.getElementById("css-didnt-load-baseurl-suggestion");
+              if (baseUrlSuggestion) {
+                baseUrlSuggestion.innerHTML = window.location.pathname;
+              }
+              `,
+            },
+          ]}
+        />
+        <div
+          className="css-loading-warning"
+          style={{border: 'solid red thick', padding: '16px'}}>
+          <span>
+            You site CSS did not load properly. Your baseUrl setting is probably
+            bad.{' '}
+          </span>
+          <span>
+            Maybe try baseUrl ={' '}
+            <span id="css-didnt-load-baseurl-suggestion">
+              {/* baseUrl suggestion message */}
+            </span>
+          </span>
+        </div>
+      </>
+    );
+  }
+  return null;
+}
 
 function App(): JSX.Element {
   const [isClient, setIsClient] = useState(false);
@@ -27,6 +70,7 @@ function App(): JSX.Element {
   return (
     <DocusaurusContext.Provider
       value={{siteConfig, siteMetadata, globalData, isClient}}>
+      <CSSLoadingWarningMessage />
       <PendingNavigation routes={routes}>
         {renderRoutes(routes)}
       </PendingNavigation>
