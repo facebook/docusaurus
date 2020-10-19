80f2dcd2bdf254391934f781f2b2c3b99edfc13e
Sam <sam@developersam.com>
2020-10-19 14:57:38 -0400

    fix(v2): Added back support for optional logo field in theme-classic navbarConfig

--- packages/docusaurus-theme-classic/src/validateThemeConfig.js
+++ packages/docusaurus-theme-classic/src/validateThemeConfig.js
@@ -248,7 +248,7 @@ const ThemeConfigSchema = Joi.object({
       srcDark: Joi.string(),
       href: Joi.string(),
       target: Joi.string(),
-    }),
+    }).default({}),
   }).default(DEFAULT_CONFIG.navbar),
   footer: Joi.object({
     style: Joi.string().equal('dark', 'light').default('light'),
