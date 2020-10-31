cf9cd2e67a69e0c7f5ec0fdb3231b372a0da57c1
Alexey Pyltsyn <lex61rus@gmail.com>
2020-10-31 19:29:58 +0300

    Verify boolean value

--- packages/docusaurus/src/server/configValidation.ts
+++ packages/docusaurus/src/server/configValidation.ts
@@ -40,7 +40,7 @@ export const DEFAULT_CONFIG: Pick<
 const PluginSchema = Joi.alternatives().try(
   Joi.string(),
   Joi.array().items(Joi.string().required(), Joi.object().required()).length(2),
-  Joi.bool(), // In case of conditional adding of plugins.
+  Joi.bool().equal(false), // In case of conditional adding of plugins.
 );
 
 const ThemeSchema = Joi.alternatives().try(
