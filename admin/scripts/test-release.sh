401a8679ba9da0ac691f57ee300427413886bc03
Alexey Pyltsyn <lex61rus@gmail.com>
2020-11-12 01:49:26 +0300

    feat(v2: allow init project via npm

--- admin/scripts/test-release.sh
+++ admin/scripts/test-release.sh
@@ -12,7 +12,7 @@ NEW_VERSION="$(node -p "require('./packages/docusaurus/package.json').version").
 CONTAINER_NAME="verdaccio"
 
 # Run Docker container with private npm registry Verdaccio
-docker run -d --rm --name "$CONTAINER_NAME" -p 4873:4873 -v "$PWD/admin/verdaccio.yaml":/verdaccio/conf/config.yaml verdaccio/verdaccio:4
+docker run -d --rm --name "$CONTAINER_NAME" -p 4873:4873 -v "$PWD/admin/verdaccio.yaml":/verdaccio/conf/config.yaml verdaccio/verdaccio:latest
 
 # Build packages
 yarn build:packages
@@ -24,7 +24,7 @@ npx --no-install lerna publish --yes --no-verify-access --no-git-reset --no-git-
 git diff --name-only -- '*.json' | sed 's, ,\\&,g' | xargs git checkout --
 
 # Build skeleton website with new version
-npm_config_registry="$CUSTOM_REGISTRY_URL" npx @docusaurus/init@"$NEW_VERSION" init test-website classic
+npm_config_registry="$CUSTOM_REGISTRY_URL" npx @docusaurus/init@"$NEW_VERSION" init test-website classic --use-npm
 
 # Stop Docker container
 if [[ -z "${KEEP_CONTAINER:-}" ]] && ( $(docker container inspect "$CONTAINER_NAME" > /dev/null 2>&1) ); then
