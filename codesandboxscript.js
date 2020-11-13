0f7529fad0144eb0422b4fb9d2a8a94260c88957
Samson Amaugo <sammychinedu2ky@gmail.com>
2020-11-13 09:38:48 +0100

    feat:added script to generate template for codesandbox

---
+++ codesandboxscript.js
@@ -0,0 +1,41 @@
+let {readFileSync, writeFileSync, copyFileSync, readdir} = require('fs');
+let {execSync} = require('child_process');
+// get the list of all available templates
+readdir('./packages/docusaurus-init/templates', (err, data) => {
+  let templates = data.filter((i) => i !== 'README.MD');
+  templates.forEach((template) => {
+    try {
+      console.log(
+        `generating ${template} template for codesandbox in the examples folder...`,
+      );
+      // run the docusaurus script to bootstrap the template in the examples folder
+      execSync(
+        `node ./packages/docusaurus-init/bin/index.js init examples/${template} ${template}`,
+      );
+      //read the content of the package.json
+      let getPackagedDotJson = readFileSync(
+        `examples/${template}/package.json`,
+      );
+      // parse it into an object
+      let toJSON = JSON.parse(getPackagedDotJson.toString());
+      // attach the dev script which would be used in code sandbox by default
+      toJSON.scripts.dev = 'docusaurus start';
+
+      //rewrite the package.json file with the new edit
+      writeFileSync(
+        `./examples/${template}/package.json`,
+        JSON.stringify(toJSON, null, 1),
+      );
+
+      /*copy the sandbox config file from the root of docusaurus to the root
+      of template in the examples folder*/
+      copyFileSync(
+        './sandbox.config.json',
+        `./examples/${template}/sandbox.config.json`,
+      );
+      console.log('done');
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
