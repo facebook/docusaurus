de87eb5ca099d76dc2702cd7352ae7f0a5dde405
Alexey Pyltsyn <lex61rus@gmail.com>
2020-11-17 13:25:29 +0300

    chore(v2): replace wait-file with wait-on to reuce npm warnings (#3765)

--- packages/docusaurus/src/webpack/plugins/WaitPlugin.js
+++ packages/docusaurus/src/webpack/plugins/WaitPlugin.js
@@ -7,7 +7,7 @@
 
 const path = require('path');
 const fs = require('fs-extra');
-const {waitFile} = require('wait-file');
+const waitOn = require('wait-on');
 
 class WaitPlugin {
   constructor(options) {
@@ -20,7 +20,7 @@ class WaitPlugin {
       // To prevent 'waitFile' error on waiting non-existing directory
       fs.ensureDir(path.dirname(this.filepath), () => {
         // Wait until file exist
-        waitFile({
+        waitOn({
           resources: [this.filepath],
           interval: 300,
         })
