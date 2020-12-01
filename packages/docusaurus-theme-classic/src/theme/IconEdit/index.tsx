5baa022300af023d3ad3dc2faca8db3cbb62bb2e
Bartosz Kaszubowski <gosimek@gmail.com>
2020-12-01 17:24:36 +0100

    apply review suggestion

--- packages/docusaurus-theme-classic/src/theme/IconEdit/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/IconEdit/index.tsx
@@ -6,9 +6,13 @@
  */
 
 import React from 'react';
-import {Props} from '@theme/IconArrow';
+import clsx from 'clsx';
 
-const IconArrow = (props: Props): JSX.Element => {
+import {Props} from '@theme/IconEdit';
+
+import styles from './styles.module.css';
+
+const IconEdit = ({className, ...restProps}: Props): JSX.Element => {
   return (
     <svg
       fill="currentColor"
@@ -17,11 +21,8 @@ const IconArrow = (props: Props): JSX.Element => {
       preserveAspectRatio="xMidYMid meet"
       role="img"
       viewBox="0 0 40 40"
-      style={{
-        marginRight: '0.3em',
-        verticalAlign: 'sub',
-      }}
-      {...props}>
+      className={clsx(styles.iconEdit, className)}
+      {...restProps}>
       <g>
         <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" />
       </g>
@@ -29,4 +30,4 @@ const IconArrow = (props: Props): JSX.Element => {
   );
 };
 
-export default IconArrow;
+export default IconEdit;
