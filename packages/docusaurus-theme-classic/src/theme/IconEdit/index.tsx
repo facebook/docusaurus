b8babe4f70b0cc9b79cfe7a4f57e1573bd95a0d8
Alexey Pyltsyn <lex61rus@gmail.com>
2020-12-21 19:29:05 +0300

    fix(v2): minor a11y tweaks

--- packages/docusaurus-theme-classic/src/theme/IconEdit/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/IconEdit/index.tsx
@@ -22,6 +22,7 @@ const IconEdit = ({className, ...restProps}: Props): JSX.Element => {
       role="img"
       viewBox="0 0 40 40"
       className={clsx(styles.iconEdit, className)}
+      aria-label="Edit page"
       {...restProps}>
       <g>
         <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" />
