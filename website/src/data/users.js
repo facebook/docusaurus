5ea6025add168da7e56ffec275ae252176746e34
Power Lin <me@wiki-power.com>
2021-01-05 20:24:06 +0800

    Update users.js

--- website/src/data/users.js
+++ website/src/data/users.js
@@ -27,15 +27,6 @@
 // prettier-ignore
 const users = [
   {
-    title: 'Power\'s Wiki',
-    description: 'An example of personal wiki ',
-    preview: require('./showcase/power.png'),
-    website: 'https://wiki-power.com/',
-    source: 'https://github.com/linyuxuanlin/Wiki_Docusaurus',
-    fbOpenSource: false,
-    pinned: false,
-  },
-  {
     title: 'AgileTs',
     description: 'Global State and Logic Framework for reactive Applications',
     preview: require('./showcase/agilets.png'),
@@ -252,6 +243,15 @@ const users = [
     preview: require('./showcase/oxidizer.png'),
     website: 'https://oxidizer.rs',
     source: 'https://github.com/oxidizer-rs/website',
+    fbOpenSource: false,
+    pinned: false,
+  },
+  {
+    title: 'Power\'s Wiki',
+    description: 'An example of personal wiki ',
+    preview: require('./showcase/power.png'),
+    website: 'https://wiki-power.com/',
+    source: 'https://github.com/linyuxuanlin/Wiki_Docusaurus',
     fbOpenSource: false,
     pinned: false,
   },
