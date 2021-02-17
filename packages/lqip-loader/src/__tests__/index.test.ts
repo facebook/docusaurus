4e4d612018ed075dbd6abe1b21b33610cb572b98
slorber <lorber.sebastien@gmail.com>
2021-02-17 15:28:14 +0100

    fix async test errors

--- packages/lqip-loader/src/__tests__/index.test.ts
+++ packages/lqip-loader/src/__tests__/index.test.ts
@@ -49,22 +49,19 @@ describe('lqip-loader', () => {
     const imgPath = path.join(__dirname, '__fixtures__', 'endi.jpg');
     const invalidPath = path.join(__dirname, '__fixtures__', 'docusaurus.svg');
 
-    it('should reject unknown or unsupported file format', () => {
-      expect(lqip.base64(invalidPath)).rejects.toBeTruthy();
+    it('should reject unknown or unsupported file format', async () => {
+      await expect(lqip.base64(invalidPath)).rejects.toBeTruthy();
     });
 
-    it('should generate a valid base64', () => {
+    it('should generate a valid base64', async () => {
       const expectedBase64 = 'data:image/jpeg;base64,/9j/2wBDA';
-      lqip.base64(imgPath).then((base64: string) => {
-        expect(base64).toContain(expectedBase64);
-      });
+      await expect(lqip.base64(imgPath)).resolves.toContain(expectedBase64);
     });
 
-    it('should generate a valid color palette', () => {
-      lqip.palette(imgPath).then((imgPalette: string[]) => {
-        expect(imgPalette).toHaveLength(6);
-        expect(imgPalette).toContain('#578ca1');
-      });
+    it('should generate a valid color palette', async () => {
+      const imgPalette = await lqip.palette(imgPath);
+      expect(imgPalette).toHaveLength(6);
+      expect(imgPalette).toContain('#578ca1');
     });
   });
 });
