1ffed82bc3636187f86e90341400a782865032fe
jhonebee <zhangjin19960908@gamil.com>
2021-02-26 17:56:22 +0800

    feat: 添加内容信息

--- packages/docusaurus-theme-live-codeblock/src/theme/Playground/index.js
+++ packages/docusaurus-theme-live-codeblock/src/theme/Playground/index.js
@@ -29,31 +29,48 @@ function IconCodeSquare(props) {
 }
 
 // 匹配对应的头部注释信息
-// function getHeaderNotesTxtToJSONObject (code) {
-//   const rules = /\*\*[\s\S.]+?\*\//g;
-//   let responJSON = {};
-//   const headerNotesTxt = rules.exec(code)?.[0];
-//   if (headerNotesTxt) {
-//     headerNotesTxt.split(/\n/).forEach((line) => {
-//       const key = /\s*\*\s*[a-zA-z0-9]+\s*:/.exec(line)?.[0]
-//       if (key) {
-//         const value = line.substr(key.length)
-//         if (key) {
-//           const trimKey = key.trim();
-//           responJSON[trimKey.substr(1, trimKey.length - 2).trim()] = value.trim();
-//         }
-//       }
-//     })
-//   }
-//   return responJSON;
-// }
+function getHeaderNotesTxtToJSONObject(codeInfo) {
+  const rules = /\/\*\*[\s\S.]+?\*\//g;
+  const responJSON = {};
+  const headerNotesTxt = rules.exec(codeInfo)?.[0];
+  let code = codeInfo;
+  let isHaveHeaderNotesTxt = false;
+  if (headerNotesTxt) {
+    headerNotesTxt.split(/\n/).forEach((line) => {
+      const key = /\s*\*\s*[a-zA-z0-9]+\s*:/.exec(line)?.[0];
+      if (key) {
+        const value = line.substr(key.length);
+        if (key) {
+          isHaveHeaderNotesTxt = true;
+          const trimKey = key.trim();
+          responJSON[
+            trimKey.substr(1, trimKey.length - 2).trim()
+          ] = value.trim();
+        }
+      }
+    });
+  }
+  if (isHaveHeaderNotesTxt) {
+    console.log(headerNotesTxt);
+    code = code.replace(headerNotesTxt, '');
+  }
+  return {
+    code,
+    isHaveHeaderNotesTxt,
+    params: responJSON,
+  };
+}
 
 export default function Playground({children, theme, transformCode, ...props}) {
-  // const params = getHeaderNotesTxtToJSONObject(children);
+  const {
+    params,
+    isHaveHeaderNotesTxt,
+    code: sourceCode,
+  } = getHeaderNotesTxtToJSONObject(children.replace(/\n$/, ''));
   const [isShowCode, setShowCode] = React.useState(false);
   return (
     <LiveProvider
-      code={children.replace(/\n$/, '')}
+      code={sourceCode}
       transformCode={transformCode || ((code) => `${code};`)}
       theme={theme}
       {...props}>
@@ -61,6 +78,12 @@ export default function Playground({children, theme, transformCode, ...props}) {
         <div className={clsx(styles.playgroundLivePreview)}>
           <LivePreview />
         </div>
+        {isHaveHeaderNotesTxt ? (
+          <div className={clsx(styles.playgroundMate)}>
+            <span>{params.title}</span>
+            <div>{params.desc}</div>
+          </div>
+        ) : undefined}
         <div className={clsx(styles.playgroundToolbar)}>
           <IconCodeSquare
             onClick={() => {
