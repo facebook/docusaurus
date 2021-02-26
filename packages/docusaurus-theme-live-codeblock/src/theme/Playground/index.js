e3e564b9bf28e73cf6bfb2c40362df2dc0d467e1
jhonebee <zhangjin19960908@gamil.com>
2021-02-26 15:55:13 +0800

    fix: 修复icon鼠标样式

--- packages/docusaurus-theme-live-codeblock/src/theme/Playground/index.js
+++ packages/docusaurus-theme-live-codeblock/src/theme/Playground/index.js
@@ -20,7 +20,7 @@ function IconCodeSquare(props) {
       width="16"
       height="16"
       fill="currentColor"
-      className="bi bi-code-slash"
+      className={clsx('bi', 'bi-code-slash', styles.playgroundIconCodeSquare)}
       viewBox="0 0 16 16"
       {...props}>
       <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z" />
