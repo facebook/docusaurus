31b69c51bf80a66cad06f78572a07aac463b5717
Armano <armano2@users.noreply.github.com>
2021-03-20 04:55:45 +0100

    refactor(v2): migrate log plugin to typescript and let it print warnings

---
+++ packages/docusaurus/src/webpack/plugins/LogPlugin.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import WebpackBar from 'webpackbar';
+import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages';
+import type {Compiler, Plugin} from 'webpack';
+
+class LogPlugin extends WebpackBar implements Plugin {
+  apply(compiler: Compiler): void {
+    super.apply(compiler);
+
+    compiler.hooks.done.tap('WebpackNiceLog', (stats) => {
+      if (stats.hasErrors() || stats.hasWarnings()) {
+        const messages = formatWebpackMessages(
+          stats.toJson({
+            all: false,
+            warnings: true,
+            errors: true,
+          }),
+        );
+        if (messages.errors.length) {
+          console.error(`\n\n${messages.errors.join('')}`);
+        }
+        if (messages.warnings.length) {
+          console.warn(`\n\n${messages.warnings.join('')}`);
+        }
+      }
+    });
+  }
+}
+
+export default LogPlugin;
