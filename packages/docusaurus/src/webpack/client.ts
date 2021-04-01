4bc8f971c4fa7b57a96369bb4183bfa76a94f65d
slorber <lorber.sebastien@gmail.com>
2021-04-01 14:59:03 +0200

    rename webpackHotDevClient

--- packages/docusaurus/src/webpack/client.ts
+++ packages/docusaurus/src/webpack/client.ts
@@ -30,7 +30,8 @@ export default function createClientConfig(
       // like CRA to bring better experience.
       // note: the one in ./dev is modified to work with Docusaurus
       // !isProd && require.resolve('react-dev-utils/hotDevServer.js'),
-      !isProd && require.resolve('./react-dev-utils-webpack5/hotDevServer.js'),
+      !isProd &&
+        require.resolve('./react-dev-utils-webpack5/webpackHotDevClient.js'),
       path.resolve(__dirname, '../client/clientEntry.js'),
     ].filter(Boolean) as string[],
     optimization: {
