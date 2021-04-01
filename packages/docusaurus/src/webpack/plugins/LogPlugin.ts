48c03ba34a6f78cb9e58a7ebea03b3941913403f
slorber <lorber.sebastien@gmail.com>
2021-04-01 15:39:52 +0200

    revert LogPlugin adapter

--- packages/docusaurus/src/webpack/plugins/LogPlugin.ts
+++ packages/docusaurus/src/webpack/plugins/LogPlugin.ts
@@ -5,25 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 import WebpackBar from 'webpackbar';
-import {Compiler, StatsCompilation} from 'webpack';
-import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages';
-
-// react-dev-utils/formatWebpackMessages does not support webpack 5 yet
-// TODO remove once react-dev-utils support Webpack 5!
-// see https://github.com/facebook/create-react-app/issues/9994
-// TODO: is using react-dev-utils/formatWebpackMessages really useful?
-export function formatWebpackMessagesWebpack5(stats?: StatsCompilation) {
-  function convertStatErrors(statsErrors?: StatsCompilation['errors']) {
-    return statsErrors
-      ?.map((e) => e.message)
-      .filter((message) => message !== undefined) as readonly string[];
-  }
-
-  return formatWebpackMessages({
-    errors: convertStatErrors(stats?.errors),
-    warnings: convertStatErrors(stats?.errors),
-  });
-}
+import {Compiler} from 'webpack';
+// import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages';
+import formatWebpackMessages from '../react-dev-utils-webpack5/formatWebpackMessages';
 
 function showError(arr) {
   console.log(`\n\n${arr.join('')}`);
@@ -36,9 +20,7 @@ export default class LogPlugin extends WebpackBar {
     // TODO can't this be done in compile(configs) alongside the warnings???
     compiler.hooks.done.tap('DocusaurusLogPlugin', (stats) => {
       if (stats.hasErrors()) {
-        const messages = formatWebpackMessagesWebpack5(
-          stats.toJson('errors-only'),
-        );
+        const messages = formatWebpackMessages(stats.toJson('errors-only'));
         if (messages.errors.length) {
           showError(messages.errors);
         }
