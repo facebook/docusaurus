3d558639814a9b1ddbe96e20f5937cdd4ad5908a
slorber <lorber.sebastien@gmail.com>
2021-04-10 19:16:09 +0200

    ability to extract number prefix

--- packages/docusaurus-plugin-content-docs/src/__tests__/numberPrefix.test.ts
+++ packages/docusaurus-plugin-content-docs/src/__tests__/numberPrefix.test.ts
@@ -5,7 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {stripNumberPrefix, stripPathNumberPrefixes} from '../numberPrefix';
+import {
+  extractNumberPrefix,
+  stripNumberPrefix,
+  stripPathNumberPrefixes,
+} from '../numberPrefix';
+
+const BadNumberPrefixPatterns = [
+  'a1-My Doc',
+  'My Doc-000',
+  '00abc01-My Doc',
+  'My 001- Doc',
+  'My -001 Doc',
+];
 
 describe('stripNumberPrefix', () => {
   test('should strip number prefix if present', () => {
@@ -39,15 +51,7 @@ describe('stripNumberPrefix', () => {
   });
 
   test('should not strip number prefix if pattern does not match', () => {
-    const badPatterns = [
-      'a1-My Doc',
-      'My Doc-000',
-      '00abc01-My Doc',
-      'My 001- Doc',
-      'My -001 Doc',
-    ];
-
-    badPatterns.forEach((badPattern) => {
+    BadNumberPrefixPatterns.forEach((badPattern) => {
       expect(stripNumberPrefix(badPattern)).toEqual(badPattern);
     });
   });
@@ -60,5 +64,48 @@ describe('stripPathNumberPrefix', () => {
         '0-MyRootFolder0/1 - MySubFolder1/2.  MyDeepFolder2/3 _MyDoc3',
       ),
     ).toEqual('MyRootFolder0/MySubFolder1/MyDeepFolder2/MyDoc3');
+  });
+});
+
+describe('extractNumberPrefix', () => {
+  test('should extract number prefix if present', () => {
+    expect(extractNumberPrefix('1-My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 1,
+    });
+    expect(extractNumberPrefix('01-My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 1,
+    });
+    expect(extractNumberPrefix('001-My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 1,
+    });
+    expect(extractNumberPrefix('001 - My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 1,
+    });
+    expect(extractNumberPrefix('001      -    My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 1,
+    });
+    expect(extractNumberPrefix('999      -        My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 999,
+    });
+
+    expect(extractNumberPrefix('0046036 - My Doc')).toEqual({
+      filename: 'My Doc',
+      numberPrefix: 46036,
+    });
+  });
+
+  test('should not extract number prefix if pattern does not match', () => {
+    BadNumberPrefixPatterns.forEach((badPattern) => {
+      expect(extractNumberPrefix(badPattern)).toEqual({
+        filename: badPattern,
+        numberPrefix: undefined,
+      });
+    });
   });
 });
--- packages/docusaurus-plugin-content-docs/src/numberPrefix.ts
+++ packages/docusaurus-plugin-content-docs/src/numberPrefix.ts
@@ -5,14 +5,31 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+const NumberPrefixRegex = /^(?<numberPrefix>\d+)(?<separator>\s*[-_.]+\s*)(?<suffix>.*)$/;
+
 // 0-myDoc => myDoc
 export function stripNumberPrefix(str: string) {
-  const numberPrefixPattern = /(?:^(\d)+(\s)*([-_.])+(\s)*)(?<suffix>.*)/;
-  const result = numberPrefixPattern.exec(str);
-  return result?.groups?.suffix ?? str;
+  return NumberPrefixRegex.exec(str)?.groups?.suffix ?? str;
 }
 
 // 0-myFolder/0-mySubfolder/0-myDoc => myFolder/mySubfolder/myDoc
 export function stripPathNumberPrefixes(path: string) {
   return path.split('/').map(stripNumberPrefix).join('/');
+}
+
+// 0-myDoc => {filename: myDoc, numberPrefix: 0}
+// 003 - myDoc => {filename: myDoc, numberPrefix: 3}
+export function extractNumberPrefix(
+  filename: string,
+): {filename: string; numberPrefix?: number} {
+  const match = NumberPrefixRegex.exec(filename);
+  const cleanFileName = match?.groups?.suffix ?? filename;
+  const numberPrefixString = match?.groups?.numberPrefix;
+  const numberPrefix = numberPrefixString
+    ? parseInt(numberPrefixString, 10)
+    : undefined;
+  return {
+    filename: cleanFileName,
+    numberPrefix,
+  };
 }
