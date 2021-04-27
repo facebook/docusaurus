3f4757569d5b0912e1ddefe7ebb1a3421658c249
slorber <lorber.sebastien@gmail.com>
2021-04-27 12:02:13 +0200

    accept empty/null custom_edit_url docs frontmatter

--- packages/docusaurus-plugin-content-docs/src/__tests__/docFrontMatter.test.ts
+++ packages/docusaurus-plugin-content-docs/src/__tests__/docFrontMatter.test.ts
@@ -34,4 +34,15 @@ describe('validateDocFrontMatter', () => {
     const frontMatter: DocFrontMatter = {description: ''};
     expect(validateDocFrontMatter(frontMatter)).toEqual(frontMatter);
   });
+
+  test('accept null custom_edit_url', () => {
+    const frontMatter: DocFrontMatter = {custom_edit_url: null};
+    expect(validateDocFrontMatter(frontMatter)).toEqual(frontMatter);
+  });
+
+  // See https://github.com/demisto/content-docs/pull/616#issuecomment-827087566
+  test('accept empty custom_edit_url', () => {
+    const frontMatter: DocFrontMatter = {custom_edit_url: ''};
+    expect(validateDocFrontMatter(frontMatter)).toEqual(frontMatter);
+  });
 });
