c6cd7bfe3f9f2d1d1a904ab892b397d979751656
slorber <lorber.sebastien@gmail.com>
2021-06-24 12:24:12 +0200

    Add tests for webpack aliases

--- packages/docusaurus/src/webpack/__tests__/base.test.ts
+++ packages/docusaurus/src/webpack/__tests__/base.test.ts
@@ -16,7 +16,7 @@ import {
 import * as utils from '../utils';
 import {mapValues} from 'lodash';
 import {posixPath} from '@docusaurus/utils';
-import {Props} from '@docusaurus/types';
+import {Props, ThemeAliases} from '@docusaurus/types';
 
 describe('babel transpilation exclude logic', () => {
   test('always transpile client dir files', () => {
@@ -81,7 +81,7 @@ describe('getDocusaurusAliases()', () => {
 describe('base webpack config', () => {
   const props: Props = {
     outDir: '',
-    siteDir: '',
+    siteDir: path.resolve(__dirname, '__fixtures__', 'base_test_site'),
     siteConfig: {},
     baseUrl: '',
     generatedFilesDir: '',
@@ -93,14 +93,32 @@ describe('base webpack config', () => {
       docusaurusVersion: '2.0.0-alpha.70',
     },
     plugins: [
-      function () {
-        return {};
+      {
+        getThemePath() {
+          return path.resolve(
+            __dirname,
+            '__fixtures__',
+            'base_test_site',
+            'pluginThemeFolder',
+          );
+        },
       },
     ],
   };
 
   afterEach(() => {
     jest.restoreAllMocks();
+  });
+
+  test('should create webpack aliases', () => {
+    // @ts-expect-error: Docusaurus webpack alias is always an object
+    const aliases: ThemeAliases =
+      createBaseConfig(props, true).resolve?.alias ?? {};
+    // Make aliases relative so that test work on all computers
+    const relativeAliases = mapValues(aliases, (a) =>
+      path.relative(props.siteDir, a),
+    );
+    expect(relativeAliases).toMatchSnapshot();
   });
 
   test('should use svg rule', () => {
