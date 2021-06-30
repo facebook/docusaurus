068dd3f013e4a7e43018b0048987f187a70f8342
Josh-Cena <sidachen2003@gmail.com>
2021-06-30 08:17:35 +0800

    Revert (to test)
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

--- packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -16,7 +16,7 @@ import type {
   DesktopOrMobileNavBarItemProps,
   Props,
 } from '@theme/NavbarItem/DefaultNavbarItem';
-import NavbarItem from '@theme/NavbarItem';
+// import NavbarItem from '@theme/NavbarItem';
 import IconExternalLink from '@theme/IconExternalLink';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 
@@ -136,7 +136,7 @@ function NavItemDesktop({
       <ul ref={dropdownMenuRef} className="dropdown__menu">
         {items.map(({className: childItemClassName, ...childItemProps}, i) => (
           <li key={i}>
-            <NavbarItem
+            <NavLink
               {...childItemProps}
               onKeyDown={(e) => {
                 if (i === items.length - 1 && e.key === 'Tab') {
@@ -220,7 +220,7 @@ function NavItemMobile({
         style={{height: collapsed ? undefined : menuListHeight}}>
         {items.map(({className: childItemClassName, ...childItemProps}, i) => (
           <li className="menu__list-item" key={i}>
-            <NavbarItem
+            <NavLink
               {...childItemProps}
               activeClassName="menu__link--active"
               className={navLinkClassNames(childItemClassName)}
