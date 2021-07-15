d81436e9848d4756bd93e507241cb1918d90cbf1
Alexey Pyltsyn <lex61rus@gmail.com>
2021-07-15 16:38:29 +0300

    Remove extra attrs

--- packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -72,6 +72,8 @@ export function NavLink({
 function DefaultNavbarItemDesktop({
   className,
   isDropdownItem = false,
+  type: _type,
+  position: _position,
   ...props
 }: DesktopOrMobileNavBarItemProps) {
   let element = (
