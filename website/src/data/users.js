cda09237cc4cb2e2cc36c723242b22dc075f9e12
François Massot <francois.massot@gmail.com>
2021-07-16 11:44:32 +0200

    docs(v2): Add quickwit to user.js and png. (#5187)
    * Add quickwit to user.js and png.
    
    * Update website/src/data/users.js
    
    Co-authored-by: Sébastien Lorber <slorber@users.noreply.github.com>

--- website/src/data/users.js
+++ website/src/data/users.js
@@ -1218,6 +1218,14 @@ const Users = [
     source: 'https://github.com/zowe/docs-site',
     tags: ['opensource', 'product', 'large', 'design', 'versioning']
   },
+  {
+    title: 'Quickwit',
+    description: 'The open source search engine on object storage with subsecond latency',
+    preview: require('./showcase/quickwit.png'),
+    website: 'https://quickwit.io',
+    source: 'https://github.com/quickwit-inc/quickwit',
+    tags: ['favorite', 'opensource', 'design']
+  }
 ];
 
 export const TagList = Object.keys(Tags);
