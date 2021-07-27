053bdf1b05dfb63d3860e90f92441bc4e9c1bcc9
slorber <lorber.sebastien@gmail.com>
2021-07-27 15:11:09 +0200

    slightly change details/summary design

--- packages/docusaurus-theme-classic/src/theme/Details/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Details/index.tsx
@@ -11,6 +11,7 @@ import {Details as DetailsGeneric} from '@docusaurus/theme-common';
 import type {Props} from '@theme/Details';
 import styles from './styles.module.css';
 
+// Should we have a custom details/summary comp in Infima instead of reusing alert classes?
 const InfimaClasses = 'alert alert--info';
 
 export default function Details({...props}: Props): JSX.Element {
