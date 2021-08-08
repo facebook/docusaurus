acfc337e10ba62c6926400cc6473243ebc80d757
Alexey Pyltsyn <lex61rus@gmail.com>
2021-08-08 12:40:54 +0300

    fix: unbreak highlighting regular navbar links

--- packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ packages/docusaurus-theme-classic/src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -16,6 +16,7 @@ import type {
 } from '@theme/NavbarItem/DefaultNavbarItem';
 import IconExternalLink from '@theme/IconExternalLink';
 import isInternalUrl from '@docusaurus/isInternalUrl';
+import {getInfimaActiveClassName} from './index';
 
 const dropdownLinkActiveClass = 'dropdown__link--active';
 
@@ -111,7 +112,7 @@ function DefaultNavbarItem({
   ...props
 }: Props): JSX.Element {
   const Comp = mobile ? DefaultNavbarItemMobile : DefaultNavbarItemDesktop;
-  return <Comp {...props} />;
+  return <Comp {...props} activeClassName={getInfimaActiveClassName(mobile)} />;
 }
 
 export default DefaultNavbarItem;
