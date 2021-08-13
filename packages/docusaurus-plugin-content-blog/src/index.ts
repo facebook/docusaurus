dd933f538b60a35044b1957b07a501faee0980f8
Josh-Cena <sidachen2003@gmail.com>
2021-08-13 19:17:45 +0800

    Guard against zero posts
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

--- packages/docusaurus-plugin-content-blog/src/index.ts
+++ packages/docusaurus-plugin-content-blog/src/index.ts
@@ -150,7 +150,8 @@ export default function pluginContentBlog(
       const totalCount = blogPosts.length;
       const postsPerPage =
         postsPerPageOption === 'ALL' ? totalCount : postsPerPageOption;
-      const numberOfPages = Math.ceil(totalCount / postsPerPage);
+      const numberOfPages =
+        postsPerPage === 0 ? 1 : Math.ceil(totalCount / postsPerPage);
       const {
         siteConfig: {baseUrl = ''},
       } = context;
