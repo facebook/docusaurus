2c2113820b8606912a203f9275e27f0bf7a4db2d
slorber <lorber.sebastien@gmail.com>
2021-08-18 17:30:24 +0200

    fix tests

--- packages/docusaurus-utils/src/tags.ts
+++ packages/docusaurus-utils/src/tags.ts
@@ -26,7 +26,11 @@ export function normalizeFrontMatterTag(
     };
   }
 
+  // TODO maybe make ensure the permalink is valid url path?
   function normalizeTagPermalink(permalink: string): string {
+    // note: we always apply tagsPath on purpose
+    // for versioned docs, v1/doc.md and v2/doc.md tags with custom permalinks don't lead to the same created page
+    // tagsPath is different for each doc version
     return normalizeUrl([tagsPath, permalink]);
   }
 
