b9266ad4d079178ef5cbb4b015993a166811a3be
Erick Zhao <erick@hotmail.ca>
2021-09-16 10:46:54 -0700

    feat: add all TOC levels to MDX loader

--- packages/docusaurus-mdx-loader/src/remark/toc/search.ts
+++ packages/docusaurus-mdx-loader/src/remark/toc/search.ts
@@ -8,40 +8,83 @@
 import toString from 'mdast-util-to-string';
 import visit, {Visitor} from 'unist-util-visit';
 import {toValue} from '../utils';
-import type {TOCItem as TOC} from '@docusaurus/types';
+import type {TOCItem} from '@docusaurus/types';
 import type {Node} from 'unist';
 import type {Heading} from 'mdast';
 
-// Visit all headings. We `slug` all headings (to account for
-// duplicates), but only take h2 and h3 headings.
-export default function search(node: Node): TOC[] {
-  const headings: TOC[] = [];
-  let current = -1;
-  let currentDepth = 0;
+// Intermediate interface for TOC algorithm
+interface SearchItem {
+  node: TOCItem;
+  level: number;
+  parentIndex: number;
+}
+
+/**
+ *
+ * Generate a TOC AST from the raw Markdown contents
+ */
+export default function search(node: Node): TOCItem[] {
+  const headings: SearchItem[] = [];
 
   const visitor: Visitor<Heading> = (child, _index, parent) => {
     const value = toString(child);
 
-    if (parent !== node || !value || child.depth > 3 || child.depth < 2) {
+    // depth:1 headings are titles and not included in the TOC
+    if (parent !== node || !value || child.depth < 2) {
       return;
     }
 
-    const entry: TOC = {
-      value: toValue(child),
-      id: child.data!.id as string,
-      children: [],
-    };
-
-    if (!headings.length || currentDepth >= child.depth) {
-      headings.push(entry);
-      current += 1;
-      currentDepth = child.depth;
-    } else {
-      headings[current].children.push(entry);
-    }
+    headings.push({
+      node: {
+        value: toValue(child),
+        id: child.data!.id as string,
+        children: [],
+      },
+      level: child.depth,
+      parentIndex: -1,
+    });
   };
 
   visit(node, 'heading', visitor);
 
-  return headings;
+  const getParentIndex = (prevParentIndex: number, currLevel: number) => {
+    let parentIndex = prevParentIndex;
+    // We start at the parent of the previous heading
+    // Recurse through its ancestors until the current heading would be a child
+    while (parentIndex >= 0 && currLevel < headings[parentIndex].level) {
+      parentIndex = headings[parentIndex].parentIndex;
+    }
+    return parentIndex >= 0 ? headings[parentIndex].parentIndex : parentIndex;
+  };
+
+  // Assign the correct `parentIndex` for each heading.
+  headings.forEach((curr, currIndex) => {
+    if (currIndex > 0) {
+      const prevIndex = currIndex - 1;
+      const prev = headings[prevIndex];
+      if (curr.level > prev.level) {
+        curr.parentIndex = prevIndex;
+      } else if (curr.level < prev.level) {
+        curr.parentIndex = getParentIndex(prev.parentIndex, curr.level);
+      } else {
+        curr.parentIndex = prev.parentIndex;
+      }
+    }
+  });
+
+  const rootNodeIndexes: number[] = [];
+
+  // For a given parentIndex, add each Node into that parent's `children` array
+  headings.forEach((heading, i) => {
+    if (heading.parentIndex >= 0) {
+      headings[heading.parentIndex].node.children.push(heading.node);
+    } else {
+      rootNodeIndexes.push(i);
+    }
+  });
+
+  const toc = headings
+    .filter((_, k) => rootNodeIndexes.includes(k)) // only return root nodes
+    .map((heading) => heading.node); // only return Node, no metadata
+  return toc;
 }
