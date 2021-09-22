2ef70cb80671fecac16d318e0c19bf4bb09ffcf2
Joshua Chen <sidachen2003@gmail.com>
2021-09-22 17:09:52 +0800

    refactor(preset-classic): migrate preset-classic to TypeScript (#5579)

--- packages/docusaurus-plugin-content-blog/src/types.ts
+++ packages/docusaurus-plugin-content-blog/src/types.ts
@@ -31,7 +31,7 @@ export type EditUrlFunction = (editUrlParams: {
   locale: string;
 }) => string | undefined;
 
-export interface PluginOptions extends RemarkAndRehypePluginOptions {
+export type PluginOptions = RemarkAndRehypePluginOptions & {
   id?: string;
   path: string;
   routeBasePath: string;
@@ -61,7 +61,7 @@ export interface PluginOptions extends RemarkAndRehypePluginOptions {
   editLocalizedFiles?: boolean;
   admonitions: Record<string, unknown>;
   authorsMapPath: string;
-}
+};
 
 export interface BlogTags {
   [key: string]: BlogTag;
