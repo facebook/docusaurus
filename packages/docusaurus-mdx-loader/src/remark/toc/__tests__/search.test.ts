d3fe29f8be0de90478630a9afc88193caca7e2c9
slorber <lorber.sebastien@gmail.com>
2021-09-24 17:20:25 +0200

    toc search: add real-world test

--- packages/docusaurus-mdx-loader/src/remark/toc/__tests__/search.test.ts
+++ packages/docusaurus-mdx-loader/src/remark/toc/__tests__/search.test.ts
@@ -31,43 +31,157 @@ test('should process all heading levels', async () => {
 ###### Foxtrot
 
   `;
-  const result = await getHeadings(md);
-  expect(result).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "children": Array [
-          Object {
-            "children": Array [
-              Object {
-                "children": Array [
-                  Object {
-                    "children": Array [
-                      Object {
-                        "children": Array [],
-                        "id": "foxtrot",
-                        "level": 6,
-                        "value": "Foxtrot",
-                      },
-                    ],
-                    "id": "echo",
-                    "level": 5,
-                    "value": "Echo",
-                  },
-                ],
-                "id": "delta",
-                "level": 4,
-                "value": "Delta",
-              },
-            ],
-            "id": "charlie",
-            "level": 3,
-            "value": "Charlie",
-          },
-        ],
-        "id": "bravo",
-        "level": 2,
-        "value": "Bravo",
-      },
-    ]
-  `);
+
+  expect(await getHeadings(md)).toEqual([
+    {
+      children: [
+        {
+          children: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      children: [],
+                      id: 'foxtrot',
+                      level: 6,
+                      value: 'Foxtrot',
+                    },
+                  ],
+                  id: 'echo',
+                  level: 5,
+                  value: 'Echo',
+                },
+              ],
+              id: 'delta',
+              level: 4,
+              value: 'Delta',
+            },
+          ],
+          id: 'charlie',
+          level: 3,
+          value: 'Charlie',
+        },
+      ],
+      id: 'bravo',
+      level: 2,
+      value: 'Bravo',
+    },
+  ]);
+});
+
+test('should process real-world well-formatted md', async () => {
+  const md = `
+# title
+
+some text
+
+## section 1
+
+some text
+
+### subsection 1-1
+
+some text
+
+#### subsection 1-1-1
+
+some text
+
+#### subsection 1-1-2
+
+some text
+
+### subsection 1-1
+
+some text
+
+### subsection 1-2
+
+some text
+
+## section 2
+
+some text
+
+### subsection 2-1
+
+some text
+
+### subsection 2-1
+
+some text
+
+## section 3
+
+some text
+
+### subsection 3-1
+
+some text
+
+### subsection 3-2
+
+some text
+
+  `;
+
+  expect(await getHeadings(md)).toEqual([
+    {
+      children: [
+        {
+          children: [
+            {
+              children: [],
+              id: 'subsection-1-1-1',
+              level: 4,
+              value: 'subsection 1-1-1',
+            },
+            {
+              children: [],
+              id: 'subsection-1-1-2',
+              level: 4,
+              value: 'subsection 1-1-2',
+            },
+          ],
+          id: 'subsection-1-1',
+          level: 3,
+          value: 'subsection 1-1',
+        },
+        {
+          children: [],
+          id: 'subsection-1-1-3',
+          level: 3,
+          value: 'subsection 1-1',
+        },
+        {children: [], id: 'subsection-1-2', level: 3, value: 'subsection 1-2'},
+      ],
+      id: 'section-1',
+      level: 2,
+      value: 'section 1',
+    },
+    {
+      children: [
+        {children: [], id: 'subsection-2-1', level: 3, value: 'subsection 2-1'},
+        {
+          children: [],
+          id: 'subsection-2-1-1',
+          level: 3,
+          value: 'subsection 2-1',
+        },
+      ],
+      id: 'section-2',
+      level: 2,
+      value: 'section 2',
+    },
+    {
+      children: [
+        {children: [], id: 'subsection-3-1', level: 3, value: 'subsection 3-1'},
+        {children: [], id: 'subsection-3-2', level: 3, value: 'subsection 3-2'},
+      ],
+      id: 'section-3',
+      level: 2,
+      value: 'section 3',
+    },
+  ]);
 });
