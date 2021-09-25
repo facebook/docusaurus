ced292ba552ed2250c9394d92c1673d0a3d50ec3
Josh-Cena <sidachen2003@gmail.com>
2021-09-25 10:19:21 +0800

    Bump deps

--- packages/docusaurus-init/bin/index.js
+++ packages/docusaurus-init/bin/index.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-
-// TODO remove when fixed: https://github.com/Stuk/eslint-plugin-header/issues/39
-/* eslint-disable header/header */
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
