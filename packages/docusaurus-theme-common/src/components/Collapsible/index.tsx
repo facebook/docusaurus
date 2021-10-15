2a6f96879b256eaeaac790f6eeee501040683dd7
Alexey Pyltsyn <lex61rus@gmail.com>
2021-10-15 15:50:52 +0300

    Restore original solution

--- packages/docusaurus-theme-common/src/components/Collapsible/index.tsx
+++ packages/docusaurus-theme-common/src/components/Collapsible/index.tsx
@@ -134,11 +134,8 @@ function useCollapseAnimation({
         // When expanding
         else {
           el.style.display = 'block';
-          // Calculate height including the scrollbar
-          el.style.overflowY = 'scroll';
           requestAnimationFrame(() => {
             applyTransitionStyles();
-            el.style.overflow = CollapsedStyles.overflow;
           });
         }
       });
@@ -200,21 +197,8 @@ function CollapsibleBase({
           return;
         }
 
-        const el = collapsibleRef.current!;
-        const currentCollapsibleElementHeight = el.style.height;
-
-        if (
-          !collapsed &&
-          parseInt(currentCollapsibleElementHeight, 10) === el.scrollHeight
-        ) {
-          applyCollapsedStyle(el, false);
-          onCollapseTransitionEnd?.(false);
-        }
-
-        if (currentCollapsibleElementHeight === CollapsedStyles.height) {
-          applyCollapsedStyle(el, true);
-          onCollapseTransitionEnd?.(true);
-        }
+        applyCollapsedStyle(collapsibleRef.current!, collapsed);
+        onCollapseTransitionEnd?.(collapsed);
       }}
       className={className}>
       {children}
