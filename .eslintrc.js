90db3b4c013f2bd1f8fb17e816ecb870c3cc7eeb
slorber <lorber.sebastien@gmail.com>
2021-10-15 14:52:05 +0200

    Enable ESLint rules of hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
     'airbnb',
     'prettier',
     'prettier/react',
