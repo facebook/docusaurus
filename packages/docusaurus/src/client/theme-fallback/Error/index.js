7daae1800d45fc2463082c3871a2058658448c2c
sebastienlorber <lorber.sebastien@gmail.com>
2021-11-04 15:36:09 +0100

    extra error boundary should recover the original screen, not the crashing error display

--- packages/docusaurus/src/client/theme-fallback/Error/index.js
+++ packages/docusaurus/src/client/theme-fallback/Error/index.js
@@ -34,7 +34,9 @@ function Error({error, tryAgain}) {
   // We wrap the error in its own error boundary because the layout can actually throw too...
   // Only the ErrorDisplay component is simple enough to be considered safe to never throw
   return (
-    <ErrorBoundary fallback={ErrorDisplay}>
+    <ErrorBoundary
+      // Note: we display the original error here, not the error that we captured in this extra error boundary
+      fallback={() => <ErrorDisplay error={error} tryAgain={tryAgain} />}>
       <Layout title="Page Error">
         <ErrorDisplay error={error} tryAgain={tryAgain} />
       </Layout>
