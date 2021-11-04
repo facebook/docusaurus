fa6d15b35f34e3c4a926f334d863c1770e214ecf
Paden Clayton <padenc2001@gmail.com>
2021-11-04 10:07:32 -0500

    feat(core): Add React ErrorBoundary component + theme default boundaries (#3104)
    Co-authored-by: Paden Clayton <paden.clayton@monkedia.com>
    Co-authored-by: Josh-Cena <sidachen2003@gmail.com>
    Co-authored-by: sebastienlorber <lorber.sebastien@gmail.com>

--- packages/docusaurus/src/client/App.tsx
+++ packages/docusaurus/src/client/App.tsx
@@ -11,24 +11,28 @@ import routes from '@generated/routes';
 import renderRoutes from './exports/renderRoutes';
 import {BrowserContextProvider} from './exports/browserContext';
 import {DocusaurusContextProvider} from './exports/docusaurusContext';
+import ErrorBoundary from '@docusaurus/ErrorBoundary';
 import PendingNavigation from './PendingNavigation';
 import BaseUrlIssueBanner from './baseUrlIssueBanner/BaseUrlIssueBanner';
 import Root from '@theme/Root';
+import Error from '@theme/Error';
 
 import './client-lifecycles-dispatcher';
 
 function App(): JSX.Element {
   return (
-    <DocusaurusContextProvider>
-      <BrowserContextProvider>
-        <Root>
-          <BaseUrlIssueBanner />
-          <PendingNavigation routes={routes} delay={1000}>
-            {renderRoutes(routes)}
-          </PendingNavigation>
-        </Root>
-      </BrowserContextProvider>
-    </DocusaurusContextProvider>
+    <ErrorBoundary fallback={Error}>
+      <DocusaurusContextProvider>
+        <BrowserContextProvider>
+          <Root>
+            <BaseUrlIssueBanner />
+            <PendingNavigation routes={routes} delay={1000}>
+              {renderRoutes(routes)}
+            </PendingNavigation>
+          </Root>
+        </BrowserContextProvider>
+      </DocusaurusContextProvider>
+    </ErrorBoundary>
   );
 }
 
