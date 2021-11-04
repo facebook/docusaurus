2fa4a1ccba32f28c86e205f44e99e2bc15642852
sebastienlorber <lorber.sebastien@gmail.com>
2021-11-04 15:25:28 +0100

    improve ErrorBoundaryTests page

--- website/_dogfooding/_pages tests/error-boundary-tests.js
+++ website/_dogfooding/_pages tests/error-boundary-tests.js
@@ -3,15 +3,19 @@ import Layout from '@theme/Layout';
 
 import ErrorBoundaryTestButton from '@site/src/components/ErrorBoundaryTestButton';
 
-export default function Home() {
+export default function ErrorBoundaryTests() {
   return (
     <>
       <ErrorBoundaryTestButton>Crash outside layout</ErrorBoundaryTestButton>
       <Layout>
-        <h1>Error boundary tests</h1>
-        <div>
-          <ErrorBoundaryTestButton>Crash inside layout</ErrorBoundaryTestButton>
-        </div>
+        <main className="container margin-vert--xl">
+          <h1>Error boundary tests</h1>
+          <div>
+            <ErrorBoundaryTestButton>
+              Crash inside layout
+            </ErrorBoundaryTestButton>
+          </div>
+        </main>
       </Layout>
     </>
   );
