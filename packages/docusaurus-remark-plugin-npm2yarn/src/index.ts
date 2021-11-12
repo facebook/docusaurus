efe9d6358677771848df71ea3ad9fadd72381425
Josh-Cena <sidachen2003@gmail.com>
2021-11-12 20:25:26 +0800

    Use unist-util-visit

--- packages/docusaurus-remark-plugin-npm2yarn/src/index.ts
+++ packages/docusaurus-remark-plugin-npm2yarn/src/index.ts
@@ -8,6 +8,7 @@
 import type {Code, Content, Literal} from 'mdast';
 import type {Plugin, Transformer} from 'unified';
 import type {Node, Parent} from 'unist';
+import visit from 'unist-util-visit';
 import npmToYarn from 'npm-to-yarn';
 
 interface PluginOptions {
@@ -17,44 +18,34 @@ interface PluginOptions {
 // E.g. global install: 'npm i' -> 'yarn'
 const convertNpmToYarn = (npmCode: string) => npmToYarn(npmCode, 'yarn');
 
-const transformNode = (node: Code, isSync: boolean): Parent => {
+const transformNode = (node: Code, isSync: boolean) => {
   const groupIdProp = isSync ? 'groupId="npm2yarn" ' : '';
   const npmCode = node.value;
   const yarnCode = convertNpmToYarn(node.value);
-  return {
-    type: '',
-    children: [
-      {
-        type: 'jsx',
-        value:
-          `<Tabs defaultValue="npm" ${groupIdProp}` +
-          `values={[
-    { label: 'npm', value: 'npm', },
-    { label: 'Yarn', value: 'yarn', },
-  ]}
->
-<TabItem value="npm">`,
-      },
-      {
-        type: node.type,
-        lang: node.lang,
-        value: npmCode,
-      },
-      {
-        type: 'jsx',
-        value: '</TabItem>\n<TabItem value="yarn">',
-      },
-      {
-        type: node.type,
-        lang: node.lang,
-        value: yarnCode,
-      },
-      {
-        type: 'jsx',
-        value: '</TabItem>\n</Tabs>',
-      },
-    ] as Content[],
-  };
+  return [
+    {
+      type: 'jsx',
+      value: `<Tabs ${groupIdProp}>\n<TabItem value="npm">`,
+    },
+    {
+      type: node.type,
+      lang: node.lang,
+      value: npmCode,
+    },
+    {
+      type: 'jsx',
+      value: '</TabItem>\n<TabItem value="yarn" label="Yarn">',
+    },
+    {
+      type: node.type,
+      lang: node.lang,
+      value: yarnCode,
+    },
+    {
+      type: 'jsx',
+      value: '</TabItem>\n</Tabs>',
+    },
+  ] as Content[];
 };
 
 const isImport = (node: Node): node is Literal => node.type === 'import';
@@ -68,38 +59,35 @@ const nodeForImport: Literal = {
     "import Tabs from '@theme/Tabs';\nimport TabItem from '@theme/TabItem';",
 };
 
-const attacher: Plugin<[PluginOptions?]> = (options = {}) => {
+const plugin: Plugin<[PluginOptions?]> = (options = {}) => {
   const {sync = false} = options;
   let transformed = false;
   let alreadyImported = false;
-  const transformer: Transformer = (node, _) => {
-    if (isImport(node) && node.value.includes('@theme/Tabs')) {
-      alreadyImported = true;
-      return undefined;
-    }
-    if (matchNode(node)) {
-      transformed = true;
-      return transformNode(node, sync);
-    }
-    if (isParent(node)) {
-      let index = 0;
-      while (index < node.children.length) {
-        const {children: result} =
-          (transformer(node.children[index], _) as Parent) ?? {};
-        if (result) {
-          node.children.splice(index, 1, ...result);
-          index += result.length;
-        } else {
-          index += 1;
+  const transformer: Transformer = (root) => {
+    visit(root, (node: Node) => {
+      if (isImport(node) && node.value.includes('@theme/Tabs')) {
+        alreadyImported = true;
+      }
+      if (isParent(node)) {
+        let index = 0;
+        while (index < node.children.length) {
+          const child = node.children[index];
+          if (matchNode(child)) {
+            const result = transformNode(child, sync);
+            node.children.splice(index, 1, ...result);
+            index += result.length;
+            transformed = true;
+          } else {
+            index += 1;
+          }
         }
       }
+    });
+    if (transformed && !alreadyImported) {
+      (root as Parent).children.unshift(nodeForImport);
     }
-    if (node.type === 'root' && transformed && !alreadyImported) {
-      (node as Parent).children.unshift(nodeForImport);
-    }
-    return undefined;
   };
   return transformer;
 };
 
-module.exports = attacher;
+export default plugin;
