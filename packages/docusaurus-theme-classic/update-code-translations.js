766a90c3e4b5af8b6cbde023b9e0e56c22daebe4
Armano <armano2@users.noreply.github.com>
2021-11-12 23:49:42 +0100

    refactor(theme-search-algolia): correct path to translations

--- packages/docusaurus-theme-classic/update-code-translations.js
+++ packages/docusaurus-theme-classic/update-code-translations.js
@@ -15,13 +15,7 @@ const CodeDirPaths = [
   path.join(__dirname, 'lib-next'),
   // TODO other themes should rather define their own translations in the future?
   path.join(__dirname, '..', 'docusaurus-theme-common', 'lib'),
-  path.join(
-    __dirname,
-    '..',
-    'docusaurus-theme-search-algolia',
-    'lib-next',
-    'theme',
-  ),
+  path.join(__dirname, '..', 'docusaurus-theme-search-algolia', 'lib', 'theme'),
   path.join(__dirname, '..', 'docusaurus-theme-live-codeblock', 'src', 'theme'),
   path.join(__dirname, '..', 'docusaurus-plugin-pwa', 'src', 'theme'),
 ];
