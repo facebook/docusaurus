69c85cec841e671216721e416239888c1ca5417b
Joshua Chen <sidachen2003@gmail.com>
2021-12-07 21:42:55 +0800

    Add new line

--- packages/docusaurus/src/client/docusaurus.ts
+++ packages/docusaurus/src/client/docusaurus.ts
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import {matchRoutes} from 'react-router-config';
 import routesChunkNames from '@generated/routesChunkNames';
 import routes from '@generated/routes';
