fd966b53955b49bd23442b231237afbb4522f0e8
Joshua Chen <sidachen2003@gmail.com>
2021-12-09 11:53:46 +0800

    refactor: add blank lines below all copyright headers (#6071)

--- packages/docusaurus-plugin-pwa/src/options.ts
+++ packages/docusaurus-plugin-pwa/src/options.ts
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import path from 'path';
 import {Joi} from '@docusaurus/utils-validation';
 import type {
