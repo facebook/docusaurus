e4f9ab072cb1d44da6af2f27428d9336d9ab587a
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2021-12-23 14:24:56 +0800

    chore: upgrade rehype-katex and katex stylesheet for website (#6172)
    * chore: Update rehype-katex and katex stylesheet for doc website
    
    * Reformat
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -79,9 +79,10 @@ const config = {
   trailingSlash: isDeployPreview,
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
       integrity:
-        'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
       crossorigin: 'anonymous',
     },
   ],
