da8c1e8c677460f083c8c25db9345f6d356e6e21
Joshua Chen <sidachen2003@gmail.com>
2021-12-23 13:09:00 +0800

    Reformat

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -79,11 +79,11 @@ const config = {
   trailingSlash: isDeployPreview,
   stylesheets: [
     {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-      type: "text/css",
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
       integrity:
-        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-      crossorigin: "anonymous",
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
     },
   ],
   i18n: {
