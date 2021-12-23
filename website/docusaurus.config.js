2039c243442bee7d5fd9aef29adddc2f8ca14324
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2021-12-23 12:10:29 +0800

    chore: Update rehype-katex and katex stylesheet for doc website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -79,10 +79,11 @@ const config = {
   trailingSlash: isDeployPreview,
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
   i18n: {
