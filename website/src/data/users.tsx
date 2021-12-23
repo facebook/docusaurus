a189c23d70ba645006a16066bf456da109c91e6c
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2021-12-23 16:07:51 +0000

    Add Digital Support notes to showcase

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -439,7 +439,7 @@ const Users: User[] = [
   {
     title: 'Digital Support Services Notes',
     description: 'All notes needed for Digital Support Services T Level',
-    preview: require('./showcase/digitalsupport.png'),
+    preview: require('./showcase/digitalsupportservices.png'),
     website: 'https://notes.nayanpatel.net',
     source: 'https://github.com/PatelN123/Digital-Support-Notes',
     tags: ['opensource', 'design', 'personal'],
