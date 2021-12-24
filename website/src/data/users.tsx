866f67afb12c3c38d5914f364e67e34bab95e371
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2021-12-24 02:03:31 +0000

    docs: add Digital Support Notes to showcase (#6178)
    * Add Digital Support Notes to the showcase
    
    Add Digital Support Notes to the showcase!
    
    * Update users.tsx
    
    * Add Digital Support notes to showcase
    
    * Update screenshot
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -437,6 +437,14 @@ const Users: User[] = [
     tags: ['opensource'],
   },
   {
+    title: 'Digital Support Services Notes',
+    description: 'All notes needed for Digital Support Services T Level',
+    preview: require('./showcase/digitalsupportservices.png'),
+    website: 'https://notes.nayanpatel.net',
+    source: 'https://github.com/PatelN123/Digital-Support-Notes',
+    tags: ['opensource', 'design', 'personal'],
+  },
+  {
     title: 'Djamaile Rahamat',
     description: 'Djamaile Rahamat Blog on making cool stuff',
     preview: require('./showcase/djamaile.png'),
