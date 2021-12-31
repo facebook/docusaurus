704075ef02de102672d419603cd5dd4dc576d3bb
Joshua Chen <sidachen2003@gmail.com>
2021-12-31 12:02:43 +0800

    revert

--- packages/docusaurus-plugin-content-blog/src/__tests__/blogUtils.test.ts
+++ packages/docusaurus-plugin-content-blog/src/__tests__/blogUtils.test.ts
@@ -5,8 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import path from 'path';
-import {parseBlogFileName, getDataFilePath} from '../blogUtils';
+import {parseBlogFileName} from '../blogUtils';
 
 describe('parseBlogFileName', () => {
   test('parse file', () => {
@@ -91,114 +90,5 @@ describe('parseBlogFileName', () => {
       text: 'announcing-docusaurus-two-beta/subfolder/subfile',
       slug: '/2021/05/12/announcing-docusaurus-two-beta/subfolder/subfile',
     });
-  });
-});
-
-describe('getDataFilePath', () => {
-  const fixturesDir = path.join(__dirname, '__fixtures__/getDataFilePath');
-  const contentPathYml1 = path.join(fixturesDir, 'contentPathYml1');
-  const contentPathYml2 = path.join(fixturesDir, 'contentPathYml2');
-  const contentPathJson1 = path.join(fixturesDir, 'contentPathJson1');
-  const contentPathJson2 = path.join(fixturesDir, 'contentPathJson2');
-  const contentPathEmpty = path.join(fixturesDir, 'contentPathEmpty');
-  const contentPathNestedYml = path.join(fixturesDir, 'contentPathNestedYml');
-
-  test('getDataFilePath returns localized Yml path in priority', async () => {
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.yml',
-        contentPaths: {
-          contentPathLocalized: contentPathYml1,
-          contentPath: contentPathYml2,
-        },
-      }),
-    ).toEqual(path.join(contentPathYml1, 'authors.yml'));
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.yml',
-        contentPaths: {
-          contentPathLocalized: contentPathYml2,
-          contentPath: contentPathYml1,
-        },
-      }),
-    ).toEqual(path.join(contentPathYml2, 'authors.yml'));
-  });
-
-  test('getDataFilePath returns localized Json path in priority', async () => {
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.json',
-        contentPaths: {
-          contentPathLocalized: contentPathJson1,
-          contentPath: contentPathJson2,
-        },
-      }),
-    ).toEqual(path.join(contentPathJson1, 'authors.json'));
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.json',
-        contentPaths: {
-          contentPathLocalized: contentPathJson2,
-          contentPath: contentPathJson1,
-        },
-      }),
-    ).toEqual(path.join(contentPathJson2, 'authors.json'));
-  });
-
-  test('getDataFilePath returns unlocalized Yml path as fallback', async () => {
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.yml',
-        contentPaths: {
-          contentPathLocalized: contentPathEmpty,
-          contentPath: contentPathYml2,
-        },
-      }),
-    ).toEqual(path.join(contentPathYml2, 'authors.yml'));
-  });
-
-  test('getDataFilePath returns unlocalized Json path as fallback', async () => {
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.json',
-        contentPaths: {
-          contentPathLocalized: contentPathEmpty,
-          contentPath: contentPathJson1,
-        },
-      }),
-    ).toEqual(path.join(contentPathJson1, 'authors.json'));
-  });
-
-  test('getDataFilePath can return undefined (file not found)', async () => {
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.json',
-        contentPaths: {
-          contentPathLocalized: contentPathEmpty,
-          contentPath: contentPathYml1,
-        },
-      }),
-    ).toBeUndefined();
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'authors.yml',
-        contentPaths: {
-          contentPathLocalized: contentPathEmpty,
-          contentPath: contentPathJson1,
-        },
-      }),
-    ).toBeUndefined();
-  });
-
-  test('getDataFilePath can return nested path', async () => {
-    expect(
-      await getDataFilePath({
-        dataFilePath: 'sub/folder/authors.yml',
-        contentPaths: {
-          contentPathLocalized: contentPathEmpty,
-          contentPath: contentPathNestedYml,
-        },
-      }),
-    ).toEqual(path.join(contentPathNestedYml, 'sub/folder/authors.yml'));
   });
 });
