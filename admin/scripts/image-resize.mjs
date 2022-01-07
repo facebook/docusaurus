75a622d3ea035f0252d3ca263037e221d640218c
Joshua Chen <sidachen2003@gmail.com>
2022-01-07 13:58:14 +0800

    Optimize

--- admin/scripts/image-resize.mjs
+++ admin/scripts/image-resize.mjs
@@ -10,9 +10,9 @@ import sharp from 'sharp';
 import fs from 'fs/promises';
 import path from 'path';
 
-const images = (await fs.readdir(
-  new URL('../../website/src/data/showcase', import.meta.url),
-)).filter((file) => ['.png', 'jpg', '.jpeg'].includes(path.extname(file)))
+const images = (
+  await fs.readdir(new URL('../../website/src/data/showcase', import.meta.url))
+).filter((file) => ['.png', 'jpg', '.jpeg'].includes(path.extname(file)));
 
 await Promise.all(
   images.map(async (img) => {
@@ -20,7 +20,15 @@ await Promise.all(
       `../../website/src/data/showcase/${img}`,
       import.meta.url,
     ).pathname;
-    const data = await sharp(imgPath).resize(640, 320, {fit: 'cover', position: 'top'}).png().toBuffer();
+    const data = await sharp(imgPath)
+      .resize(640, 320, {fit: 'cover', position: 'top'})
+      .png()
+      .toBuffer();
     await fs.writeFile(imgPath.replace(/jpe?g/, 'png'), data);
   }),
 );
+
+// You should also run optimizt `find website/src/data/showcase -type f -name '*.png'`.
+// This is not included here because @funboxteam/optimizt doesn't seem to play well with M1
+// so I had to run this in a Rosetta terminal.
+// TODO integrate this as part of the script
