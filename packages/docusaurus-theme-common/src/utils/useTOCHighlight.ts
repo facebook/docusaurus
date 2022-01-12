bb95356e50d1ee2f26b6c3ed6d72c13dae598ce5
Joshua Chen <sidachen2003@gmail.com>
2022-01-12 10:52:26 +0800

    fix page container scrolling

--- packages/docusaurus-theme-common/src/utils/useTOCHighlight.ts
+++ packages/docusaurus-theme-common/src/utils/useTOCHighlight.ts
@@ -144,7 +144,7 @@ function useTOCHighlight(config: TOCHighlightConfig | undefined): void {
         }
         link.classList.add(linkActiveClassName);
         lastActiveLinkRef.current = link;
-        link.scrollIntoView({behavior: 'smooth'});
+        link.scrollIntoView({block: 'nearest'});
       } else {
         link.classList.remove(linkActiveClassName);
       }
