facebook/docusaurus

commit e0127c66be236e4e96b897bdba9fff6be09d2faa

chore(website): enable eslint in website (#5889)
* chore: enable eslint in website

* Fixes

* prettier users

website/src/components/ColorGenerator/index.tsx
@@ -66,10 +66,10 @@ const COLOR_SHADES: Record<
 
 const DEFAULT_PRIMARY_COLOR = '3578e5';
 
-function ColorGenerator() {
+function ColorGenerator(): JSX.Element {
   const [baseColor, setBaseColor] = useState(DEFAULT_PRIMARY_COLOR);
   const [shades, setShades] = useState(COLOR_SHADES);
-  const color = Color('#' + baseColor);
+  const color = Color(`#${baseColor}`);
   const adjustedColors = Object.keys(shades)
     .map((shade) => ({
       ...shades[shade],
@@ -83,6 +83,7 @@ function ColorGenerator() {
   return (
     <div>
       <p>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label htmlFor="primary_color">
           <strong className="margin-right--sm">Primary Color:</strong>
         </label>{' '}
@@ -96,7 +97,7 @@ function ColorGenerator() {
             const colorValue = event.target.value.replace(/^#+/, '');
 
             try {
-              Color('#' + colorValue);
+              Color(`#${baseColor}`);
               setBaseColor(colorValue);
             } catch {
               // Don't update for invalid colors.
@@ -150,7 +151,7 @@ function ColorGenerator() {
                               [variableName]: {
                                 ...shades[variableName],
                                 adjustmentInput: event.target.value,
-                                adjustment: isNaN(newValue)
+                                adjustment: Number.isNaN(newValue)
                                   ? adjustment
                                   : newValue / 100.0,
                               },

website/src/components/BrowserWindow/index.tsx
@@ -15,7 +15,7 @@ interface Props {
   url: string;
 }
 
-function BrowserWindow({children, minHeight, url}: Props) {
+function BrowserWindow({children, minHeight, url}: Props): JSX.Element {
   return (
     <div className={styles.browserWindow} style={{minHeight}}>
       <div className={styles.browserWindowHeader}>

website/src/components/ErrorBoundaryTestButton/index.tsx
@@ -5,12 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {useState} from 'react';
+import React, {ReactNode, useState} from 'react';
 
-export default function ErrorBoundaryTestButton({children = 'Boom!'}) {
+export default function ErrorBoundaryTestButton({
+  children = 'Boom!',
+}: {
+  children?: ReactNode;
+}): JSX.Element {
   const [state, setState] = useState(false);
   if (state) {
     throw new Error('Boom!');
   }
-  return <button onClick={() => setState(true)}>{children}</button>;
+  return (
+    <button type="button" onClick={() => setState(true)}>
+      {children}
+    </button>
+  );
 }

website/src/components/Playground/index.tsx
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/* eslint-disable global-require */
+
 import React from 'react';
 import Translate from '@docusaurus/Translate';
 import Link from '@docusaurus/Link';
@@ -32,7 +34,7 @@ const Playgrounds = [
         id="playground.stackblitz.description"
         values={{
           webContainersLink: (
-            <Link target="https://blog.stackblitz.com/posts/introducing-webcontainers/">
+            <Link href="https://blog.stackblitz.com/posts/introducing-webcontainers/">
               WebContainers
             </Link>
           ),
@@ -77,7 +79,7 @@ function PlaygroundCard({name, image, url, description}: Props) {
   );
 }
 
-export function PlaygroundCardsRow() {
+export function PlaygroundCardsRow(): JSX.Element {
   return (
     <div className="row">
       {Playgrounds.map((playground) => (

website/src/components/TeamProfileCards/index.tsx
@@ -41,7 +41,7 @@ function TeamProfileCard({
           <div className="avatar avatar--vertical">
             <img
               className="avatar__photo avatar__photo--xl"
-              src={githubUrl + '.png'}
+              src={`${githubUrl}.png`}
               alt={`${name}'s avatar`}
             />
             <div className="avatar__intro">
@@ -71,11 +71,11 @@ function TeamProfileCard({
 
 function TeamProfileCardCol(props: ProfileProps) {
   return (
-    <TeamProfileCard {...props} className={'col col--6 margin-bottom--lg'} />
+    <TeamProfileCard {...props} className="col col--6 margin-bottom--lg" />
   );
 }
 
-export function ActiveTeamRow() {
+export function ActiveTeamRow(): JSX.Element {
   return (
     <div className="row">
       <TeamProfileCardCol
@@ -125,15 +125,15 @@ export function ActiveTeamRow() {
         twitterUrl="https://twitter.com/yangshunz">
         <Translate id="team.profile.Yangshun Tay.body">
           Full Front End Stack developer who likes working on the Jamstack.
-          Working on Docusaurus made him Facebook's unofficial part-time Open
-          Source webmaster, which is an awesome role to be in.
+          Working on Docusaurus made him Facebook&apos;s unofficial part-time
+          Open Source webmaster, which is an awesome role to be in.
         </Translate>
       </TeamProfileCardCol>
     </div>
   );
 }
 
-export function HonoraryAlumniTeamRow() {
+export function HonoraryAlumniTeamRow(): JSX.Element {
   return (
     <div className="row">
       <TeamProfileCardCol
@@ -157,7 +157,7 @@ export function HonoraryAlumniTeamRow() {
   );
 }
 
-export function StudentFellowsTeamRow() {
+export function StudentFellowsTeamRow(): JSX.Element {
   return (
     <div className="row">
       <TeamProfileCardCol
@@ -166,7 +166,7 @@ export function StudentFellowsTeamRow() {
         twitterUrl="https://twitter.com/ar_goyal">
         Fullstack developer who loves to code and try new technologies. In his
         free time, he contributes to open source, writes blog posts on his{' '}
-        <a href="https://anshulgoyal.dev/" target="_blank">
+        <a href="https://anshulgoyal.dev/" target="_blank" rel="noreferrer">
           website
         </a>{' '}
         and watches Anime.
@@ -184,11 +184,14 @@ export function StudentFellowsTeamRow() {
         Fanny got started with web development in high school, building a
         project for the school kitchen. In her free time she loves contributing
         to Open Source, occasionally writing on{' '}
-        <a href="https://dev.to/fannyvieira" target="_blank">
+        <a href="https://dev.to/fannyvieira" target="_blank" rel="noreferrer">
           her blog
         </a>{' '}
         about her experiences, cooking, and creating{' '}
-        <a href="https://open.spotify.com/user/anotherfanny" target="_blank">
+        <a
+          href="https://open.spotify.com/user/anotherfanny"
+          target="_blank"
+          rel="noreferrer">
           Spotify playlists
         </a>
         .

website/src/components/showcase/ShowcaseCheckbox/index.tsx
@@ -14,7 +14,12 @@ interface Props extends ComponentProps<'input'> {
   label: ReactNode;
 }
 
-function ShowcaseCheckbox({title, className, label, ...props}: Props) {
+function ShowcaseCheckbox({
+  title,
+  className,
+  label,
+  ...props
+}: Props): JSX.Element {
   const id = `showcase_checkbox_id_${props.name};`;
   return (
     <div className={clsx(className, styles.checkboxContainer)} title={title}>

website/src/components/showcase/ShowcaseSelect/index.tsx
@@ -13,7 +13,7 @@ interface Props extends ComponentProps<'select'> {
   label: string;
 }
 
-function ShowcaseSelect({label, ...props}: Props) {
+function ShowcaseSelect({label, ...props}: Props): JSX.Element {
   const id = `showcase_select_id_${props.name};`;
   return (
     <div className={styles.selectContainer}>

website/src/pages/index.tsx
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/* eslint-disable global-require */
+
 import React from 'react';
 import Link from '@docusaurus/Link';
 import Translate, {translate} from '@docusaurus/Translate';
@@ -78,7 +80,7 @@ const QUOTES = [
   },
 ];
 
-function Home() {
+function Home(): JSX.Element {
   const {
     siteConfig: {
       customFields: {description},
@@ -98,6 +100,7 @@ function Home() {
               />
               <span
                 className={styles.heroTitleTextHtml}
+                // eslint-disable-next-line react/no-danger
                 dangerouslySetInnerHTML={{
                   __html: translate({
                     id: 'homepage.hero.title',
@@ -162,11 +165,11 @@ function Home() {
                 </h2>
                 <p className="padding-horiz--md">
                   <Translate>
-                    Save time and focus on your project's documentation. Simply
-                    write docs and blog posts with Markdown/MDX and Docusaurus
-                    will publish a set of static HTML files ready to serve. You
-                    can even embed JSX components into your Markdown thanks to
-                    MDX.
+                    Save time and focus on your project&apos;s documentation.
+                    Simply write docs and blog posts with Markdown/MDX and
+                    Docusaurus will publish a set of static HTML files ready to
+                    serve. You can even embed JSX components into your Markdown
+                    thanks to MDX.
                   </Translate>
                 </p>
               </div>
@@ -181,9 +184,9 @@ function Home() {
                 </h2>
                 <p className="padding-horiz--md">
                   <Translate>
-                    Extend or customize your project's layout by reusing React.
-                    Docusaurus can be extended while reusing the same header and
-                    footer.
+                    Extend or customize your project&apos;s layout by reusing
+                    React. Docusaurus can be extended while reusing the same
+                    header and footer.
                   </Translate>
                 </p>
               </div>

commit 9cdd7c1fbcaa47509b20bacddae1eb4fcc73f94e

fix a bug in ColorGenerator which was introduced by the commit e0127c6 (#6343)

website/src/components/ColorGenerator/index.tsx
@@ -97,7 +97,7 @@ function ColorGenerator(): JSX.Element {
             const colorValue = event.target.value.replace(/^#+/, '');
 
             try {
-              Color(`#${baseColor}`);
+              Color(`#${colorValue}`);
               setBaseColor(colorValue);
             } catch {
               // Don't update for invalid colors.