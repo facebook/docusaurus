788e24dfd63f8fb7da37f5d392e4ac2139e79916
Nick Schonning <nschonni@gmail.com>
2022-01-15 01:30:16 -0500

    chore: upgrade lint-staged and globs
    - lint-staged to v12
    - Update Prettier globs to run on all files
    - Add lint-staged hook for stylelint

--- packages/create-docusaurus/templates/facebook/src/pages/index.js
+++ packages/create-docusaurus/templates/facebook/src/pages/index.js
@@ -69,7 +69,8 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
@@ -80,7 +81,8 @@ export default function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('docs/')}
+            >
               Get Started
             </Link>
           </div>
