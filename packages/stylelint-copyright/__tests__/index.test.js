14bec7cf118773306d9a01bd07db6ab075ef2f1f
Joshua Chen <sidachen2003@gmail.com>
2022-01-16 11:34:10 +0800

    feat(stylelint-copyright): autofix, stricter config (#6374)
    * feat(stylelint-copyright): autofix, stricter config
    
    * revert TS
    
    * oops

--- packages/stylelint-copyright/__tests__/index.test.js
+++ packages/stylelint-copyright/__tests__/index.test.js
@@ -5,17 +5,128 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+const stylelint = require('stylelint');
 const path = require('path');
 const rule = require('..');
 
 const {ruleName, messages} = rule;
 
+function getOutputCss(output) {
+  const result = output.results[0]._postcssResult;
+  return result.root.toString(result.opts.syntax);
+}
+
+function testStylelintRule(config, tests) {
+  describe(tests.ruleName, () => {
+    const checkTestCaseContent = (testCase) =>
+      testCase.description || testCase.code || 'no description';
+
+    if (tests.accept && tests.accept.length) {
+      describe('accept cases', () => {
+        tests.accept.forEach((testCase) => {
+          const spec = testCase.only ? it.only : it;
+
+          spec(checkTestCaseContent(testCase), () => {
+            const options = {
+              code: testCase.code,
+              config,
+              syntax: tests.syntax,
+            };
+
+            return stylelint.lint(options).then((output) => {
+              expect(output.results[0].warnings).toEqual([]);
+
+              if (!tests.fix) {
+                return null;
+              }
+
+              // Check the fix.
+              return stylelint
+                .lint({...options, fix: true})
+                .then((fixedOutput) => getOutputCss(fixedOutput))
+                .then((fixedCode) => expect(fixedCode).toBe(testCase.fixed));
+            });
+          });
+        });
+      });
+    }
+
+    if (tests.reject && tests.reject.length) {
+      describe('reject cases', () => {
+        tests.reject.forEach((testCase) => {
+          const skip = testCase.skip ? it.skip : it;
+          const spec = testCase.only ? it.only : skip;
+
+          spec(checkTestCaseContent(testCase), () => {
+            const options = {
+              code: testCase.code,
+              config,
+              syntax: tests.syntax,
+            };
+
+            return stylelint.lint(options).then((output) => {
+              const {warnings} = output.results[0];
+              const warning = warnings[0];
+
+              expect(warnings.length).toBeGreaterThanOrEqual(1);
+              expect(testCase).toHaveMessage();
+
+              if (testCase.message != null) {
+                expect(warning.text).toBe(testCase.message);
+              }
+
+              if (testCase.line != null) {
+                expect(warning.line).toBe(testCase.line);
+              }
+
+              if (testCase.column != null) {
+                expect(warning.column).toBe(testCase.column);
+              }
+
+              if (!tests.fix) {
+                return null;
+              }
+
+              if (!testCase.fixed) {
+                throw new Error(
+                  'If using { fix: true } in test tests, all reject cases must have { fixed: .. }',
+                );
+              }
+
+              // Check the fix.
+              return stylelint
+                .lint({...options, fix: true})
+                .then((fixedOutput) => getOutputCss(fixedOutput))
+                .then((fixedCode) => expect(fixedCode).toBe(testCase.fixed));
+            });
+          });
+        });
+      });
+    }
+
+    expect.extend({
+      toHaveMessage(testCase) {
+        if (testCase.message == null) {
+          return {
+            message: () =>
+              'Expected "reject" test case to have a "message" property',
+            pass: false,
+          };
+        }
+
+        return {
+          pass: true,
+        };
+      },
+    });
+  });
+}
+
 testStylelintRule(
   {
-    // Relative to repo root.
-    plugins: [path.join(process.cwd(), 'packages', 'stylelint-copyright')],
+    plugins: [path.join(__dirname, '..')],
     rules: {
-      [ruleName]: true,
+      [ruleName]: [true, {header: '*\n * Copyright'}],
     },
   },
   {
@@ -30,16 +141,19 @@ testStylelintRule(
 
  .foo {}`,
       },
-      {
-        code: `
-      /**
- * copyright
- */
-
- .foo {}`,
-      },
     ],
     reject: [
+      {
+        code: `
+    /**
+* copyright
+*/
+
+.foo {}`,
+        message: messages.rejected,
+        line: 1,
+        column: 1,
+      },
       {
         code: `
 /**
@@ -48,7 +162,7 @@ testStylelintRule(
 
  .foo {}`,
         message: messages.rejected,
-        line: 2,
+        line: 1,
         column: 1,
       },
       {
@@ -62,7 +176,7 @@ testStylelintRule(
  */
  .foo {}`,
         message: messages.rejected,
-        line: 2,
+        line: 1,
         column: 1,
       },
     ],
