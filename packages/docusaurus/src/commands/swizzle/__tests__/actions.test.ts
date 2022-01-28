044499fae097e87b9d059a23b66e2d0f27fe138d
sebastienlorber <lorber.sebastien@gmail.com>
2022-01-28 14:43:57 +0100

    add tests to surface existing eject problems

--- packages/docusaurus/src/commands/swizzle/__tests__/actions.test.ts
+++ packages/docusaurus/src/commands/swizzle/__tests__/actions.test.ts
@@ -13,50 +13,51 @@ import type {SwizzleAction} from '@docusaurus/types';
 // @ts-expect-error: TODO no typedefs
 import tree from 'tree-node-cli';
 
-describe('executeAction', () => {
-  async function testExecuteAction(
-    action: SwizzleAction,
-    componentName: string,
-  ) {
-    const siteDir = await createTempSiteDir();
-    const siteThemePath = path.join(siteDir, 'src/theme');
-    const result = await executeAction({
-      action,
-      siteDir,
-      componentName,
-      themePath: ThemePath,
-    });
-    const from = path.relative(ThemePath, result.from);
-    const to = path.relative(siteThemePath, result.to);
+async function testExecuteAction(action: SwizzleAction, componentName: string) {
+  const siteDir = await createTempSiteDir();
+  const siteThemePath = path.join(siteDir, 'src/theme');
+  const result = await executeAction({
+    action,
+    siteDir,
+    componentName,
+    themePath: ThemePath,
+  });
+  const from = path.relative(ThemePath, result.from);
+  const to = path.relative(siteThemePath, result.to);
 
-    return {
-      siteDir,
-      siteThemePath,
-      from,
-      to,
-      tree: tree(siteThemePath),
-    };
-  }
+  return {
+    siteDir,
+    siteThemePath,
+    from,
+    to,
+    tree: tree(siteThemePath),
+  };
+}
 
-  describe('eject', () => {
-    test('eject FirstLevelComponent', async () => {
-      const result = await testExecuteAction(
-        'eject',
-        Components.FirstLevelComponent,
-      );
-      expect(result.from).toEqual('FirstLevelComponent.tsx');
-      expect(result.to).toEqual('FirstLevelComponent.tsx');
-    });
+describe('eject', () => {
+  test(`eject ${Components.FirstLevelComponent}`, async () => {
+    const result = await testExecuteAction(
+      'eject',
+      Components.FirstLevelComponent,
+    );
+    // TODO remove the .tsx extension from output + copy also css file
+    expect(result.from).toEqual(`${Components.FirstLevelComponent  }.tsx`);
+    expect(result.to).toEqual(`${Components.FirstLevelComponent  }.tsx`);
+    expect(result.tree).toMatchInlineSnapshot(`
+      "theme
+      └── FirstLevelComponent.tsx"
+    `);
+  });
 
-    test('eject ComponentInSubFolder', async () => {
-      const result = await testExecuteAction(
-        'eject',
-        Components.ComponentInSubFolder,
-      );
+  test(`eject ${Components.ComponentInSubFolder}`, async () => {
+    const result = await testExecuteAction(
+      'eject',
+      Components.ComponentInSubFolder,
+    );
 
-      expect(result.from).toEqual('ComponentInFolder/ComponentInSubFolder');
-      expect(result.to).toEqual('ComponentInFolder/ComponentInSubFolder');
-      expect(result.tree).toMatchInlineSnapshot(`
+    expect(result.from).toEqual(Components.ComponentInSubFolder);
+    expect(result.to).toEqual(Components.ComponentInSubFolder);
+    expect(result.tree).toMatchInlineSnapshot(`
         "theme
         └── ComponentInFolder
             └── ComponentInSubFolder
@@ -65,6 +66,38 @@ describe('executeAction', () => {
                 ├── index.test.tsx
                 └── index.tsx"
       `);
-    });
+  });
+
+  test(`eject ${Components.ComponentInFolder}`, async () => {
+    const result = await testExecuteAction(
+      'eject',
+      Components.ComponentInFolder,
+    );
+
+    expect(result.from).toEqual(Components.ComponentInFolder);
+    expect(result.to).toEqual(Components.ComponentInFolder);
+
+    // TODO only copy index.{tsx,css} here
+    expect(result.tree).toMatchInlineSnapshot(`
+      "theme
+      └── ComponentInFolder
+          ├── ComponentInSubFolder
+          │   ├── index.css
+          │   ├── index.stories.tsx
+          │   ├── index.test.tsx
+          │   └── index.tsx
+          ├── Sibling.css
+          ├── Sibling.js
+          ├── Sibling.stories.jsx
+          ├── Sibling.test.js
+          ├── __fixtures__
+          │   └── FileInTest.ts
+          ├── __tests__
+          │   └── FileInFixtures.ts
+          ├── index.css
+          ├── index.stories.tsx
+          ├── index.test.tsx
+          └── index.tsx"
+    `);
   });
 });
