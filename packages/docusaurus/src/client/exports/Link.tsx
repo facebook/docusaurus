168b0ea13d534d8320ceaf5cc47fad876f89c5c8
Joshua Chen <sidachen2003@gmail.com>
2022-02-10 13:45:35 +0800

    refactor

--- packages/docusaurus/src/client/exports/Link.tsx
+++ packages/docusaurus/src/client/exports/Link.tsx
@@ -6,7 +6,6 @@
  */
 
 import React, {
-  forwardRef,
   useEffect,
   useImperativeHandle,
   useRef,
@@ -37,159 +36,157 @@ declare global {
 // like "introduction" to "/baseUrl/introduction" => bad behavior to fix
 const shouldAddBaseUrlAutomatically = (to: string) => to.startsWith('/');
 
-const Link = forwardRef<HTMLAnchorElement>(
-  (
-    {
-      isNavLink,
-      to,
-      href,
-      activeClassName,
-      isActive,
-      'data-noBrokenLinkCheck': noBrokenLinkCheck,
-      autoAddBaseUrl = true,
-      ...props
-    }: LinkProps,
+function Link(
+  {
+    isNavLink,
+    to,
+    href,
+    activeClassName,
+    isActive,
+    'data-noBrokenLinkCheck': noBrokenLinkCheck,
+    autoAddBaseUrl = true,
+    ...props
+  }: LinkProps,
+  forwardedRef: React.ForwardedRef<HTMLAnchorElement>,
+): JSX.Element {
+  const {
+    siteConfig: {trailingSlash, baseUrl},
+  } = useDocusaurusContext();
+  const {withBaseUrl} = useBaseUrlUtils();
+  const linksCollector = useLinksCollector();
+  const innerRef = useRef<HTMLAnchorElement | null>(null);
+
+  useImperativeHandle(
     forwardedRef,
-  ): JSX.Element => {
-    const {
-      siteConfig: {trailingSlash, baseUrl},
-    } = useDocusaurusContext();
-    const {withBaseUrl} = useBaseUrlUtils();
-    const linksCollector = useLinksCollector();
-    const innerRef = useRef<HTMLAnchorElement | null>(null);
+    () => innerRef.current as HTMLAnchorElement,
+  );
 
-    useImperativeHandle(
-      forwardedRef,
-      () => innerRef.current as HTMLAnchorElement,
-    );
+  // IMPORTANT: using to or href should not change anything
+  // For example, MDX links will ALWAYS give us the href props
+  // Using one prop or the other should not be used to distinguish
+  // internal links (/docs/myDoc) from external links (https://github.com)
+  const targetLinkUnprefixed = to || href;
 
-    // IMPORTANT: using to or href should not change anything
-    // For example, MDX links will ALWAYS give us the href props
-    // Using one prop or the other should not be used to distinguish
-    // internal links (/docs/myDoc) from external links (https://github.com)
-    const targetLinkUnprefixed = to || href;
+  function maybeAddBaseUrl(str: string) {
+    return autoAddBaseUrl && shouldAddBaseUrlAutomatically(str)
+      ? withBaseUrl(str)
+      : str;
+  }
 
-    function maybeAddBaseUrl(str: string) {
-      return autoAddBaseUrl && shouldAddBaseUrlAutomatically(str)
-        ? withBaseUrl(str)
-        : str;
-    }
+  const isInternal = isInternalUrl(targetLinkUnprefixed);
 
-    const isInternal = isInternalUrl(targetLinkUnprefixed);
+  // pathname:// is a special "protocol" we use to tell Docusaurus link
+  // that a link is not "internal" and that we shouldn't use history.push()
+  // this is not ideal but a good enough escape hatch for now
+  // see https://github.com/facebook/docusaurus/issues/3309
+  // note: we want baseUrl to be appended (see issue for details)
+  // TODO read routes and automatically detect internal/external links?
+  const targetLinkWithoutPathnameProtocol = targetLinkUnprefixed?.replace(
+    'pathname://',
+    '',
+  );
 
-    // pathname:// is a special "protocol" we use to tell Docusaurus link
-    // that a link is not "internal" and that we shouldn't use history.push()
-    // this is not ideal but a good enough escape hatch for now
-    // see https://github.com/facebook/docusaurus/issues/3309
-    // note: we want baseUrl to be appended (see issue for details)
-    // TODO read routes and automatically detect internal/external links?
-    const targetLinkWithoutPathnameProtocol = targetLinkUnprefixed?.replace(
-      'pathname://',
-      '',
-    );
+  // TODO we should use ReactRouter basename feature instead!
+  // Automatically apply base url in links that start with /
+  let targetLink =
+    typeof targetLinkWithoutPathnameProtocol !== 'undefined'
+      ? maybeAddBaseUrl(targetLinkWithoutPathnameProtocol)
+      : undefined;
 
-    // TODO we should use ReactRouter basename feature instead!
-    // Automatically apply base url in links that start with /
-    let targetLink =
-      typeof targetLinkWithoutPathnameProtocol !== 'undefined'
-        ? maybeAddBaseUrl(targetLinkWithoutPathnameProtocol)
-        : undefined;
+  if (targetLink && isInternal) {
+    targetLink = applyTrailingSlash(targetLink, {trailingSlash, baseUrl});
+  }
 
-    if (targetLink && isInternal) {
-      targetLink = applyTrailingSlash(targetLink, {trailingSlash, baseUrl});
-    }
+  const preloaded = useRef(false);
+  const LinkComponent = (
+    isNavLink ? NavLink : RRLink
+  ) as ComponentType<LinkProps>;
 
-    const preloaded = useRef(false);
-    const LinkComponent = (
-      isNavLink ? NavLink : RRLink
-    ) as ComponentType<LinkProps>;
+  const IOSupported = ExecutionEnvironment.canUseIntersectionObserver;
 
-    const IOSupported = ExecutionEnvironment.canUseIntersectionObserver;
-
-    const ioRef = useRef<IntersectionObserver>();
-    const handleIntersection = (el: HTMLAnchorElement, cb: () => void) => {
-      ioRef.current = new window.IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (el === entry.target) {
-            // If element is in viewport, stop observing and run callback.
-            // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-            if (entry.isIntersecting || entry.intersectionRatio > 0) {
-              ioRef.current!.unobserve(el);
-              ioRef.current!.disconnect();
-              cb();
-            }
+  const ioRef = useRef<IntersectionObserver>();
+  const handleIntersection = (el: HTMLAnchorElement, cb: () => void) => {
+    ioRef.current = new window.IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (el === entry.target) {
+          // If element is in viewport, stop observing and run callback.
+          // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+          if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            ioRef.current!.unobserve(el);
+            ioRef.current!.disconnect();
+            cb();
           }
-        });
+        }
       });
+    });
 
-      // Add element to the observer.
-      ioRef.current!.observe(el);
-    };
+    // Add element to the observer.
+    ioRef.current!.observe(el);
+  };
 
-    const handleRef = (ref: HTMLAnchorElement | null) => {
-      innerRef.current = ref;
+  const handleRef = (ref: HTMLAnchorElement | null) => {
+    innerRef.current = ref;
 
-      if (IOSupported && ref && isInternal) {
-        // If IO supported and element reference found, set up Observer.
-        handleIntersection(ref, () => {
-          if (targetLink != null) {
-            window.docusaurus.prefetch(targetLink);
-          }
-        });
-      }
-    };
-
-    const onMouseEnter = () => {
-      if (!preloaded.current && targetLink != null) {
-        window.docusaurus.preload(targetLink);
-        preloaded.current = true;
-      }
-    };
-
-    useEffect(() => {
-      // If IO is not supported. We prefetch by default (only once).
-      if (!IOSupported && isInternal) {
+    if (IOSupported && ref && isInternal) {
+      // If IO supported and element reference found, set up Observer.
+      handleIntersection(ref, () => {
         if (targetLink != null) {
           window.docusaurus.prefetch(targetLink);
         }
+      });
+    }
+  };
+
+  const onMouseEnter = () => {
+    if (!preloaded.current && targetLink != null) {
+      window.docusaurus.preload(targetLink);
+      preloaded.current = true;
+    }
+  };
+
+  useEffect(() => {
+    // If IO is not supported. We prefetch by default (only once).
+    if (!IOSupported && isInternal) {
+      if (targetLink != null) {
+        window.docusaurus.prefetch(targetLink);
       }
-
-      // When unmounting, stop intersection observer from watching.
-      return () => {
-        if (IOSupported && ioRef.current) {
-          ioRef.current.disconnect();
-        }
-      };
-    }, [ioRef, targetLink, IOSupported, isInternal]);
-
-    const isAnchorLink = targetLink?.startsWith('#') ?? false;
-    const isRegularHtmlLink = !targetLink || !isInternal || isAnchorLink;
-
-    if (targetLink && isInternal && !isAnchorLink && !noBrokenLinkCheck) {
-      linksCollector.collectLink(targetLink);
     }
 
-    return isRegularHtmlLink ? (
-      // eslint-disable-next-line jsx-a11y/anchor-has-content
-      <a
-        ref={innerRef}
-        href={targetLink}
-        {...(targetLinkUnprefixed &&
-          !isInternal && {target: '_blank', rel: 'noopener noreferrer'})}
-        {...props}
-      />
-    ) : (
-      <LinkComponent
-        {...props}
-        onMouseEnter={onMouseEnter}
-        innerRef={handleRef}
-        to={targetLink || ''}
-        // avoid "React does not recognize the `activeClassName` prop on a DOM
-        // element"
-        {...(isNavLink && {isActive, activeClassName})}
-      />
-    );
-  },
-);
+    // When unmounting, stop intersection observer from watching.
+    return () => {
+      if (IOSupported && ioRef.current) {
+        ioRef.current.disconnect();
+      }
+    };
+  }, [ioRef, targetLink, IOSupported, isInternal]);
 
-export default Link;
+  const isAnchorLink = targetLink?.startsWith('#') ?? false;
+  const isRegularHtmlLink = !targetLink || !isInternal || isAnchorLink;
+
+  if (targetLink && isInternal && !isAnchorLink && !noBrokenLinkCheck) {
+    linksCollector.collectLink(targetLink);
+  }
+
+  return isRegularHtmlLink ? (
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    <a
+      ref={innerRef}
+      href={targetLink}
+      {...(targetLinkUnprefixed &&
+        !isInternal && {target: '_blank', rel: 'noopener noreferrer'})}
+      {...props}
+    />
+  ) : (
+    <LinkComponent
+      {...props}
+      onMouseEnter={onMouseEnter}
+      innerRef={handleRef}
+      to={targetLink || ''}
+      // avoid "React does not recognize the `activeClassName` prop on a DOM
+      // element"
+      {...(isNavLink && {isActive, activeClassName})}
+    />
+  );
+}
+
+export default React.forwardRef(Link);
