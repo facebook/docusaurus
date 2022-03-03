7317b1fdc996c2f66ad896ce000cea2639a25f41
xcbchina <94103090+xcbchina@users.noreply.github.com>
2022-03-03 15:22:28 +0800

    initial commit

--- examples/classic/src/pages/index.js
+++ examples/classic/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Docusaurus Tutorial - 9min ⏱️
           </Link>
         </div>
       </div>
