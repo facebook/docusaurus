3e14f2d7038eab550c193a77f6173d0dc4b6e2af
Joshua Chen <sidachen2003@gmail.com>
2022-03-19 09:20:25 +0800

    refactor(core): improve error message when a page has no default-export

--- packages/docusaurus/src/client/exports/ComponentCreator.tsx
+++ packages/docusaurus/src/client/exports/ComponentCreator.tsx
@@ -75,21 +75,23 @@ export default function ComponentCreator(
       // Clone the original object since we don't want to alter the original.
       const loadedModules = JSON.parse(JSON.stringify(chunkNames));
       Object.keys(loaded).forEach((key) => {
+        const newComp = loaded[key].default;
+        if (!newComp) {
+          throw new Error(
+            `The page component at ${path} doesn't have a default export. This makes it impossible to render anything. Consider default-exporting a React component.`,
+          );
+        }
+        Object.keys(loaded[key])
+          .filter((k) => k !== 'default')
+          .forEach((nonDefaultKey) => {
+            newComp[nonDefaultKey] = loaded[key][nonDefaultKey];
+          });
         let val = loadedModules;
         const keyPath = key.split('.');
         keyPath.slice(0, -1).forEach((k) => {
           val = val[k];
         });
-        val[keyPath[keyPath.length - 1]!] = loaded[key].default;
-        const nonDefaultKeys = Object.keys(loaded[key]).filter(
-          (k) => k !== 'default',
-        );
-        if (nonDefaultKeys?.length) {
-          nonDefaultKeys.forEach((nonDefaultKey) => {
-            val[keyPath[keyPath.length - 1]!][nonDefaultKey] =
-              loaded[key][nonDefaultKey];
-          });
-        }
+        val[keyPath[keyPath.length - 1]!] = newComp;
       });
 
       const Component = loadedModules.component;
