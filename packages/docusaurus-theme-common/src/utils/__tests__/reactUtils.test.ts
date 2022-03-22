ff60ab9e5ad6f1f43ac2b6f36fe03fee21502227
Joshua Chen <sidachen2003@gmail.com>
2022-03-22 15:07:24 +0800

    no need for this

--- packages/docusaurus-theme-common/src/utils/__tests__/reactUtils.test.ts
+++ packages/docusaurus-theme-common/src/utils/__tests__/reactUtils.test.ts
@@ -3,12 +3,8 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @jest-environment jsdom
  */
 
-// Jest doesn't allow pragma below other comments. https://github.com/facebook/jest/issues/12573
-// eslint-disable-next-line header/header
 import {usePrevious} from '../reactUtils';
 import {renderHook} from '@testing-library/react-hooks';
 
