dd0537bdf775781ab597105f90382df3508bf320
Alexey Pyltsyn <lex61rus@gmail.com>
2022-03-23 12:07:32 +0300

    Set SVG icons container as ARIA hidden

--- packages/docusaurus-theme-classic/src/theme/CodeBlock/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/CodeBlock/index.tsx
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type { MouseEvent} from 'react';
+import type {MouseEvent} from 'react';
 import React, {isValidElement, useEffect, useState} from 'react';
 import clsx from 'clsx';
 import Highlight, {defaultProps, type Language} from 'prism-react-renderer';
@@ -178,18 +178,13 @@ export default function CodeBlock({
                 isCopied && styles.copyButtonCopied,
               )}
               onClick={handleCopyCode}>
-              <span className={styles.copyButtonIcons}>
-                <svg
-                  className={styles.copyButtonIcon}
-                  viewBox="0 0 24 24"
-                  aria-hidden="true">
+              <span className={styles.copyButtonIcons} aria-hidden="true">
+                <svg className={styles.copyButtonIcon} viewBox="0 0 24 24">
                   <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
                 </svg>
-
                 <svg
                   className={styles.copyButtonSuccessIcon}
-                  viewBox="0 0 24 24"
-                  aria-hidden="true">
+                  viewBox="0 0 24 24">
                   <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
                 </svg>
               </span>
