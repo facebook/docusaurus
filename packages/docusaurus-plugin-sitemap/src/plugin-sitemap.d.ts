0a66920b0bf76c68edfa0dffec6713fe9f1e47e4
Joshua Chen <sidachen2003@gmail.com>
2022-03-24 21:50:44 +0800

    docs

--- packages/docusaurus-plugin-sitemap/src/plugin-sitemap.d.ts
+++ packages/docusaurus-plugin-sitemap/src/plugin-sitemap.d.ts
@@ -9,9 +9,13 @@ import type {EnumChangefreq} from 'sitemap';
 
 export type PluginOptions = {
   /** @see https://www.sitemaps.org/protocol.html#xmlTagDefinitions */
-  changefreq?: EnumChangefreq;
+  changefreq: EnumChangefreq;
   /** @see https://www.sitemaps.org/protocol.html#xmlTagDefinitions */
-  priority?: number;
+  priority: number;
+  /**
+   * A list of glob patterns; matching route paths will be filtered from the
+   * sitemap. Note that you may need to include the base URL in here.
+   */
   ignorePatterns: string[];
 };
 
