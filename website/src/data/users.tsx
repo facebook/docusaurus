e6838dd7a648926171415cc4de143b464d7b5cb7
Martin Blom <martin@blom.org>
2022-03-26 15:11:41 +0100

    docs: add Divine WSF and Ghostly to showcase (#7020)
    * Added Divine WSF and Ghostly showcases.
    
    * optimize images
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -476,6 +476,14 @@ const Users: User[] = [
     tags: ['opensource'],
   },
   {
+    title: 'Divine Web Service Framework',
+    description: 'A divine collection of awesome web-related Node.js modules',
+    preview: require('./showcase/divine-wsf.png'),
+    website: 'https://divine-software.github.io/WSF/',
+    source: 'https://github.com/Divine-Software/WSF/tree/master/website',
+    tags: ['opensource'],
+  },
+  {
     title: 'Djamaile Rahamat',
     description: 'Djamaile Rahamat Blog on making cool stuff',
     preview: require('./showcase/djamaile.png'),
@@ -670,6 +678,14 @@ const Users: User[] = [
     website: 'https://www.froggit.fr/',
     source: 'https://lab.frogg.it/froggit/www/froggit.fr',
     tags: ['opensource', 'product'],
+  },
+  {
+    title: 'Ghostly',
+    description: 'A divine template/print formatter engine',
+    preview: require('./showcase/ghostly.png'),
+    website: 'https://divine-software.github.io/ghostly/',
+    source: 'https://github.com/Divine-Software/ghostly/tree/master/website',
+    tags: ['opensource'],
   },
   {
     title: 'Gladys Assistant',
