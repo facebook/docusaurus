8524966d95a65f6908a7c91e9c88c8c23844c26c
Joshua Chen <sidachen2003@gmail.com>
2022-03-27 12:44:36 +0800

    little refactor

--- packages/docusaurus-theme-classic/src/theme-classic.d.ts
+++ packages/docusaurus-theme-classic/src/theme-classic.d.ts
@@ -5,6 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+
+/// <reference types="@docusaurus/module-type-aliases" />
+/// <reference types="@docusaurus/plugin-content-docs" />
+/// <reference types="@docusaurus/plugin-content-blog" />
+/// <reference types="@docusaurus/plugin-content-pages" />
+
 declare module '@docusaurus/theme-classic' {
   export type Options = {
     customCss?: string | string[];
