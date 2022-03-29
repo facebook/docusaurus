177e8d7c02f0c99b0cb812c60e2f7a3384f23fad
Joshua Chen <sidachen2003@gmail.com>
2022-03-29 19:09:01 +0800

    docs: add firelordjs to showcase (#7059)
    * docs: add firelordjs to showcase
    
     docs: add firelordjs to showcase
    
    fixes
    
    kick CI
    
    Delete firelordjs.png
    
    Add files via upload
    
    * kick CI
    
    Co-authored-by: Acid Coder <5227509+tylim88@users.noreply.github.com>

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -623,6 +623,14 @@ const Users: User[] = [
     tags: ['opensource', 'design'],
   },
   {
+    title: 'FirelordJS',
+    description: 'Typescript Wrapper for Firestore',
+    preview: require('./showcase/firelordjs.png'),
+    website: 'https://firelordjs.com',
+    source: 'https://github.com/tylim88/FirelordJSDoc',
+    tags: ['opensource'],
+  },
+  {
     title: 'Flagsmith',
     description: 'Open Source Feature Flag and Remote Config Service',
     preview: require('./showcase/flagsmith.png'),
