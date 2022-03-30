f271f17e5ae21baa50a8e885e32ecab3d5602174
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2022-03-30 23:12:37 +0900

    Remove @reach/portal dep in favor of built-in portal
    Someone suggested to this to adjust some code and to prevent
    blocked on an external dep as commented.
    
    Signed-off-by: Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>

---
+++ packages/docusaurus/src/client/exports/Portal.tsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import {createPortal} from 'react-dom';
+
+type PortalProps = {
+  children: React.ReactNode;
+  type: string;
+};
+
+export function Portal({
+  children,
+  type = 'docusaurus-portal',
+}: PortalProps): React.ReactPortal {
+  const portalNode = React.useRef<HTMLElement | null>(null);
+  const [, forceUpdate] = React.useState<{[key: string]: unknown}>();
+  React.useEffect(() => {
+    portalNode.current = document.createElement(type);
+    document.body.appendChild(portalNode.current);
+    forceUpdate({});
+    return () => {
+      if (portalNode.current) {
+        document.body.removeChild(portalNode.current);
+      }
+    };
+  }, [type]);
+
+  return portalNode.current ? createPortal(children, portalNode.current) : null;
+}
