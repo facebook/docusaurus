ce2b631455c38693983bc48fb55abaefe5e5183f
Joshua Chen <sidachen2003@gmail.com>
2022-03-31 15:49:07 +0800

    fix(create): install types for JS template as well (#7078)

--- packages/create-docusaurus/templates/classic/docusaurus.config.js
+++ packages/create-docusaurus/templates/classic/docusaurus.config.js
@@ -13,6 +13,9 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
