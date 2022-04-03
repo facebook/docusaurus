dc18e12249a8b2bfd8890f670fec10055c6b4651
Joshua Chen <sidachen2003@gmail.com>
2022-04-03 14:50:41 +0800

    fix

--- packages/docusaurus/src/client/exports/__tests__/Interpolate.test.tsx
+++ packages/docusaurus/src/client/exports/__tests__/Interpolate.test.tsx
@@ -130,7 +130,7 @@ describe('<Interpolate>', () => {
         </Interpolate>,
       ),
     ).toThrowErrorMatchingInlineSnapshot(
-      `"The Docusaurus <Interpolate> component only accept simple string values"`,
+      `"The Docusaurus <Interpolate> component only accept simple string values. Received: {\\"type\\":\\"span\\",\\"key\\":null,\\"ref\\":null,\\"props\\":{\\"children\\":\\"aaa\\"},\\"_owner\\":null,\\"_store\\":{}}"`,
     );
   });
 });
