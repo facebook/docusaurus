0cb4736d505ee4c198ac9ec347ea54a00221950d
Joshua Chen <sidachen2003@gmail.com>
2022-04-10 19:00:55 +0800

    refactor: unify how MDX content types are represented

--- packages/docusaurus-mdx-loader/src/mdx-loader.d.ts
+++ packages/docusaurus-mdx-loader/src/mdx-loader.d.ts
@@ -16,3 +16,21 @@ export type MDXOptions = {
   beforeDefaultRemarkPlugins: MDXPlugin[];
   beforeDefaultRehypePlugins: MDXPlugin[];
 };
+
+export type LoadedMDXContent<FrontMatter, Metadata, Assets = undefined> = {
+  /** As verbatim declared in the MDX document. */
+  readonly frontMatter: FrontMatter;
+  /** As provided by the content plugin. */
+  readonly metadata: Metadata;
+  /** A list of TOC items (headings). */
+  readonly toc: readonly TOCItem[];
+  /** First h1 title before any content. */
+  readonly contentTitle: string | undefined;
+  /**
+   * Usually image assets that may be collocated like `./img/thumbnail.png`.
+   * The loader would also bundle these assets and the client should use these
+   * in priority.
+   */
+  readonly assets: Assets;
+  (): JSX.Element;
+};
