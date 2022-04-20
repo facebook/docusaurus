b15acadfdb5924d4c6135a0e660a8ad4b99ee498
Joshua Chen <sidachen2003@gmail.com>
2022-04-20 16:23:48 +0800

    use hash function

--- packages/docusaurus-plugin-content-blog/src/footnoteIDFixer.ts
+++ packages/docusaurus-plugin-content-blog/src/footnoteIDFixer.ts
@@ -6,52 +6,18 @@
  */
 
 import visit from 'unist-util-visit';
+import {simpleHash} from '@docusaurus/utils';
 import type {Transformer} from 'unified';
 import type {FootnoteReference, FootnoteDefinition} from 'mdast';
 
-declare global {
-  /* eslint-disable vars-on-top, no-var */
-  var fixerUsedUniqueStrings: string[];
-}
-
-interface IFootnoteIDMap {
-  originalID: string;
-  fixedID: string;
-}
-
-global.fixerUsedUniqueStrings = global.fixerUsedUniqueStrings ?? [];
-
 export default function plugin(): Transformer {
-  const idMaps: IFootnoteIDMap[] = [];
-  return (root) => {
+  return (root, vfile) => {
+    const suffix = `-${simpleHash(vfile.path!, 6)}`;
     visit(root, 'footnoteReference', (node: FootnoteReference) => {
-      const map: IFootnoteIDMap = {
-        originalID: node.identifier.concat(''),
-        fixedID: '',
-      };
-      const existingIDMap = idMaps.findIndex(
-        (idMap) => idMap.originalID === map.originalID,
-      );
-      if (existingIDMap > -1) {
-        idMaps.splice(existingIDMap, 1);
-      }
-      let randomString = '';
-      /* eslint-disable no-loop-func */
-      do {
-        randomString = Math.random().toString(36).substring(2, 8);
-      } while (
-        global.fixerUsedUniqueStrings.find((str) => str === randomString) !==
-        undefined
-      );
-      node.identifier = `${node.identifier}-${randomString}`;
-      map.fixedID = node.identifier;
-      idMaps.push(map);
+      node.identifier += suffix;
     });
     visit(root, 'footnoteDefinition', (node: FootnoteDefinition) => {
-      const idMap = idMaps.find((map) => map.originalID === node.identifier);
-      if (idMap) {
-        node.identifier = idMap.fixedID;
-      }
+      node.identifier += suffix;
     });
   };
 }
