4dda8a6f584c5d9c638a801964594efc3641ee6b
Joshua Chen <sidachen2003@gmail.com>
2022-04-28 22:00:33 +0800

    minor refactors

--- packages/docusaurus/src/client/PendingNavigation.tsx
+++ packages/docusaurus/src/client/PendingNavigation.tsx
@@ -60,14 +60,16 @@ class PendingNavigation extends React.Component<Props, State> {
 
     // Load data while the old screen remains.
     preload(nextLocation.pathname)
-      .then(() => this.setState({nextRouteHasLoaded: true}))
+      .then(() => {
+        this.setState({nextRouteHasLoaded: true});
+        this.routeUpdateCb?.();
+      })
       .catch((e) => console.warn(e));
     return false;
   }
 
   override render(): JSX.Element {
     const {children, location} = this.props;
-    this.routeUpdateCb?.();
     // Use a controlled <Route> to trick all descendants into rendering the old
     // location.
     return (
