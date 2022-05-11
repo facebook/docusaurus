2c1ae934f313bfc9631d03e0b34b75b52cdcd241
Rui Peres <sauron.is.alive@gmail.com>
2022-05-11 20:20:33 +0100

    Added vital to showcase

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -2162,6 +2162,14 @@ const Users: User[] = [
     tags: ['product'],
   },
   {
+    title: 'Vital',
+    description: 'Wearables and Lab test API for digital health companies.',
+    preview: require('./showcase/vital.png'),
+    website: 'https://docs.tryvital.io',
+    source: 'https://github.com/tryVital/vital-docs-new',
+    tags: ['opensource', 'product'],
+  },
+  {
     title: 'Vue NodeGui',
     description: 'A cross-platform native desktop app library',
     preview: require('./showcase/vue-nodegui.png'),
