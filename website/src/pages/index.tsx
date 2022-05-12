fa10d1b1a4e18f2fcc088e736b303b8c2d5c4d93
Matthias Koch <ithrowexceptions@gmail.com>
2022-05-12 04:30:59 +0200

    fix(website): use react-lite-youtube-embed for lazy YouTube video (#7390)
    * fix(website): use react-lite-youtube-embed for lazy YouTube video
    
    * fix(website): use react-lite-youtube-embed for lazy YouTube video
    
    * fix(website): use react-lite-youtube-embed for lazy YouTube video
    
    * Update multiple-sidebars.md
    
    * Update installation.md
    
    * refactor intro
    
    * rename file back
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -18,6 +18,8 @@ import Tweet from '@site/src/components/Tweet';
 import Tweets, {type TweetItem} from '@site/src/data/tweets';
 import Quotes from '@site/src/data/quotes';
 import Features, {type FeatureItem} from '@site/src/data/features';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 
 import clsx from 'clsx';
 
@@ -162,14 +164,12 @@ function VideoContainer() {
             <Translate>Check it out in the intro video</Translate>
           </h2>
           <div className="video-container">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/_An9EsKPhp0"
+            <LiteYouTubeEmbed
+              id="_An9EsKPhp0"
+              params="autoplay=1&autohide=1&showinfo=0&rel=0"
               title="Explain Like I'm 5: Docusaurus"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              loading="lazy"
+              poster="maxresdefault"
+              webp
             />
           </div>
         </div>
