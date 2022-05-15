7830d55bb1a5f3378ae0f02ec4411fce321b2d0d
Ori Shalom <ori-shalom@users.noreply.github.com>
2022-05-15 16:08:28 +0300

    delay sending event to gtag to next tick

--- packages/docusaurus-plugin-google-gtag/src/gtag.ts
+++ packages/docusaurus-plugin-google-gtag/src/gtag.ts
@@ -17,15 +17,17 @@ const clientModule: ClientModule = {
     if (previousLocation && location.pathname !== previousLocation.pathname) {
       // Always refer to the variable on window in case it gets overridden
       // elsewhere.
-      window.gtag('config', trackingID, {
-        page_path: location.pathname,
-        page_title: document.title,
-      });
-      window.gtag('event', 'page_view', {
-        page_title: document.title,
-        page_location: window.location.href,
-        page_path: location.pathname,
-      });
+      setTimeout(() => {
+        window.gtag('config', trackingID, {
+          page_path: location.pathname,
+          page_title: document.title,
+        });
+        window.gtag('event', 'page_view', {
+          page_title: document.title,
+          page_location: window.location.href,
+          page_path: location.pathname,
+        });
+      };
     }
   },
 };
