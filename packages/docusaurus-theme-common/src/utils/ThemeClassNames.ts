fcc5451d80612d6f30f074ab9780a71b8b371dc9
sebastienlorber <lorber.sebastien@gmail.com>
2022-06-03 15:50:02 +0200

    add admonition default label translations

--- packages/docusaurus-theme-common/src/utils/ThemeClassNames.ts
+++ packages/docusaurus-theme-common/src/utils/ThemeClassNames.ts
@@ -37,6 +37,8 @@ export const ThemeClassNames = {
     backToTopButton: 'theme-back-to-top-button',
     codeBlock: 'theme-code-block',
     admonition: 'theme-admonition',
+    admonitionType: (type: 'note' | 'tip' | 'danger' | 'info' | 'caution') =>
+      `theme-admonition-${type}`,
   },
   layout: {
     // TODO add other stable classNames here
