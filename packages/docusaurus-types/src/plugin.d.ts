faa3872377b3c70438acc7311f9e390fa167d9e4
sebastienlorber <lorber.sebastien@gmail.com>
2022-07-01 11:49:02 +0200

    missing import

--- packages/docusaurus-types/src/plugin.d.ts
+++ packages/docusaurus-types/src/plugin.d.ts
@@ -14,6 +14,7 @@ import type {HelmetServerState} from 'react-helmet-async';
 import type {ThemeConfig} from './config';
 import type {LoadContext, Props} from './context';
 import type {SwizzleConfig} from './swizzle';
+import type {RouteConfig} from './routing';
 
 export type PluginOptions = {id?: string} & {[key: string]: unknown};
 
