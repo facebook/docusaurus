2e83a1b5225a598cbb5726392ed08bb0975530aa
Huy Nguyen <Smilefounder@users.noreply.github.com>
2022-07-07 14:58:11 +0700

    docs: add Mixcore CMS to showcase (#7730)
    * Update users.tsx
    
    * add screenshot
    
    * Update website/src/data/users.tsx
    
    * Update website/src/data/users.tsx
    
    * optimize image
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- website/src/data/users.tsx
+++ website/src/data/users.tsx
@@ -1180,6 +1180,15 @@ const Users: User[] = [
     tags: ['design'],
   },
   {
+    title: 'Mixcore CMS',
+    description:
+      'Fully Open Source ASP.Net Core / Dotnet Core CMS UI Toolkit',
+    preview: require('./showcase/mixcore.png'),
+    website: 'https://docs.mixcore.org/',
+    source: 'https://github.com/mixcore/docs/tree/master/website',
+    tags: ['opensource'],
+  },
+  {
     title: 'moja global',
     description:
       'A non-profit project to accurately and affordably estimate greenhouse gas emissions and removals from the AFOLU sector',
