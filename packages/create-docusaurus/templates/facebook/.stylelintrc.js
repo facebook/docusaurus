9150c7aefc7bd871f449145bee5bc70767d09eca
Yangshun Tay <tay.yang.shun@gmail.com>
2022-07-28 14:48:03 +0800

    chore: update FB template headers (#7842)

--- packages/create-docusaurus/templates/facebook/.stylelintrc.js
+++ packages/create-docusaurus/templates/facebook/.stylelintrc.js
@@ -1,8 +1,7 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * @format
  */
 
 module.exports = {
--- packages/create-docusaurus/templates/facebook/docusaurus.config.js
+++ packages/create-docusaurus/templates/facebook/docusaurus.config.js
@@ -1,8 +1,5 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
  *
  * @format
  */
