7b6f1cb41c660e620f8f822365ce31cab302728d
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2022-09-11 07:22:25 +0530

    sanjaiyan: optimize

--- packages/docusaurus/src/webpack/templates/ssr.html.template.ts
+++ packages/docusaurus/src/webpack/templates/ssr.html.template.ts
@@ -16,10 +16,11 @@ export default `
     <% }); %>
     <%~ it.headTags %>
     <% it.stylesheets.forEach((stylesheet) => { %>
+      <link rel="preload" href="<%= it.baseUrl %><%= stylesheet %>" as="style" />
       <link rel="stylesheet" href="<%= it.baseUrl %><%= stylesheet %>" />
     <% }); %>
     <% it.scripts.forEach((script) => { %>
-      <link rel="preload" href="<%= it.baseUrl %><%= script %>" as="script">
+      <script src="<%= it.baseUrl %><%= script %>" defer></script>
     <% }); %>
   </head>
   <body <%~ it.bodyAttributes %>>
@@ -27,9 +28,6 @@ export default `
     <div id="__docusaurus">
       <%~ it.appHtml %>
     </div>
-    <% it.scripts.forEach((script) => { %>
-      <script src="<%= it.baseUrl %><%= script %>"></script>
-    <% }); %>
     <%~ it.postBodyTags %>
   </body>
 </html>
