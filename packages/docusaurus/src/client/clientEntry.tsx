1453ef6ca73a20efdbc5ff762b65aa979fbf3a24
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2022-09-19 01:34:49 +0530

    revert react 18

--- packages/docusaurus/src/client/clientEntry.tsx
+++ packages/docusaurus/src/client/clientEntry.tsx
@@ -5,14 +5,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
-import ExecutionEnvironment from "./exports/ExecutionEnvironment";
-import App from "./App";
-import preload from "./preload";
-import docusaurus from "./docusaurus";
+import ExecutionEnvironment from './exports/ExecutionEnvironment';
+import App from './App';
+import preload from './preload';
+import docusaurus from './docusaurus';
 
 declare global {
   interface NodeModule {
@@ -28,55 +29,18 @@ if (ExecutionEnvironment.canUseDOM) {
   // first-load experience.
   // For development, there is no existing markup so we had to render it.
   // We also preload async component to avoid first-load loading screen.
-
-  if (React.version.includes("18.")) {
-    if (process.env.NODE_ENV === "production") {
-      const { hydrateRoot } = require("react-dom/client");
-
-      preload(window.location.pathname).then(() => {
-        hydrateRoot(
-          document.getElementById("__docusaurus"),
-          <HelmetProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </HelmetProvider>
-        );
-      });
-    } else {
-      const { createRoot } = require("react-dom/client");
-
-      preload(window.location.pathname).then(() => {
-        const docReactRoot = createRoot(
-          document.getElementById("__docusaurus")
-        );
-
-        docReactRoot.render(
-          <HelmetProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </HelmetProvider>
-        );
-      });
-    }
-  } else {
-    const ReactDOM = require("react-dom");
-    const renderMethod =
-      process.env.NODE_ENV === "production"
-        ? ReactDOM.hydrate
-        : ReactDOM.render;
-    preload(window.location.pathname).then(() => {
-      renderMethod(
-        <HelmetProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </HelmetProvider>,
-        document.getElementById("__docusaurus")
-      );
-    });
-  }
+  const renderMethod =
+    process.env.NODE_ENV === 'production' ? ReactDOM.hydrate : ReactDOM.render;
+  preload(window.location.pathname).then(() => {
+    renderMethod(
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>,
+      document.getElementById('__docusaurus'),
+    );
+  });
 
   // Webpack Hot Module Replacement API
   if (module.hot) {
