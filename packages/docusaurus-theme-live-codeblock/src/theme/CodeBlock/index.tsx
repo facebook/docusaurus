0c7d58e027ca56e9ea45c15d013117e21530af47
Martin Turoci <martin.turoci@h2o.ai>
2022-10-11 20:48:44 +0200

    fix: Add err boundary to the live code block #8009.

--- packages/docusaurus-theme-live-codeblock/src/theme/CodeBlock/index.tsx
+++ packages/docusaurus-theme-live-codeblock/src/theme/CodeBlock/index.tsx
@@ -6,15 +6,21 @@
  */
 
 import React from 'react';
+import ErrorBoundary from '@docusaurus/ErrorBoundary';
 import Playground from '@theme/Playground';
 import ReactLiveScope from '@theme/ReactLiveScope';
 import CodeBlock, {type Props} from '@theme-init/CodeBlock';
+import ErrorPageContent from '@theme/ErrorPageContent';
 
 const withLiveEditor = (Component: typeof CodeBlock) => {
   function WrappedComponent(props: Props) {
     if (props.live) {
-      // @ts-expect-error: we have deliberately widened the type of language prop
-      return <Playground scope={ReactLiveScope} {...props} />;
+      return (
+        <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
+          {/* @ts-expect-error: we have deliberately widened the type of language prop */}
+          <Playground scope={ReactLiveScope} {...props} />
+        </ErrorBoundary>
+      );
     }
 
     return <Component {...props} />;
