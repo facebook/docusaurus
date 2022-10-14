14f4f8916171a97f2313f5531cbfb6eb485f46bc
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-10-13 22:00:56 -0300

    feat(theme-classic): add translations for mobile menu button

--- packages/docusaurus-theme-classic/src/theme/Navbar/MobileSidebar/Toggle/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Navbar/MobileSidebar/Toggle/index.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import {useNavbarMobileSidebar} from '@docusaurus/theme-common/internal';
+import {translate} from '@docusaurus/Translate';
 import IconMenu from '@theme/Icon/Menu';
 
 export default function MobileSidebarToggle(): JSX.Element {
@@ -14,7 +15,12 @@ export default function MobileSidebarToggle(): JSX.Element {
   return (
     <button
       onClick={toggle}
-      aria-label="Navigation bar toggle"
+      aria-label={translate({
+        id: 'theme.docs.sidebar.toggleSidebarButtonAriaLabel',
+        message: 'Toggle navigation bar',
+        description:
+          'The ARIA label for hamburger menu button of mobile navigation',
+      })}
       aria-expanded={shown}
       className="navbar__toggle clean-btn"
       type="button">
