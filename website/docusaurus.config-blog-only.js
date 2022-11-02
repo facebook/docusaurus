d531e500ab81699db10f1bdb5316eaea9dedf350
Sébastien Lorber <slorber@users.noreply.github.com>
2022-11-02 17:03:00 +0100

    fix(create-docusaurus): improve init template misleading doc + add Docuaurus social card (#8279)

--- website/docusaurus.config-blog-only.js
+++ website/docusaurus.config-blog-only.js
@@ -41,7 +41,7 @@ module.exports = {
     ],
   ],
   themeConfig: {
-    image: 'img/docusaurus-soc.png',
+    image: 'img/docusaurus-social-card.jpg',
     algolia: {
       appId: 'X1Z85QJPUV',
       apiKey: 'bf7211c161e8205da2f933a02534105a',
