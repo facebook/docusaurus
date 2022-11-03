2c98f9186a3bcb71c5ff4380f53898d79ea0c516
sebastienlorber <lorber.sebastien@gmail.com>
2022-11-03 18:21:04 +0100

    fix TOC?

--- packages/docusaurus-mdx-loader/src/index.ts
+++ packages/docusaurus-mdx-loader/src/index.ts
@@ -7,13 +7,11 @@
 
 import {mdxLoader} from './loader';
 
+import type {TOCItem as TOCItemImported} from './remark/toc';
+
 export default mdxLoader;
 
-export type TOCItem = {
-  readonly value: string;
-  readonly id: string;
-  readonly level: number;
-};
+export type TOCItem = TOCItemImported;
 
 export type LoadedMDXContent<FrontMatter, Metadata, Assets = undefined> = {
   /** As verbatim declared in the MDX document. */
--- packages/docusaurus-mdx-loader/src/remark/toc/index.ts
+++ packages/docusaurus-mdx-loader/src/remark/toc/index.ts
@@ -10,26 +10,35 @@ import traverse from '@babel/traverse';
 import stringifyObject from 'stringify-object';
 import toString from 'mdast-util-to-string';
 import visit from 'unist-util-visit';
+import {valueToEstree} from 'estree-util-value-to-estree'; // TODO upgrade to v2 ESM?
 import {toValue} from '../utils';
-
 import type {Identifier} from '@babel/types';
-import type {TOCItem} from '../..';
 import type {Node, Parent} from 'unist';
 import type {Heading, Literal} from 'mdast';
 import type {Transformer} from 'unified';
+
+export type TOCItem = {
+  readonly value: string;
+  readonly id: string;
+  readonly level: number;
+};
 
 const parseOptions: ParserOptions = {
   plugins: ['jsx'],
   sourceType: 'module',
 };
 
-const name = 'toc';
-
-const isImport = (child: Node): child is Literal => child.type === 'import';
+const isImport = (child: any): child is Literal =>
+  child.type === 'mdxjsEsm' && child.value.startsWith('import');
 const hasImports = (index: number) => index > -1;
-const isExport = (child: Node): child is Literal => child.type === 'export';
+const isExport = (child: any): child is Literal =>
+  child.type === 'mdxjsEsm' && child.value.startsWith('export');
 
-const isTarget = (child: Literal) => {
+interface PluginOptions {
+  name?: string;
+}
+
+const isTarget = (child: Literal, name: string) => {
   let found = false;
   const ast = parse(child.value, parseOptions);
   traverse(ast, {
@@ -42,24 +51,20 @@ const isTarget = (child: Literal) => {
   return found;
 };
 
-const getOrCreateExistingTargetIndex = (children: Node[]) => {
+const getOrCreateExistingTargetIndex = (children: Node[], name: string) => {
   let importsIndex = -1;
   let targetIndex = -1;
 
   children.forEach((child, index) => {
     if (isImport(child)) {
       importsIndex = index;
-    } else if (isExport(child) && isTarget(child)) {
+    } else if (isExport(child) && isTarget(child, name)) {
       targetIndex = index;
     }
   });
 
   if (targetIndex === -1) {
-    const target = {
-      default: false,
-      type: 'export',
-      value: `export const ${name} = [];`,
-    };
+    const target = createExportNode(name, []);
 
     targetIndex = hasImports(importsIndex) ? importsIndex + 1 : 0;
     children.splice(targetIndex, 0, target);
@@ -68,15 +73,17 @@ const getOrCreateExistingTargetIndex = (children: Node[]) => {
   return targetIndex;
 };
 
-export default function plugin(): Transformer {
+export default function plugin(options: PluginOptions = {}): Transformer {
+  const name = options.name || 'toc';
+
   return (root) => {
     const headings: TOCItem[] = [];
 
-    visit(root, 'heading', (child: Heading, index, parent) => {
+    visit(root, 'heading', (child: Heading) => {
       const value = toString(child);
 
-      // depth: 1 headings are titles and not included in the TOC
-      if (parent !== root || !value || child.depth < 2) {
+      // depth:1 headings are titles and not included in the TOC
+      if (!value || child.depth < 2) {
         return;
       }
 
@@ -87,12 +94,44 @@ export default function plugin(): Transformer {
       });
     });
     const {children} = root as Parent<Literal>;
-    const targetIndex = getOrCreateExistingTargetIndex(children);
+    const targetIndex = getOrCreateExistingTargetIndex(children, name);
 
-    if (headings.length) {
-      children[targetIndex]!.value = `export const ${name} = ${stringifyObject(
-        headings,
-      )};`;
+    if (headings?.length) {
+      children[targetIndex] = createExportNode(name, headings);
     }
+  };
+}
+
+function createExportNode(name: string, object: any) {
+  return {
+    type: 'mdxjsEsm',
+    value: `export const ${name} = ${stringifyObject(object)}`,
+    data: {
+      estree: {
+        type: 'Program',
+        body: [
+          {
+            type: 'ExportNamedDeclaration',
+            declaration: {
+              type: 'VariableDeclaration',
+              declarations: [
+                {
+                  type: 'VariableDeclarator',
+                  id: {
+                    type: 'Identifier',
+                    name,
+                  },
+                  init: valueToEstree(object),
+                },
+              ],
+              kind: 'const',
+            },
+            specifiers: [],
+            source: null,
+          },
+        ],
+        sourceType: 'module',
+      },
+    },
   };
 }
