029417154c4469a49d79c1c9605de4ea60a15bae
Muhammad Hammad <33136628+mhnaeem@users.noreply.github.com>
2022-11-17 05:22:12 -0500

    fix(core): normalize input for poll option (#8342)
    Fixes https://github.com/facebook/docusaurus/issues/8306

--- packages/docusaurus/bin/docusaurus.mjs
+++ packages/docusaurus/bin/docusaurus.mjs
@@ -104,6 +104,23 @@ cli
   )
   .action(deploy);
 
+/**
+ * @param {string | undefined} value
+ * @returns {boolean | number}
+ */
+function normalizePollValue(value) {
+  if (value === undefined || value === '') {
+    return false;
+  }
+
+  const parsedIntValue = Number.parseInt(value, 10);
+  if (!Number.isNaN(parsedIntValue)) {
+    return parsedIntValue;
+  }
+
+  return value === 'true';
+}
+
 cli
   .command('start [siteDir]')
   .description('Start the development server.')
@@ -122,6 +139,7 @@ cli
   .option(
     '--poll [interval]',
     'use polling rather than watching for reload (default: false). Can specify a poll interval in milliseconds',
+    normalizePollValue,
   )
   .option(
     '--no-minify',
