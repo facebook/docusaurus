25feea3eccd4a07a7b3a4320447416dcd6b66c96
sebastienlorber <lorber.sebastien@gmail.com>
2022-11-24 17:55:18 +0100

    cleanup admonition implementation

--- packages/docusaurus-mdx-loader/src/remark/admonitions/index.ts
+++ packages/docusaurus-mdx-loader/src/remark/admonitions/index.ts
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 import visit from 'unist-util-visit';
-// import toHast from 'mdast-util-to-hast';
 import type {Transformer, Processor, Plugin} from 'unified';
 
 // @ts-expect-error: TODO see https://github.com/microsoft/TypeScript/issues/49721
@@ -16,7 +15,7 @@ import type {ContainerDirective} from 'mdast-util-directive';
 // Maybe we'll want to provide different tags for different admonition types?
 // Also maybe rename "keywords" to "types"?
 export type AdmonitionOptions = {
-  tag: string;
+  tag: string; // TODO remove this config option, now unused with md directives
   keywords: string[];
   extendDefaults: boolean;
 };
@@ -36,10 +35,6 @@ export const DefaultAdmonitionOptions: AdmonitionOptions = {
   ],
   extendDefaults: false, // TODO make it true by default: breaking change
 };
-
-function escapeRegExp(s: string): string {
-  return s.replace(/[-[\]{}()*+?.\\^$|/]/g, '\\$&');
-}
 
 function normalizeOptions(
   providedOptions: Partial<AdmonitionOptions>,
@@ -74,285 +69,36 @@ const plugin: Plugin = function plugin(
   return async (tree, file) => {
     const {h} = await import('hastscript');
 
-    visit(
-      tree,
-      'containerDirective',
-      (containerDirective: ContainerDirective) => {
-        //
-      },
-    );
+    visit(tree, 'containerDirective', (directive: ContainerDirective) => {
+      const isAdmonition = keywords.includes(directive.name);
 
-    visit(tree, (node: any) => {
-      if (
-        // TODO we only need containerDirective for admonitions?
-        node.type === 'containerDirective'
-      ) {
-        const isAdmonition = keywords.includes(node.name);
-
-        if (!isAdmonition) {
-          return;
-        }
-
-        /*
-        const str = JSON.stringify(node, null, 2);
-        if (str.includes('TEST')) {
-          console.log('TEST', node.name, JSON.stringify(node, null, 2));
-        }
-
-         */
-
-        const data = node.data || (node.data = {});
-        const tagName = 'admonition';
-
-        const hasDirectiveLabel =
-          node.children?.[0].data?.directiveLabel === true;
-
-        if (hasDirectiveLabel) {
-          const dl = node.children[0];
-          dl.data.hName = 'mdxAdmonitionTitle';
-          dl.data.hProperties = dl.data.hProperties ?? {};
-          dl.data.hProperties.mdxType = 'mdxAdmonitionTitle';
-          // console.log('dl', dl);
-        }
-
-        /*
-        const directiveLabel = hasDirectiveLabel
-          ? node.children.shift()
-          : undefined;
-
-        const directiveLabelHast = directiveLabel
-          ? toHast(directiveLabel)
-          : undefined;
-
-        if (directiveLabel) {
-          console.log('directiveLabel', directiveLabel);
-          console.log('directiveLabelHast', directiveLabelHast);
-        }
-
-         */
-
-        const title = node.attributes?.title; // ?? directiveLabelHast;
-        // file.message('testMessageReason');
-
-        // title && console.log('title', title);
-
-        data.hName = tagName;
-        data.hProperties = h(tagName, {
-          ...node.attributes,
-          type: node.name,
-          title,
-          // title: 'test hardcoded title',
-        }).properties;
-
-        // data.hProperties.title =  h('code', 'hardcoded code value test');
-        // data.hProperties.title = title;
-
-        // data.hChildren = [directiveLabelHast].filter(Boolean);
-
-        /*
-        data.hChildren = [
-          // directiveLabel,
-          {
-            type: 'text',
-            value: 'Hello',
-            position: {
-              start: {
-                line: 1,
-                column: 3,
-                offset: 2,
-              },
-              end: {
-                line: 1,
-                column: 8,
-                offset: 7,
-              },
-            },
-          },
-        ];
-
-         */
+      if (!isAdmonition) {
+        return;
       }
+
+      const data = directive.data || (directive.data = {});
+      const tagName = 'admonition';
+
+      const hasDirectiveLabel =
+        directive.children?.[0].data?.directiveLabel === true;
+
+      // TODO this is our old MDX v1 <mdxAdmonitionTitle> workaround
+      // in v2 we should transform the whole directive to mdxJsxFlowElement
+      if (hasDirectiveLabel) {
+        const directiveLabel = directive.children[0];
+        directiveLabel.data.hName = 'mdxAdmonitionTitle';
+        directiveLabel.data.hProperties = directiveLabel.data.hProperties ?? {};
+        directiveLabel.data.hProperties.mdxType = 'mdxAdmonitionTitle';
+      }
+
+      data.hName = tagName;
+      data.hProperties = h(tagName, {
+        ...directive.attributes,
+        type: directive.name,
+        title: directive.attributes?.title,
+      }).properties;
     });
   };
-
-  // TODO markdown directive tag is not configurable?
-  // Option to remove
-  // const tag = escapeRegExp(options.tag);
-
-  /*
-  const regex = new RegExp(`${tag}(${keywords})(?: *(.*))?\n`);
-  const escapeTag = new RegExp(escapeRegExp(`\\${options.tag}`), 'g');
-  const keywords = Object.values(options.keywords).map(escapeRegExp).join('|');
-  const nestingChar = escapeRegExp(options.tag.slice(0, 1));
-  const tag = escapeRegExp(options.tag);
-
-  // resolve th nesting level of an opening tag
-  // ::: -> 0, :::: -> 1, ::::: -> 2 ...
-  const nestingLevelRegex = new RegExp(
-    `^${tag}(?<nestingLevel>${nestingChar}*)`,
-  );
-
-  const regex = new RegExp(`${tag}${nestingChar}*(${keywords})(?: *(.*))?\n`);
-  const escapeTag = new RegExp(
-    escapeRegExp(`\\${options.tag}${options.tag.slice(0, 1)}*`),
-    'g',
-  );
-
-  // The tokenizer is called on blocks to determine if there is an admonition
-  // present and create tags for it
-  function blockTokenizer(this: any, eat: any, value: string, silent: boolean) {
-    // Stop if no match or match does not start at beginning of line
-    const match = regex.exec(value);
-    if (!match || match.index !== 0) {
-      return false;
-    }
-    // If silent return the match
-    if (silent) {
-      return true;
-    }
-
-    const now = eat.now();
-    const [opening, keyword, title] = match as string[] as [
-      string,
-      string,
-      string,
-    ];
-    const food = [];
-    const content = [];
-    // get the nesting level of the opening tag
-    const openingLevel =
-      nestingLevelRegex.exec(opening)!.groups!.nestingLevel!.length;
-    // used as a stack to keep track of nested admonitions
-    const nestingLevels: number[] = [openingLevel];
-
-    let newValue = value;
-    // consume lines until a closing tag
-    let idx = newValue.indexOf(NEWLINE);
-    while (idx !== -1) {
-      // grab this line and eat it
-      const next = newValue.indexOf(NEWLINE, idx + 1);
-      const line =
-        next !== -1 ? newValue.slice(idx + 1, next) : newValue.slice(idx + 1);
-      food.push(line);
-      newValue = newValue.slice(idx + 1);
-      const nesting = nestingLevelRegex.exec(line);
-      idx = newValue.indexOf(NEWLINE);
-      if (!nesting) {
-        content.push(line);
-        continue;
-      }
-      const tagLevel = nesting.groups!.nestingLevel!.length;
-      // first level
-      if (nestingLevels.length === 0) {
-        nestingLevels.push(tagLevel);
-        content.push(line);
-        continue;
-      }
-      const currentLevel = nestingLevels[nestingLevels.length - 1]!;
-      if (tagLevel < currentLevel) {
-        // entering a nested admonition block
-        nestingLevels.push(tagLevel);
-      } else if (tagLevel === currentLevel) {
-        // closing a nested admonition block
-        nestingLevels.pop();
-        // the closing tag is NOT part of the content
-        if (nestingLevels.length === 0) {
-          break;
-        }
-      }
-      content.push(line);
-    }
-
-    // consume the processed tag and replace escape sequences
-    const contentString = content.join(NEWLINE).replace(escapeTag, options.tag);
-    const add = eat(opening + food.join(NEWLINE));
-
-    // parse the content in block mode
-    const exit = this.enterBlock();
-    const contentNodes = this.tokenizeBlock(contentString, now);
-    exit();
-
-    const titleNodes = this.tokenizeInline(title, now);
-
-    const isSimpleTextTitle =
-      titleNodes.length === 1 && titleNodes[0].type === 'text';
-
-    const element = {
-      type: admonitionNodeType,
-      data: {
-        // hName/hProperties come from HAST
-        // See https://github.com/syntax-tree/mdast-util-to-hast#fields-on-nodes
-        hName: 'admonition',
-        hProperties: {
-          ...(title && isSimpleTextTitle && {title}),
-          type: keyword,
-        },
-      },
-      children: [
-        // For titles containing MDX syntax: create a custom element. The theme
-        // component will extract it and render it nicely.
-        //
-        // Temporary workaround, because it's complex in MDX v1 to emit
-        // interpolated JSX prop syntax (title={<>my <code>title</code></>}).
-        // For this reason, we use children instead of the title prop.
-        title &&
-          !isSimpleTextTitle && {
-            type: admonitionNodeType,
-            data: {
-              hName: 'mdxAdmonitionTitle',
-              hProperties: {},
-            },
-            children: titleNodes,
-          },
-        ...contentNodes,
-      ].filter(Boolean),
-    };
-
-    return add(element);
-  }
-
-  // add tokenizer to parser after fenced code blocks
-  const Parser = this.Parser!.prototype;
-  Parser.blockTokenizers.admonition = blockTokenizer;
-  Parser.blockMethods.splice(
-    Parser.blockMethods.indexOf('fencedCode') + 1,
-    0,
-    'admonition',
-  );
-  Parser.interruptParagraph.splice(
-    Parser.interruptParagraph.indexOf('fencedCode') + 1,
-    0,
-    ['admonition'],
-  );
-  Parser.interruptList.splice(
-    Parser.interruptList.indexOf('fencedCode') + 1,
-    0,
-    ['admonition'],
-  );
-  Parser.interruptBlockquote.splice(
-    Parser.interruptBlockquote.indexOf('fencedCode') + 1,
-    0,
-    ['admonition'],
-  );
-  */
-
-  /*
-
-  return (root) => {
-    // escape everything except admonitionHTML nodes
-    visit(
-      root,
-      (node: unknown): node is Literal =>
-        (node as Literal | undefined)?.type !== admonitionNodeType,
-      (node: Literal) => {
-        if (node.value) {
-          node.value = node.value.replace(escapeTag, options.tag);
-        }
-      },
-    );
-  };
-
-   */
 };
 
 export default plugin;
