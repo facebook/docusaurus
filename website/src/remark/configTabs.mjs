6a6782260f7552630104be66a24afbdc33119c3e
sebastienlorber <lorber.sebastien@gmail.com>
2022-12-01 15:37:47 +0100

    remove useless escaping line

--- website/src/remark/configTabs.mjs
+++ website/src/remark/configTabs.mjs
@@ -37,8 +37,6 @@ export default function plugin() {
         .trim()
         .replace(/^.*?= /, '')
         .replace(/;$/, '');
-       
-      // .replace(/([`$\\])/g, '\\$1');
 
       const importNode = {
         type: 'mdxjsEsm',
