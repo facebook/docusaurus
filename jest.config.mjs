1d4d1033f71a49fddfbe063362c9e8d5789bac4b
sebastienlorber <lorber.sebastien@gmail.com>
2022-12-02 16:13:25 +0100

    vendor remark-gfm@3.0.1.js

--- jest.config.mjs
+++ jest.config.mjs
@@ -87,6 +87,7 @@ export default {
     '^remark$': '<rootDir>/jest/vendor/remark@14.0.2.js',
     '^remark-mdx$': '<rootDir>/jest/vendor/remark-mdx@2.1.5.js',
     '^remark-directive$': '<rootDir>/jest/vendor/remark-directive@2.0.1.js',
+    '^remark-gfm$': '<rootDir>/jest/vendor/remark-gfm@3.0.1.js',
     '^estree-util-value-to-estree$':
       '<rootDir>/jest/vendor/estree-util-value-to-estree@2.1.0.js',
     '^mdast-util-to-string$':
