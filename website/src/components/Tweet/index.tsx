45f3e87356082a5a5dae3f08493773326627c261
Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>
2022-12-15 17:19:38 +0530

    docs: fix website home testimonials broken image URLs (#8443)
    Co-authored-by: Sébastien Lorber <slorber@users.noreply.github.com>
    closes undefined

--- website/src/components/Tweet/index.tsx
+++ website/src/components/Tweet/index.tsx
@@ -17,8 +17,8 @@ export interface Props {
   handle: string;
   name: string;
   content: ReactNode;
-  avatar: string;
   date: string;
+  githubUsername: string;
 }
 
 export default function Tweet({
@@ -26,8 +26,8 @@ export default function Tweet({
   handle,
   name,
   content,
-  avatar,
   date,
+  githubUsername,
 }: Props): JSX.Element {
   return (
     <div className={clsx('card', styles.tweet)}>
@@ -36,7 +36,7 @@ export default function Tweet({
           <img
             alt={name}
             className="avatar__photo"
-            src={avatar}
+            src={`https://unavatar.io/twitter/${handle}?fallback=https://github.com/${githubUsername}.png`}
             width="48"
             height="48"
             loading="lazy"
