31cf3228e84fac6a36f6b2ca937880a9c089b3d9
sebastienlorber <lorber.sebastien@gmail.com>
2023-01-06 19:07:08 +0100

    remove useless mdx <Code> inline render logic

--- packages/docusaurus-theme-classic/src/theme/MDXComponents/Code.tsx
+++ packages/docusaurus-theme-classic/src/theme/MDXComponents/Code.tsx
@@ -10,51 +10,7 @@ import React, {isValidElement} from 'react';
 import CodeBlock from '@theme/CodeBlock';
 import type {Props} from '@theme/MDXComponents/Code';
 
+// Keep this component for now, might be useful to swizzle it?
 export default function MDXCode(props: Props): JSX.Element {
-  const inlineElements: (string | undefined)[] = [
-    'a',
-    'abbr',
-    'b',
-    'br',
-    'button',
-    'cite',
-    'code',
-    'del',
-    'dfn',
-    'em',
-    'i',
-    'img',
-    'input',
-    'ins',
-    'kbd',
-    'label',
-    'object',
-    'output',
-    'q',
-    'ruby',
-    's',
-    'small',
-    'span',
-    'strong',
-    'sub',
-    'sup',
-    'time',
-    'u',
-    'var',
-    'wbr',
-  ];
-  const shouldBeInline = React.Children.toArray(props.children).every(
-    (el) =>
-      (typeof el === 'string' && !el.includes('\n')) ||
-      (isValidElement(el) &&
-        inlineElements.includes(
-          (el.props as {mdxType: string} | null)?.mdxType,
-        )),
-  );
-
-  return shouldBeInline ? (
-    <code {...props} />
-  ) : (
-    <CodeBlock {...(props as ComponentProps<typeof CodeBlock>)} />
-  );
+  return <CodeBlock {...(props as ComponentProps<typeof CodeBlock>)} />;
 }
