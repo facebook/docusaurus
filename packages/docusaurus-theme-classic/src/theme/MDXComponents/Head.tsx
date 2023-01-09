875b998aaaec59f548df6a9f9d1a8ba8c05fa6d9
Joshua Chen <sidachen2003@gmail.com>
2023-01-09 17:12:02 -0500

    chore: upgrade dependencies

--- packages/docusaurus-theme-classic/src/theme/MDXComponents/Head.tsx
+++ packages/docusaurus-theme-classic/src/theme/MDXComponents/Head.tsx
@@ -9,11 +9,13 @@ import React, {type ReactElement} from 'react';
 import Head, {type Props as HeadProps} from '@docusaurus/Head';
 import type {Props} from '@theme/MDXComponents/Head';
 
+type MDXElement = ReactElement<
+  {mdxType?: string; originalType?: string} | undefined
+>;
+
 // MDX elements are wrapped through the MDX pragma. In some cases (notably usage
 // with Head/Helmet) we need to unwrap those elements.
-function unwrapMDXElement(
-  element: ReactElement<{mdxType?: string; originalType?: string} | undefined>,
-) {
+function unwrapMDXElement(element: MDXElement) {
   if (element.props?.mdxType && element.props.originalType) {
     const {mdxType, originalType, ...newProps} = element.props;
     return React.createElement(element.props.originalType, newProps);
@@ -23,7 +25,7 @@ function unwrapMDXElement(
 
 export default function MDXHead(props: Props): JSX.Element {
   const unwrappedChildren = React.Children.map(props.children, (child) =>
-    React.isValidElement(child) ? unwrapMDXElement(child) : child,
+    React.isValidElement(child) ? unwrapMDXElement(child as MDXElement) : child,
   );
   return <Head {...(props as HeadProps)}>{unwrappedChildren}</Head>;
 }
