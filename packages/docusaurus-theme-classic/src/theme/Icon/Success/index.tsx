5d9dc390fc867b78cdb099cf8f079e2dcffc9275
Armano <armano2@users.noreply.github.com>
2023-04-02 13:23:06 +0200

    fix: correct icons

--- packages/docusaurus-theme-classic/src/theme/Icon/Success/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Icon/Success/index.tsx
@@ -13,7 +13,7 @@ export default function IconSuccess(props: Props): JSX.Element {
     <svg viewBox="0 0 24 24" {...props}>
       <path
         fill="currentColor"
-        d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
+        d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"
       />
     </svg>
   );
