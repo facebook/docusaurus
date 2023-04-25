fd27627679e2d42198bfe662057411117634047e
Joshua Chen <sidachen2003@gmail.com>
2023-04-25 03:19:55 -0400

    fix(utils): make Markdown link replacement much more rigorous

--- packages/docusaurus-utils/src/markdownLinks.ts
+++ packages/docusaurus-utils/src/markdownLinks.ts
@@ -102,12 +102,26 @@ export function replaceMarkdownLinks<T extends ContentPaths>({
     // Replace inline-style links or reference-style links e.g:
     // This is [Document 1](doc1.md)
     // [doc1]: doc1.md
-    const mdRegex =
-      /(?:\]\(|\]:\s*)(?!https?:\/\/|@site\/)<?(?<filename>[^'"\]\s>]+(?:\s[^'"\]\s>]+)*\.mdx?)>?/g;
-    let mdMatch = mdRegex.exec(modifiedLine);
+    const linkTitlePattern = '(?:\\s+(?:\'.*?\'|".*?"|\\(.*?\\)))?';
+    const linkSuffixPattern = '(?:\\?[^#>\\s]+)?(?:#[^>\\s]+)?';
+    const linkCapture = (forbidden: string) =>
+      `((?!https?://|@site/)[^${forbidden}#?]+)`;
+    const linkURLPattern = `(?:${linkCapture(
+      '()\\s',
+    )}${linkSuffixPattern}|<${linkCapture('>')}${linkSuffixPattern}>)`;
+    const linkPattern = new RegExp(
+      `\\[(?:(?!\\]\\().)*\\]\\(\\s*${linkURLPattern}${linkTitlePattern}\\s*\\)|^\\s*\\[[^[\\]]*[^[\\]\\s][^[\\]]*\\]:\\s*${linkURLPattern}${linkTitlePattern}$`,
+      'dgm',
+    );
+    let mdMatch = linkPattern.exec(modifiedLine);
     while (mdMatch !== null) {
       // Replace it to correct html link.
-      const mdLink = mdMatch.groups!.filename!;
+      const mdLink = mdMatch.slice(1, 5).find(Boolean)!;
+      const mdLinkRange = mdMatch.indices!.slice(1, 5).find(Boolean)!;
+      if (!/\.mdx?$/.test(mdLink)) {
+        mdMatch = linkPattern.exec(modifiedLine);
+        continue;
+      }
 
       const sourcesToTry: string[] = [];
       // ./file.md and ../file.md are always relative to the current file
@@ -135,13 +149,13 @@ export function replaceMarkdownLinks<T extends ContentPaths>({
           .split('/')
           .map((part) => part.replace(/\s/g, '%20'))
           .join('/');
-        modifiedLine = modifiedLine.replace(
-          mdMatch[0]!,
-          mdMatch[0]!.replace(mdLink, encodedPermalink),
-        );
+        modifiedLine = `${modifiedLine.slice(
+          0,
+          mdLinkRange[0],
+        )}${encodedPermalink}${modifiedLine.slice(mdLinkRange[1])}`;
         // Adjust the lastIndex to avoid passing over the next link if the
         // newly replaced URL is shorter.
-        mdRegex.lastIndex += encodedPermalink.length - mdLink.length;
+        linkPattern.lastIndex += encodedPermalink.length - mdLink.length;
       } else {
         const brokenMarkdownLink: BrokenMarkdownLink<T> = {
           contentPaths,
@@ -151,7 +165,7 @@ export function replaceMarkdownLinks<T extends ContentPaths>({
 
         brokenMarkdownLinks.push(brokenMarkdownLink);
       }
-      mdMatch = mdRegex.exec(modifiedLine);
+      mdMatch = linkPattern.exec(modifiedLine);
     }
     return modifiedLine;
   });
