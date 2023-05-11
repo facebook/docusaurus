b0e8e86ed8812573efb8e4171a1ef19156cbdb03
robert lawrence <robwebsitedesign@gmail.com>
2023-05-11 01:48:16 -0600

    fixes docSidbar display overlapping display issue

--- packages/docusaurus-theme-classic/src/theme/DocSidebar/Desktop/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/DocSidebar/Desktop/index.tsx
@@ -5,9 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, {useState, useLayoutEffect} from 'react';
 import clsx from 'clsx';
 import {useThemeConfig} from '@docusaurus/theme-common';
+import {
+  useAnnouncementBar,
+  useScrollPosition,
+} from '@docusaurus/theme-common/internal';
 import Logo from '@theme/Logo';
 import CollapseButton from '@theme/DocSidebar/Desktop/CollapseButton';
 import Content from '@theme/DocSidebar/Desktop/Content';
@@ -16,6 +20,9 @@ import type {Props} from '@theme/DocSidebar/Desktop';
 import styles from './styles.module.css';
 
 function DocSidebarDesktop({path, sidebar, onCollapse, isHidden}: Props) {
+  const {isActive} = useAnnouncementBar();
+  const [dynamicHeight, setDynamicHeight] = useState(0);
+
   const {
     navbar: {hideOnScroll},
     docs: {
@@ -23,13 +30,37 @@ function DocSidebarDesktop({path, sidebar, onCollapse, isHidden}: Props) {
     },
   } = useThemeConfig();
 
+  useLayoutEffect(() => {
+    if (isActive) {
+      setDynamicHeight(30);
+    } else {
+      setDynamicHeight(0);
+    }
+  }, [isActive]);
+
+  useScrollPosition(
+    ({scrollY}) => {
+      if (isActive) {
+        setDynamicHeight(30 - scrollY);
+      }
+    },
+
+    [isActive],
+  );
+
   return (
     <div
       className={clsx(
         styles.sidebar,
         hideOnScroll && styles.sidebarWithHideableNavbar,
         isHidden && styles.sidebarHidden,
-      )}>
+      )}
+      style={{
+        height:
+          isActive && dynamicHeight >= 0 && dynamicHeight <= 30
+            ? `calc(100% - ${dynamicHeight}px)`
+            : '100%',
+      }}>
       {hideOnScroll && <Logo tabIndex={-1} className={styles.sidebarLogo} />}
       <Content path={path} sidebar={sidebar} />
       {hideable && <CollapseButton onClick={onCollapse} />}
