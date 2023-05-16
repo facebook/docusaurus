4e5dd2bbe5aa9dbd7462cdb1bd860fd0678c6d9d
robert lawrence <robwebsitedesign@gmail.com>
2023-05-16 09:29:27 -0600

    fixes docSidbar display overlapping display issue

--- packages/docusaurus-theme-classic/src/theme/DocSidebar/Desktop/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/DocSidebar/Desktop/index.tsx
@@ -5,9 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, {useState, useLayoutEffect} from 'react';
 import clsx from 'clsx';
 import {useThemeConfig} from '@docusaurus/theme-common';
+import {
+  useAnnouncementBar,
+  useScrollPosition,
+} from '@docusaurus/theme-common/internal';
 import Logo from '@theme/Logo';
 import CollapseButton from '@theme/DocSidebar/Desktop/CollapseButton';
 import Content from '@theme/DocSidebar/Desktop/Content';
@@ -16,6 +20,12 @@ import type {Props} from '@theme/DocSidebar/Desktop';
 import styles from './styles.module.css';
 
 function DocSidebarDesktop({path, sidebar, onCollapse, isHidden}: Props) {
+  const {isActive} = useAnnouncementBar();
+  const [announcementBarHandler, setAnnouncementBarHandler] = useState({
+    barActive: false,
+    height: 0,
+  });
+
   const {
     navbar: {hideOnScroll},
     docs: {
@@ -23,13 +33,45 @@ function DocSidebarDesktop({path, sidebar, onCollapse, isHidden}: Props) {
     },
   } = useThemeConfig();
 
+  useLayoutEffect(() => {
+    if (isActive) {
+      setAnnouncementBarHandler({barActive: isActive, height: 30});
+    } else {
+      setAnnouncementBarHandler({barActive: isActive, height: 0});
+    }
+  }, [isActive]);
+
+  useScrollPosition(
+    ({scrollY}) => {
+      if (isActive) {
+        setAnnouncementBarHandler((prevState) => ({
+          ...prevState,
+          height: 30 - scrollY,
+        }));
+      }
+    },
+
+    [announcementBarHandler.height],
+  );
+
   return (
     <div
       className={clsx(
+        announcementBarHandler.barActive
+          ? styles.sidebarAnnouncementActive
+          : styles.sidebarAnnouncementInactive,
         styles.sidebar,
         hideOnScroll && styles.sidebarWithHideableNavbar,
         isHidden && styles.sidebarHidden,
-      )}>
+      )}
+      style={{
+        height:
+          announcementBarHandler.barActive &&
+          announcementBarHandler.height >= 0 &&
+          announcementBarHandler.height <= 30
+            ? `calc(100% - ${announcementBarHandler.height}px)`
+            : '100%',
+      }}>
       {hideOnScroll && <Logo tabIndex={-1} className={styles.sidebarLogo} />}
       <Content path={path} sidebar={sidebar} />
       {hideable && <CollapseButton onClick={onCollapse} />}
