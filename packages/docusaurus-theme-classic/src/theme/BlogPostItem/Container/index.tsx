05156d05b99d88844f5a568a33619b7c3f161be4
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-06-29 03:02:22 -0700

    feat(theme-classic): add description & keywords microdata to blog posts (#9108)

--- packages/docusaurus-theme-classic/src/theme/BlogPostItem/Container/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/BlogPostItem/Container/index.tsx
@@ -14,17 +14,26 @@ export default function BlogPostItemContainer({
   children,
   className,
 }: Props): JSX.Element {
-  const {frontMatter, assets} = useBlogPost();
+  const {
+    frontMatter,
+    assets,
+    metadata: {description},
+  } = useBlogPost();
   const {withBaseUrl} = useBaseUrlUtils();
   const image = assets.image ?? frontMatter.image;
+  const keywords = frontMatter.keywords ?? [];
   return (
     <article
       className={className}
       itemProp="blogPost"
       itemScope
       itemType="http://schema.org/BlogPosting">
+      {description && <meta itemProp="description" content={description} />}
       {image && (
-        <meta itemProp="image" content={withBaseUrl(image, {absolute: true})} />
+        <link itemProp="image" href={withBaseUrl(image, {absolute: true})} />
+      )}
+      {keywords.length > 0 && (
+        <meta itemProp="keywords" content={keywords.join(',')} />
       )}
       {children}
     </article>
