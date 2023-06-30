32869fd7a02d914fcbb97f49c9e69cf08f4a1b2e
sebastienlorber <lorber.sebastien@gmail.com>
2023-06-30 15:08:59 +0200

    proper fix

--- packages/docusaurus-theme-classic/src/theme/MDXComponents/Ul/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/MDXComponents/Ul/index.tsx
@@ -11,7 +11,11 @@ import type {Props} from '@theme/MDXComponents/Ul';
 
 import styles from './styles.module.css';
 
-function transformUlClassName(className?: string): string {
+function transformUlClassName(className?: string): string | undefined {
+  // Fix https://github.com/facebook/docusaurus/issues/9098
+  if (typeof className === 'undefined') {
+    return undefined;
+  }
   return clsx(
     className,
     // This class is set globally by GitHub/MDX. We keep the global class, and
@@ -22,5 +26,5 @@ function transformUlClassName(className?: string): string {
 }
 
 export default function MDXUl(props: Props): JSX.Element {
-  return <ul {...props} className={props.className ? transformUlClassName(props.className) : undefined} />;
+  return <ul {...props} className={transformUlClassName(props.className)} />;
 }
