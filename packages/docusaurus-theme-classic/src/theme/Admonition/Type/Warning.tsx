0fb02a6f5a71f1ba3298424ccde4f95eec5918ed
sebastienlorber <lorber.sebastien@gmail.com>
2023-09-14 18:04:24 +0200

    Make warning a first-class admonition, and deprecate caution

--- packages/docusaurus-theme-classic/src/theme/Admonition/Type/Warning.tsx
+++ packages/docusaurus-theme-classic/src/theme/Admonition/Type/Warning.tsx
@@ -8,7 +8,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import Translate from '@docusaurus/Translate';
-import type {Props} from '@theme/Admonition/Type/Caution';
+import type {Props} from '@theme/Admonition/Type/Warning';
 import AdmonitionLayout from '@theme/Admonition/Layout';
 import IconWarning from '@theme/Admonition/Icon/Warning';
 
@@ -18,16 +18,14 @@ const defaultProps = {
   icon: <IconWarning />,
   title: (
     <Translate
-      id="theme.admonition.caution"
-      description="The default label used for the Caution admonition (:::caution)">
-      caution
+      id="theme.admonition.warning"
+      description="The default label used for the Warning admonition (:::warning)">
+      warning
     </Translate>
   ),
 };
 
-// TODO remove before v4: Caution replaced by Warning
-// see https://github.com/facebook/docusaurus/issues/7558
-export default function AdmonitionTypeCaution(props: Props): JSX.Element {
+export default function AdmonitionTypeWarning(props: Props): JSX.Element {
   return (
     <AdmonitionLayout
       {...defaultProps}
