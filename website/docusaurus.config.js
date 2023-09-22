d8dff86c0f66e3d564ee12ff7a8242f98da546ae
sebastienlorber <lorber.sebastien@gmail.com>
2023-09-22 17:42:02 +0200

    Do not fail the build if a localized site has a broken link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -183,7 +183,11 @@ module.exports = async function createConfigAsync() {
         return result;
       },
     },
-    onBrokenLinks: 'throw',
+    onBrokenLinks:
+      // Do not fail the build if a localized site has a broken link
+      process.env.DOCUSAURUS_CURRENT_LOCALE === defaultLocale
+        ? 'throw'
+        : 'warn',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/docusaurus.ico',
     customFields: {
