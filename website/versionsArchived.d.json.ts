4f3e37b5c8b696e1a4e1c11ff0793cf6be83ae46
sebastienlorber <lorber.sebastien@gmail.com>
2023-10-14 02:06:37 +0200

    lint

--- website/versionsArchived.d.json.ts
+++ website/versionsArchived.d.json.ts
@@ -5,6 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-declare const versionsArchived: { [key: string]: string };
+declare const versionsArchived: {[key: string]: string};
 
 export default versionsArchived;
