2bb4fd0a6514cba424e5238e190a70626edf19d6
Greg Bergé <berge.greg@gmail.com>
2023-10-20 17:03:37 +0200

    chore(ci): use new Argos playwright integration (#9419)
    Co-authored-by: sebastienlorber <lorber.sebastien@gmail.com>

--- argos/playwright.config.ts
+++ argos/playwright.config.ts
@@ -14,6 +14,8 @@ import type {PlaywrightTestConfig} from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
 
+  reporter: [['list'], ['@argos-ci/playwright/reporter']],
+
   // Run website production built
   // Need to run "yarn website:build:fast" before
   webServer: {
