8bdb9ed3c2fa2655b9fa53b2db2911d5702e38b3
axmmisaka <6500159+axmmisaka@users.noreply.github.com>
2023-12-03 06:16:45 -0800

    Add title prop for imported inline SVG React components
    This would make TypeScript server stop complaining should one decide to use the "title" prop
    
    Relate issue: https://github.com/facebook/docusaurus/issues/9609

--- packages/docusaurus-module-type-aliases/src/index.d.ts
+++ packages/docusaurus-module-type-aliases/src/index.d.ts
@@ -356,7 +356,7 @@ declare module '@docusaurus/useGlobalData' {
 declare module '*.svg' {
   import type {ComponentType, SVGProps} from 'react';
 
-  const ReactComponent: ComponentType<SVGProps<SVGSVGElement>>;
+  const ReactComponent: ComponentType<SVGProps<SVGSVGElement> & {title?: string}>;
 
   export default ReactComponent;
 }
