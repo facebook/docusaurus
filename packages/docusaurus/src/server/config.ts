7ee136cf9af5aa63d0a3f62ce2c92435bb57d128
sebastienlorber <lorber.sebastien@gmail.com>
2023-12-07 22:57:34 +0100

    warn when no theme config found

--- packages/docusaurus/src/server/config.ts
+++ packages/docusaurus/src/server/config.ts
@@ -101,14 +101,12 @@ export async function findThemeConfigPath(
   );
   // In Docusaurus v3 we require users to provide either the theme config
   // through the conventional path, or through the legacy siteConfig attribute
-  // To avoid issues we prevent users to not provide any theme config at all
+  // To avoid issues we warn users when no theme config is provided at all
   if (
     !conventionalThemeConfigPath &&
     Object.keys(siteConfig.themeConfig ?? {}).length === 0
   ) {
-    throw new Error(
-      `Theme config file couldn't be found at ${DEFAULT_THEME_FILE_NAME}.js or ${DEFAULT_THEME_FILE_NAME}.tsx`,
-    );
+    logger.warn`Theme config file couldn't be found at ${DEFAULT_THEME_FILE_NAME}.js or ${DEFAULT_THEME_FILE_NAME}.tsx`;
   }
   return conventionalThemeConfigPath;
 }
