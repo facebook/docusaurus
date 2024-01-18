5d68e76c21597564923e462d0deea5769ee15758
sebastienlorber <lorber.sebastien@gmail.com>
2024-01-18 19:22:29 +0100

    use MdxjsEsm type guard

--- packages/docusaurus-mdx-loader/src/remark/toc/utils.ts
+++ packages/docusaurus-mdx-loader/src/remark/toc/utils.ts
@@ -5,14 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type {Literal} from 'mdast';
 import type {Node} from 'unist';
 import type {
   MdxjsEsm,
   // @ts-expect-error: TODO see https://github.com/microsoft/TypeScript/issues/49721
 } from 'mdast-util-mdx';
 
-export const isImport = (child: Node): child is Literal => {
+export const isImport = (child: Node): child is MdxjsEsm => {
   if (child.type === 'mdxjsEsm') {
     return (child as MdxjsEsm).value.startsWith('import');
   }
@@ -21,7 +20,7 @@ export const isImport = (child: Node): child is Literal => {
 
 export const hasImports = (index: number): boolean => index > -1;
 
-export const isExport = (child: Node): child is Literal => {
+export const isExport = (child: Node): child is MdxjsEsm => {
   if (child.type === 'mdxjsEsm') {
     return (child as MdxjsEsm).value.startsWith('export');
   }
