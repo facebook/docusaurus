128738786b5f7c99c07922cede1c640804cdbed9
Sébastien Lorber <slorber@users.noreply.github.com>
2024-04-18 15:08:30 +0200

    fix(core): sortRoutes shouldn't have a default baseUrl value, this led to a bug (#10054)

--- packages/docusaurus/src/server/plugins/routeConfig.ts
+++ packages/docusaurus/src/server/plugins/routeConfig.ts
@@ -27,10 +27,11 @@ export function applyRouteTrailingSlash<Route extends RouteConfig>(
   };
 }
 
-export function sortRoutes(
-  routeConfigs: RouteConfig[],
-  baseUrl: string = '/',
-): void {
+export function sortRoutes<Route extends RouteConfig>(
+  routesToSort: Route[],
+  baseUrl: string,
+): Route[] {
+  const routeConfigs = [...routesToSort];
   // Sort the route config. This ensures that route with nested
   // routes is always placed last.
   routeConfigs.sort((a, b) => {
@@ -48,6 +49,23 @@ export function sortRoutes(
     if (!a.routes && b.routes) {
       return -1;
     }
+
+    // If both are parent routes (for example routeBasePath: "/" and "/docs/"
+    // We must order them carefully in case of overlapping paths
+    if (a.routes && b.routes) {
+      if (a.path === b.path) {
+        // We don't really support that kind of routing ATM
+        // React-Router by default will only "enter" a single parent route
+      } else {
+        if (a.path.includes(b.path)) {
+          return -1;
+        }
+        if (b.path.includes(a.path)) {
+          return 1;
+        }
+      }
+    }
+
     // Higher priority get placed first.
     if (a.priority || b.priority) {
       const priorityA = a.priority ?? 0;
@@ -64,7 +82,9 @@ export function sortRoutes(
 
   routeConfigs.forEach((routeConfig) => {
     if (routeConfig.routes) {
-      sortRoutes(routeConfig.routes, baseUrl);
+      routeConfig.routes = sortRoutes(routeConfig.routes, baseUrl);
     }
   });
+
+  return routeConfigs;
 }
