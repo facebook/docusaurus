b82e8a5f734d1bcaabd0ee6aa5fd36fe81364607
sebastien <lorber.sebastien@gmail.com>
2024-05-17 11:19:12 +0200

    add missing route codegen after merge

--- packages/docusaurus/src/server/codegen/codegen.ts
+++ packages/docusaurus/src/server/codegen/codegen.ts
@@ -146,6 +146,22 @@ function genSiteStorage({
   );
 }
 
+function genRouter({
+  generatedFilesDir,
+  siteConfig,
+}: {
+  generatedFilesDir: string;
+  siteConfig: DocusaurusConfig;
+}) {
+  const routerImport =
+    siteConfig.router === 'hash' ? 'HashRouter' : 'BrowserRouter';
+  return generate(
+    generatedFilesDir,
+    'router.js',
+    `export {${routerImport} as default} from 'react-router-dom';`,
+  );
+}
+
 type CodegenParams = {
   generatedFilesDir: string;
   siteConfig: DocusaurusConfig;
@@ -170,5 +186,6 @@ export async function generateSiteFiles(params: CodegenParams): Promise<void> {
     genSiteStorage(params),
     genI18n(params),
     genCodeTranslations(params),
+    genRouter(params),
   ]);
 }
