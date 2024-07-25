fb4e32fb81a3bcc708b969493e4419545100e29f
ozaki <29860391+OzakIOne@users.noreply.github.com>
2024-07-25 12:34:34 +0200

    fix(theme): docsVersionDropdown navbar item not showing the appropriate version (#10309)
    Co-authored-by: sebastien <lorber.sebastien@gmail.com>

--- packages/docusaurus-plugin-content-docs/src/client/docsClientUtils.ts
+++ packages/docusaurus-plugin-content-docs/src/client/docsClientUtils.ts
@@ -63,14 +63,23 @@ export function getActiveVersion(
   data: GlobalPluginData,
   pathname: string,
 ): GlobalVersion | undefined {
-  const lastVersion = getLatestVersion(data);
-  // Last version is a route like /docs/*,
-  // we need to match it last or it would match /docs/version-1.0/* as well
-  const orderedVersionsMetadata = [
-    ...data.versions.filter((version) => version !== lastVersion),
-    lastVersion,
-  ];
-  return orderedVersionsMetadata.find(
+  // Sort paths so that a match-all version like /docs/* is matched last
+  // Otherwise /docs/* would match /docs/1.0.0/* routes
+  // This is simplified but similar to the core sortRoutes() logic
+  const sortedVersions = [...data.versions].sort((a, b) => {
+    if (a.path === b.path) {
+      return 0;
+    }
+    if (a.path.includes(b.path)) {
+      return -1;
+    }
+    if (b.path.includes(a.path)) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return sortedVersions.find(
     (version) =>
       !!matchPath(pathname, {
         path: version.path,
