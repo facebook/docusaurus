f688ec569f9c35bf9acd5ae492da8f09b71151e4
slorber <slorber@users.noreply.github.com>
2024-08-09 21:45:52 +0000

    deploy: bde44146145ab024ac4645bc2373ed75c663e56c

---
+++ assets/js/07b390a7.a1ec2d0a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[89656],{79700:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/changelog","page":1,"postsPerPage":20,"totalPages":5,"totalCount":99,"nextPage":"/changelog/page/2","blogDescription":"Keep yourself up-to-date about new features in every release","blogTitle":"Docusaurus changelog"}}')}}]);
