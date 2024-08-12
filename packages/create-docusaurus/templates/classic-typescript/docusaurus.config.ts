547979a59f1ab60da3829afe95f4e0b585d46ffd
Sébastien Lorber <slorber@users.noreply.github.com>
2024-08-12 15:59:03 +0200

    fix(create-docusaurus): Improve init templates blog setup + fix warnings (#10392)

--- packages/create-docusaurus/templates/classic-typescript/docusaurus.config.ts
+++ packages/create-docusaurus/templates/classic-typescript/docusaurus.config.ts
@@ -50,6 +50,10 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
         },
         theme: {
           customCss: './src/css/custom.css',
--- packages/create-docusaurus/templates/shared/blog/2021-08-26-welcome/index.md
+++ packages/create-docusaurus/templates/shared/blog/2021-08-26-welcome/index.md
@@ -7,6 +7,10 @@ tags: [facebook, hello, docusaurus]
 
 [Docusaurus blogging features](https://docusaurus.io/docs/blog) are powered by the [blog plugin](https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog).
 
+Here are a few tips you might find useful.
+
+<!-- truncate -->
+
 Simply add Markdown files (or folders) to the `blog` directory.
 
 Regular blog authors can be added to `authors.yml`.
