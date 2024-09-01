4710070969d72755169d4bb20b59e32652283163
ma5d <62488969+ma5d@users.noreply.github.com>
2024-09-01 23:08:41 +0800

    Create GiteeFileContent.js
    新建一个Gitee组件，用于直接展示Gitee仓库代码，无需存放两份。

---
+++ website/src/components/GiteeFileContent.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import CodeBlock from '@theme/CodeBlock';
+
+const GiteeFileContent = ({ owner, repo, filePath, branch = 'main' }) => {
+  const [fileContent, setFileContent] = useState('');
+
+  useEffect(() => {
+    const fetchFileContent = async () => {
+      try {
+        const url = `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${filePath}?ref=${branch}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const binaryString = atob(data.content);
+        const binaryArray = Uint8Array.from(binaryString, char => char.charCodeAt(0));
+        const decodedContent = new TextDecoder('utf-8').decode(binaryArray);
+
+        setFileContent(decodedContent);
+
+      } catch (error) {
+        console.error('Error fetching file content:', error);
+      }
+    };
+
+    fetchFileContent();
+  }, [owner, repo, filePath, branch]); // 监听这些参数的变化
+
+  return (
+    <CodeBlock language="java" title={filePath}>
+      {fileContent}
+    </CodeBlock>
+  );
+};
+
+export default GiteeFileContent;
