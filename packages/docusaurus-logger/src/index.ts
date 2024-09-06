558e486ea8fc554dd6affcd3f79a8de371760497
sebastien <lorber.sebastien@gmail.com>
2024-09-06 10:45:54 +0200

    fix ESM usage?

--- packages/docusaurus-logger/src/index.ts
+++ packages/docusaurus-logger/src/index.ts
@@ -5,13 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export {
-  default,
-  // Extra named export to avoid problems in ESM modules
-  // Notably: core .mjs CLI + create-docusaurus
-  // See https://github.com/facebook/docusaurus/pull/6661
-  // See https://github.com/facebook/docusaurus/pull/7295
-  default as logger,
-} from './logger';
+import OriginalLogger from './logger';
+
+export default OriginalLogger;
+
+// Extra named export to avoid problems in ESM modules
+// Notably: core .mjs CLI + create-docusaurus
+// See https://github.com/facebook/docusaurus/pull/6661
+// See https://github.com/facebook/docusaurus/pull/7295
+export const logger = OriginalLogger;
 
 export {PerfLogger} from './perfLogger';
