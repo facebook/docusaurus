35aa39bddb9c6c201f2d3d3f98dd15d88f15244d
Sébastien Lorber <slorber@users.noreply.github.com>
2024-10-23 20:01:07 +0200

    docs: mention config/sidebars run in Node.js runtime (#10608)

--- packages/create-docusaurus/templates/classic-typescript/docusaurus.config.ts
+++ packages/create-docusaurus/templates/classic-typescript/docusaurus.config.ts
@@ -2,6 +2,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
 const config: Config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
