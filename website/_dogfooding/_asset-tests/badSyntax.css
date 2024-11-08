faf01a4359030f5d252e36445b85b319ae9719c1
sebastien <lorber.sebastien@gmail.com>
2024-11-08 17:58:07 +0100

    fix copy plugin

---
+++ website/_dogfooding/_asset-tests/badSyntax.css
@@ -0,0 +1,7 @@
+
+
+See https://github.com/facebook/docusaurus/issues/10460
+
+Using bad JS syntax on purpose, this file shouldn't be processed and cause build errors, it should just be copied over.
+
+import export with }{>< default switch
