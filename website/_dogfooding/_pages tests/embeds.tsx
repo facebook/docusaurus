bb10a24b4a26bafb0ada102ba61f72c52b28882a
sebastien <lorber.sebastien@gmail.com>
2024-11-18 14:18:54 +0100

    provide ambiant TS declarations for .md / .mdx

--- website/_dogfooding/_pages tests/embeds.tsx
+++ website/_dogfooding/_pages tests/embeds.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import IframeWindow from '@site/src/components/BrowserWindow/IframeWindow';
+import PagePartial from './_pagePartial.mdx';
 
 // See https://github.com/facebook/docusaurus/issues/8672
 export default function Embeds(): JSX.Element {
@@ -16,14 +17,21 @@ export default function Embeds(): JSX.Element {
     <Layout>
       <div style={{padding: 10}}>
         <Heading as="h1">Test Embeds</Heading>
-        <div style={{display: 'flex', flexWrap: 'wrap'}}>
-          <IframeWindow url="/?docusaurus-theme=light" />
-          <IframeWindow url="/?docusaurus-theme=dark" />
-          <IframeWindow url="/?docusaurus-theme=unexpected-value" />
-          <IframeWindow url="/" />
-          <IframeWindow url="https://docusaurus.io/" />
-          <IframeWindow url="https://tutorial.docusaurus.io/" />
-        </div>
+        <section>
+          <Heading as="h2">MDX Embeds</Heading>
+          <PagePartial />
+        </section>
+        <section>
+          <Heading as="h2">Iframe Embeds</Heading>
+          <div style={{display: 'flex', flexWrap: 'wrap'}}>
+            <IframeWindow url="/?docusaurus-theme=light" />
+            <IframeWindow url="/?docusaurus-theme=dark" />
+            <IframeWindow url="/?docusaurus-theme=unexpected-value" />
+            <IframeWindow url="/" />
+            <IframeWindow url="https://docusaurus.io/" />
+            <IframeWindow url="https://tutorial.docusaurus.io/" />
+          </div>
+        </section>
       </div>
     </Layout>
   );
