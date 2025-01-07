489122ffff5946dc36bfd3c54fea6cc0f1f570dd
sebastien <lorber.sebastien@gmail.com>
2025-01-07 10:18:11 +0100

    SSG thread pool

--- packages/docusaurus/src/ssg/ssgExecutor.ts
+++ packages/docusaurus/src/ssg/ssgExecutor.ts
@@ -5,13 +5,113 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import * as path from 'path';
+import os from 'os';
+import _ from 'lodash';
 import {PerfLogger} from '@docusaurus/logger';
-import {createSSGParams} from './ssgParams';
 import {generateStaticFiles} from './ssg';
+import {createSSGParams} from './ssgParams';
 import {renderHashRouterTemplate} from './ssgTemplate';
 import {generateHashRouterEntrypoint} from './ssgUtils';
 import type {Props, RouterType} from '@docusaurus/types';
 import type {SiteCollectedData} from '../common';
+import type {GenerateStaticFilesResult} from './ssg';
+import type {SSGParams} from './ssgParams';
+
+function mergeResults(
+  results: GenerateStaticFilesResult[],
+): GenerateStaticFilesResult {
+  return {
+    collectedData: Object.assign({}, ...results.map((r) => r.collectedData)),
+  };
+}
+
+type SSGExecutor = {
+  run: () => Promise<GenerateStaticFilesResult>;
+  destroy: () => Promise<void>;
+};
+
+type CreateSSGExecutor = (params: {
+  params: SSGParams;
+  pathnames: string[];
+}) => Promise<SSGExecutor>;
+
+const createSimpleSSGExecutor: CreateSSGExecutor = async ({
+  params,
+  pathnames,
+}) => {
+  return {
+    run: () => {
+      return PerfLogger.async('Generate static files', () =>
+        generateStaticFiles({
+          pathnames,
+          params,
+        }),
+      );
+    },
+
+    destroy: async () => {
+      // nothing to do
+    },
+  };
+};
+
+const createPooledSSGExecutor: CreateSSGExecutor = async ({
+  params,
+  pathnames,
+}) => {
+  // TODO make this configurable
+  // Sensible default that gives the best improvement so far:
+  const numberOfThreads = os.cpus().length / 2;
+
+  const pathnamesChunks = _.chunk(
+    pathnames,
+    Math.ceil(pathnames.length / numberOfThreads),
+  );
+
+  const pool = await PerfLogger.async(
+    `Create SSG pool with ${numberOfThreads} threads`,
+    async () => {
+      const Tinypool = await import('tinypool').then((m) => m.default);
+      return new Tinypool({
+        filename: path.resolve(__dirname, './ssg.js'),
+        minThreads: numberOfThreads,
+        maxThreads: numberOfThreads,
+        concurrentTasksPerWorker: 1,
+        runtime: 'worker_threads',
+      });
+    },
+  );
+
+  return {
+    run: async () => {
+      const results = await PerfLogger.async(
+        'Generate static files - pooled',
+        async () => {
+          return Promise.all(
+            pathnamesChunks.map((chunk, chunkIndex) => {
+              return PerfLogger.async(
+                `Generate static files for chunk=${chunkIndex} with ${chunk.length} pathnames`,
+                () => {
+                  return pool.run({
+                    pathnames: chunk,
+                    params,
+                  }) as Promise<GenerateStaticFilesResult>;
+                },
+              );
+            }),
+          );
+        },
+      );
+
+      return mergeResults(results);
+    },
+
+    destroy: async () => {
+      await pool.destroy();
+    },
+  };
+};
 
 // TODO Docusaurus v4 - introduce SSG worker threads
 export async function executeSSG({
@@ -39,12 +139,12 @@ export async function executeSSG({
     return {collectedData: {}};
   }
 
-  const ssgResult = await PerfLogger.async('Generate static files', () =>
-    generateStaticFiles({
-      pathnames: props.routesPaths,
-      params,
-    }),
-  );
+  const createExecutor =
+    process.env.DOCUSAURUS_DISABLE_SSG_POOL === 'true'
+      ? createSimpleSSGExecutor
+      : createPooledSSGExecutor;
 
-  return ssgResult;
+  const executor = await createExecutor({params, pathnames: props.routesPaths});
+
+  return executor.run();
 }
