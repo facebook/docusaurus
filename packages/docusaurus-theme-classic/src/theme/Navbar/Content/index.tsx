ef30d9365b8c5c10df4bafedefef26193eea9872
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2025-01-15 23:25:47 -0300

    fix: include a `left` class to navbar

--- packages/docusaurus-theme-classic/src/theme/Navbar/Content/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Navbar/Content/index.tsx
@@ -55,7 +55,7 @@ function NavbarContentLayout({
 }) {
   return (
     <div className="navbar__inner">
-      <div className="navbar__items">{left}</div>
+      <div className="navbar__items navbar__items--left">{left}</div>
       <div className="navbar__items navbar__items--right">{right}</div>
     </div>
   );
