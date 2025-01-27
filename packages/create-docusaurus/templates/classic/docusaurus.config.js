70d8fc8a73fa50b1eb6d5ccdc043d201a970e41b
sebastien <lorber.sebastien@gmail.com>
2025-01-27 15:38:31 +0100

    init template: add future.v4 flag

--- packages/create-docusaurus/templates/classic/docusaurus.config.js
+++ packages/create-docusaurus/templates/classic/docusaurus.config.js
@@ -14,6 +14,11 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
