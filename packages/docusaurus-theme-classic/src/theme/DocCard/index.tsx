77a044e4c33107e07176f41b5fbfe6216077ae6e
sebastien <lorber.sebastien@gmail.com>
2025-02-06 12:11:48 +0100

    add better dogfooding for sidebar_class_name

--- packages/docusaurus-theme-classic/src/theme/DocCard/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/DocCard/index.tsx
@@ -43,34 +43,38 @@ function useCategoryItemsPlural() {
 }
 
 function CardContainer({
+  className,
   href,
   children,
 }: {
+  className?: string;
   href: string;
   children: ReactNode;
 }): ReactNode {
   return (
     <Link
       href={href}
-      className={clsx('card padding--lg', styles.cardContainer)}>
+      className={clsx('card padding--lg', styles.cardContainer, className)}>
       {children}
     </Link>
   );
 }
 
 function CardLayout({
+  className,
   href,
   icon,
   title,
   description,
 }: {
+  className?: string;
   href: string;
   icon: ReactNode;
   title: string;
   description?: string;
 }): ReactNode {
   return (
-    <CardContainer href={href}>
+    <CardContainer href={href} className={className}>
       <Heading
         as="h2"
         className={clsx('text--truncate', styles.cardTitle)}
@@ -112,6 +116,7 @@ function CardLink({item}: {item: PropSidebarItemLink}): ReactNode {
   const doc = useDocById(item.docId ?? undefined);
   return (
     <CardLayout
+      className={item.className}
       href={item.href}
       icon={icon}
       title={item.label}
