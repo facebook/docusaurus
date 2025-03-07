7698db25a1c982bdf3ded906b59e8d4662c99635
sebastien <lorber.sebastien@gmail.com>
2025-03-07 17:07:06 +0100

    fix eslint CodeBlockLine error

--- packages/docusaurus-theme-classic/src/theme/CodeBlock/Line/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/CodeBlock/Line/index.tsx
@@ -11,16 +11,29 @@ import type {Props} from '@theme/CodeBlock/Line';
 
 import styles from './styles.module.css';
 
+type Token = Props['line'][number];
+
+// Replaces '\n' by ''
+// Historical code, not sure why we even need this :/
+function fixLineBreak(line: Token[]) {
+  const singleLineBreakToken =
+    line.length === 1 && line[0]!.content === '\n' ? line[0] : undefined;
+
+  if (singleLineBreakToken) {
+    return [{...singleLineBreakToken, content: ''}];
+  }
+
+  return line;
+}
+
 export default function CodeBlockLine({
-  line,
+  line: lineProp,
   classNames,
   showLineNumbers,
   getLineProps,
   getTokenProps,
 }: Props): ReactNode {
-  if (line.length === 1 && line[0]!.content === '\n') {
-    line[0]!.content = '';
-  }
+  const line = fixLineBreak(lineProp);
 
   const lineProps = getLineProps({
     line,
