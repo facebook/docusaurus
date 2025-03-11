549ab795c32f0e16ea548dbc26e989ea1191f172
Jake Boone <jakeboone02@gmail.com>
2025-03-11 01:37:01 -0700

    feat(plugin-npm2yarn): Add Bun to default tabs conversions (#10953)
    * fix(plugin-npm2yarn): Add Bun as default to align with docs
    
    * fix(plugin-npm2yarn): update test snapshot with bunx
    
    * fix(plugin-npm2yarn): update test snapshot with bunx-create
    
    * fix(plugin-npm2yarn): update screenshot to add pnpm and Bun
    
    ---------
    
    Co-authored-by: Sébastien Lorber <slorber@users.noreply.github.com>

--- packages/docusaurus-remark-plugin-npm2yarn/src/index.ts
+++ packages/docusaurus-remark-plugin-npm2yarn/src/index.ts
@@ -172,7 +172,7 @@ function createImportNode() {
 }
 
 const plugin: Plugin<[PluginOptions?]> = (options = {}): Transformer => {
-  const {sync = false, converters = ['yarn', 'pnpm']} = options;
+  const {sync = false, converters = ['yarn', 'pnpm', 'bun']} = options;
   return async (root) => {
     const {visit} = await import('unist-util-visit');
 
