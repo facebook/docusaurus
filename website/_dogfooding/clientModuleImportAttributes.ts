7048ed07abd79bd16f641448780bc0ac21af2660
sebastien <lorber.sebastien@gmail.com>
2025-03-27 13:37:17 +0100

    add @ts-nocheck

--- website/_dogfooding/clientModuleImportAttributes.ts
+++ website/_dogfooding/clientModuleImportAttributes.ts
@@ -5,6 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck: TODO temp, for TS 5.1 CI jobs
+
 // Test for import attribute support
 // See https://github.com/facebook/docusaurus/issues/11016
 import '@generated/i18n' with { type: "json" }
