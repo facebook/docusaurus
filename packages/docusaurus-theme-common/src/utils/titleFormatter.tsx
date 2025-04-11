fffb5a7213253d3e305b1a4a922c65ee0f2a8330
sebastien <lorber.sebastien@gmail.com>
2025-04-11 15:51:36 +0200

    title formatter

---
+++ packages/docusaurus-theme-common/src/utils/titleFormatter.tsx
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+type TitleFormatterParams = {
+  title: string | undefined;
+  siteTitle: string;
+  titleDelimiter: string;
+};
+
+type TitleFormatterFn = (params: TitleFormatterParams) => string;
+
+export const DefaultTitleFormatter: TitleFormatterFn = ({
+  title,
+  siteTitle,
+  titleDelimiter,
+}: TitleFormatterParams): string => {
+  const trimmedTitle = title?.trim();
+  if (!trimmedTitle || trimmedTitle === siteTitle) {
+    return siteTitle;
+  }
+  return `${trimmedTitle} ${titleDelimiter} ${siteTitle}`;
+};
+
+type TitleFormatter = {format: (title?: string) => string};
+
+/**
+ * Returns a function to format the page title
+ */
+export function useTitleFormatter(): TitleFormatter {
+  const {siteConfig} = useDocusaurusContext();
+  const formatter = DefaultTitleFormatter;
+  const {title: siteTitle, titleDelimiter} = siteConfig;
+  return {
+    format: (title) => formatter({title, siteTitle, titleDelimiter}),
+  };
+}
