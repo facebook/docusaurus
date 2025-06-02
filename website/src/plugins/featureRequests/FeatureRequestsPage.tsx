5081557f4acd5669fc5d2d9660748015112d3782
sebastien <lorber.sebastien@gmail.com>
2025-06-02 12:11:58 +0200

    better fix

--- website/src/plugins/featureRequests/FeatureRequestsPage.tsx
+++ website/src/plugins/featureRequests/FeatureRequestsPage.tsx
@@ -5,10 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {type ReactNode, useEffect} from 'react';
+import React, {type ReactNode, useEffect, useState} from 'react';
 import clsx from 'clsx';
 import {useColorMode} from '@docusaurus/theme-common';
-import BrowserOnly from '@docusaurus/BrowserOnly';
 import Layout from '@theme/Layout';
 
 import cannyScript from './cannyScript';
@@ -16,10 +15,25 @@ import styles from './styles.module.css';
 
 const BOARD_TOKEN = '054e0e53-d951-b14c-7e74-9eb8f9ed2f91';
 
-// TODO find a way to remove this unreliable useColorMode() hook
-// See also https://github.com/facebook/docusaurus/pull/11224
+// TODO useColorMode() hook is not reliable on first call
+//  To ensure we always init Canny with the correct theme, we prefer to delay
+//  initialization until we know the theme is correct
+//  See also https://github.com/facebook/docusaurus/issues/7986
+//  See also https://github.com/facebook/docusaurus/pull/11224
+function useIsColorModeReliable() {
+  const [isColorModeReliable, setIsColorModeReliable] = useState(false);
+  useEffect(() => {
+    setIsColorModeReliable(true);
+  }, []);
+  return isColorModeReliable;
+}
+
 function useCannyTheme() {
   const {colorMode} = useColorMode();
+  const isColorModeReliable = useIsColorModeReliable();
+  if (!isColorModeReliable) {
+    return null;
+  }
   return colorMode === 'light' ? 'light' : 'dark';
 }
 
@@ -30,6 +44,9 @@ function CannyWidget({basePath}: {basePath: string}) {
 
   const theme = useCannyTheme();
   useEffect(() => {
+    if (!theme) {
+      return;
+    }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const {Canny} = window as any;
     Canny('render', {
@@ -55,7 +72,7 @@ export default function FeatureRequests({
 }): ReactNode {
   return (
     <Layout title="Feedback" description="Docusaurus 2 Feature Requests page">
-      <BrowserOnly>{() => <CannyWidget basePath={basePath} />}</BrowserOnly>
+      <CannyWidget basePath={basePath} />
     </Layout>
   );
 }
