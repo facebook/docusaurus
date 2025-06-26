b0df38d31788d68663e637e90698d174dd1d18b5
Sébastien Lorber <slorber@users.noreply.github.com>
2025-06-26 15:36:46 +0200

    feat(create-docusaurus): use respectPrefersColorScheme in init template (#11293)

--- packages/create-docusaurus/templates/classic/docusaurus.config.js
+++ packages/create-docusaurus/templates/classic/docusaurus.config.js
@@ -79,6 +79,9 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: 'My Site',
         logo: {
