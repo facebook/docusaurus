0382b6f02056747d2e24d316d8b8fa14d2731ad9
sebastien <lorber.sebastien@gmail.com>
2025-09-12 19:07:47 +0200

    add useful comment

--- packages/docusaurus-theme-search-algolia/src/client/utils.ts
+++ packages/docusaurus-theme-search-algolia/src/client/utils.ts
@@ -34,5 +34,7 @@ export function mergeFacetFilters(
   // Historical behavior: we flatten everything
   // TODO I'm pretty sure this is incorrect
   //  see https://www.algolia.com/doc/api-reference/api-parameters/facetFilters/?client=javascript
+  //  Note: Algolia is working to provide a reliable facet merging strategy
+  //  see https://github.com/facebook/docusaurus/pull/11327#issuecomment-3284742923
   return [...normalize(f1), ...normalize(f2)];
 }
