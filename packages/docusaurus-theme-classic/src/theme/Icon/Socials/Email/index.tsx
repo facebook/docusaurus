bb1ecf1107a6945924a27d3928b8c9be07198e16
sebastien <lorber.sebastien@gmail.com>
2025-09-23 14:02:09 +0200

    Add support for email social icon + resize default social icon a bit

--- packages/docusaurus-theme-classic/src/theme/Icon/Socials/Email/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/Icon/Socials/Email/index.tsx
@@ -8,26 +8,21 @@
 import type {ReactNode, SVGProps} from 'react';
 
 // SVG Source: https://github.com/tabler/tabler-icons
-function DefaultSocial(props: SVGProps<SVGSVGElement>): ReactNode {
+function Email(props: SVGProps<SVGSVGElement>): ReactNode {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      strokeWidth={2}
       {...props}>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M1.2 12a10.8 10.8 0 1 0 21.6 0a10.8 10.8 0 0 0 -21.6 0" />
-      <path d="M1.92 8.4h20.16" />
-      <path d="M1.92 15.6h20.16" />
-      <path d="M11.4 1.2a20.4 20.4 0 0 0 0 21.6" />
-      <path d="M12.6 1.2a20.4 20.4 0 0 1 0 21.6" />
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M7.2 12a4.8 4.8 0 1 0 9.6 0 4.8 4.8 0 1 0-9.6 0" />
+      <path d="M16.8 12v1.8a3 3 0 0 0 6 0V12a10.8 10.8 0 1 0-6.6 9.936" />
     </svg>
   );
 }
-export default DefaultSocial;
+export default Email;
