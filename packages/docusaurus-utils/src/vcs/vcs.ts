6bf515d5fc9c96c3f77141d709379e8165761d39
sebastien <lorber.sebastien@gmail.com>
2025-11-07 19:09:14 +0100

    minor refactor

--- packages/docusaurus-utils/src/vcs/vcs.ts
+++ packages/docusaurus-utils/src/vcs/vcs.ts
@@ -33,7 +33,7 @@ export function getVcsPreset(presetName: VcsPreset): VcsConfig {
   }
 }
 
-export function getDefaultVcsPreset(): VcsPreset {
+export function getDefaultVcsPresetName(): VcsPreset {
   // Escape hatch to override the default VCS preset we use
   if (process.env.DOCUSAURUS_VCS) {
     return process.env.DOCUSAURUS_VCS as VcsPreset;
@@ -58,27 +58,7 @@ export function getDefaultVcsPreset(): VcsPreset {
 }
 
 export function getDefaultVcsConfig(): VcsConfig {
-  // Escape hatch to override the default VCS preset we use
-  if (process.env.DOCUSAURUS_VCS) {
-    return getVcsPreset(process.env.DOCUSAURUS_VCS as VcsPreset);
-  }
-
-  if (process.env.NODE_ENV === 'production') {
-    // TODO add feature flag switch for git-eager / git-ad-hoc strategies
-    // return getVcsPreset('git-ad-hoc');
-    return getVcsPreset('git-eager');
-  }
-  // Return hardcoded values in dev to improve DX
-  if (process.env.NODE_ENV === 'development') {
-    return getVcsPreset('hardcoded');
-  }
-
-  // Return hardcoded values in test to make tests simpler and faster
-  if (process.env.NODE_ENV === 'test') {
-    return getVcsPreset('hardcoded');
-  }
-
-  return VcsGitAdHoc;
+  return getVcsPreset(getDefaultVcsPresetName());
 }
 
 export const DEFAULT_TEST_VCS_CONFIG: VcsConfig = VcsHardcoded;
