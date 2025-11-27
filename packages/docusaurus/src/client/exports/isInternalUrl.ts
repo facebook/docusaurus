adc0325e7178cf1b8c2c080f955471393a168611
sebastien <lorber.sebastien@gmail.com>
2025-11-27 17:16:42 +0100

    hasProtocol should implement the spec more strictly

--- packages/docusaurus/src/client/exports/isInternalUrl.ts
+++ packages/docusaurus/src/client/exports/isInternalUrl.ts
@@ -5,8 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// Poor man's protocol detection
+// Spec: https://datatracker.ietf.org/doc/html/rfc3986#section-3.1
+// In particular: scheme = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
 export function hasProtocol(url: string): boolean {
-  return /^(?:\w*:|\/\/)/.test(url);
+  return /^(?:[A-Za-z][A-Za-z\d+.-]*:|\/\/)/.test(url);
 }
 
 export default function isInternalUrl(url?: string): boolean {
