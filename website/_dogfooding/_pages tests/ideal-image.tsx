7657b25e74e8c6c63513e40a14b29a127593adb1
Zoey Greer <zoey@buildbuddy.io>
2026-01-08 16:18:53 -0500

    add test of IdealImage property propagation in pages

---
+++ website/_dogfooding/_pages tests/ideal-image.tsx
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Image from '@theme/IdealImage';
+
+import styles from './ideal-image.module.css';
+
+function Component() {
+  return (
+    <Image
+      className={styles.logo}
+      style={{width: '100px', height: '32px'}}
+      img={require('./img/oss_logo.png')}
+    />
+  );
+}
+
+export default Component;
