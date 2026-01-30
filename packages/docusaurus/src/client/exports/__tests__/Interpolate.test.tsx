bdca267207ef524edfb07e2c62b157ba998d088c
sebastien <lorber.sebastien@gmail.com>
2026-01-30 15:57:31 +0100

    better jest pragma integration

--- packages/docusaurus/src/client/exports/__tests__/Interpolate.test.tsx
+++ packages/docusaurus/src/client/exports/__tests__/Interpolate.test.tsx
@@ -1,16 +1,14 @@
 /**
- * @jest-environment jsdom
- */
-
-/**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @jest-environment jsdom
  */
-/* eslint-disable header/header */
-/* The Facebook header is here, but we need the Jest pragma first */
 
+// Jest doesn't allow pragma below other comments. https://github.com/facebook/jest/issues/12573
+// eslint-disable-next-line header/header
 import React from 'react';
 import {render} from '@testing-library/react';
 import '@testing-library/jest-dom';
