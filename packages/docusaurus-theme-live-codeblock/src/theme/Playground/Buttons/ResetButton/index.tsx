f4742c43d5e5c24dc87c9909571f9ea2af08198d
sebastien <lorber.sebastien@gmail.com>
2026-02-19 17:10:54 +0100

    subcomponents

--- packages/docusaurus-theme-live-codeblock/src/theme/Playground/Buttons/ResetButton/index.tsx
+++ packages/docusaurus-theme-live-codeblock/src/theme/Playground/Buttons/ResetButton/index.tsx
@@ -12,6 +12,29 @@ import {usePlayground} from '@docusaurus/theme-live-codeblock/client';
 import type {Props} from '@theme/Playground/Buttons/ResetButton';
 import styles from './styles.module.css';
 
+function Icon() {
+  return (
+    <svg
+      className={styles.resetButtonIcon}
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      aria-hidden="true">
+      <path d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
+      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
+    </svg>
+  );
+}
+
+function Label() {
+  return (
+    <Translate
+      id="theme.Playground.buttons.reset"
+      description="The reset button label for live code blocks">
+      Reset
+    </Translate>
+  );
+}
+
 export default function ResetButton({className}: Props): ReactNode {
   const {reset} = usePlayground();
   return (
@@ -21,19 +44,8 @@ export default function ResetButton({className}: Props): ReactNode {
       title="Reset"
       className={clsx('clean-btn', className, styles.resetButton)}
       onClick={() => reset()}>
-      <svg
-        className={styles.resetButtonIcon}
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        aria-hidden="true">
-        <path d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
-        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
-      </svg>
-      <Translate
-        id="theme.Playground.buttons.reset"
-        description="The reset button label for live code blocks">
-        Reset
-      </Translate>
+      <Icon />
+      <Label />
     </button>
   );
 }
