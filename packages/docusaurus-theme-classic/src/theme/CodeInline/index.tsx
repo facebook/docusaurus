b57fdfcefe7877308658e1fb2d3f4c6deef9ba9d
misrilal <misrilalsah09@gmail.com>
2026-03-31 03:50:10 +0530

    fix(theme-classic): align inline code to baseline

--- packages/docusaurus-theme-classic/src/theme/CodeInline/index.tsx
+++ packages/docusaurus-theme-classic/src/theme/CodeInline/index.tsx
@@ -6,11 +6,13 @@
  */
 
 import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
 import type {Props} from '@theme/CodeInline';
+import styles from './styles.module.css';
 
 // Simple component used to render inline code blocks
 // its purpose is to be swizzled and customized
 // MDX 1 used to have a inlineCode comp, see https://mdxjs.com/migrating/v2/
 export default function CodeInline(props: Props): ReactNode {
-  return <code {...props} />;
+  return <code {...props} className={clsx(styles.codeInline, props.className)} />;
 }
