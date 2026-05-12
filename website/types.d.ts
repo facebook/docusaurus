1bec17bd46f06037175876afc5a0c19f018a562c
sebastien <lorber.sebastien@gmail.com>
2026-05-12 11:53:29 +0200

    restore MDX JSX types

--- website/types.d.ts
+++ website/types.d.ts
@@ -5,10 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import 'vitest';
+// This is a bad workaround to make our site typecheck under React 19
+// We run our site with "skipLibCheck=false", unfortunately some libraries
+// are still using the global JSX namespace, that has been removed in v19
+// See https://react.dev/blog/2024/04/25/react-19-upgrade-guide#the-jsx-namespace-in-typescript
+// See https://github.com/mdx-js/mdx/issues/2487
+import type {JSX as Jsx} from 'react/jsx-runtime';
 
-declare module 'vitest' {
-  interface Matchers<R> {
-    toHaveGoodDimensions: () => R;
+declare global {
+  namespace JSX {
+    type ElementClass = Jsx.ElementClass;
+    type Element = Jsx.Element;
+    type ElementType = Jsx.ElementType;
+    type IntrinsicElements = Jsx.IntrinsicElements;
   }
 }
