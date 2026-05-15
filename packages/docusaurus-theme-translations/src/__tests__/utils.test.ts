4d2532e4ecc31798ffab309405bcc8e80dd5e925
sebastien <lorber.sebastien@gmail.com>
2026-05-15 17:01:35 +0200

    snapshot

--- packages/docusaurus-theme-translations/src/__tests__/utils.test.ts
+++ packages/docusaurus-theme-translations/src/__tests__/utils.test.ts
@@ -14,12 +14,12 @@ describe('extractThemeCodeMessages', () => {
     await expect(() =>
       extractThemeCodeMessages([path.join(__dirname, '__fixtures__/theme')]),
     ).rejects.toThrowErrorMatchingInlineSnapshot(`
-      [Error: 
+      [Error:
       Please make sure all theme translations are static!
       Some warnings were found!
 
       Translate content could not be extracted. It has to be a static string and use optional but static props, like <Translate id="my-id" description="my-description">text</Translate>.
-      File: packages/docusaurus-theme-translations/src/__tests__/__fixtures__/theme/index.js at line 4
+      File: packages/docusaurus-theme-translations/src/__tests__/__fixtures__/theme/index.js at line 5
       Full code: <Translate>{index}</Translate>
       ]
     `);
