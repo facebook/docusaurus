2df1cf864676df655f4cd9e1ce99579684808b66
seyoon20087 <24852454+seyoon20087@users.noreply.github.com>
2026-07-23 17:33:21 +0900

    Swap react-loadable to my one of react-loadable
    Currently, the version of react-loadable used is `@docusaurus/react-loadable`.
    
    While I greatly appreciate their version of react-loadable,
    I found that it uses UNSAFE_componentWillMount under the hood,
    which is not safe in strict-mode.
    
    Additionally, as we don't know when React
    will remove such lifecycle methods
    (they *could* eventually do so in the future),
    I swapped the version of react-loadable to my one in advance.
    
    While `@react-loadable/revised` or `loadable-components`
    are other alternatives too, the API and types
    aren't fully backwards-compatible, which is why
    my attempts to fix it were failed previously.
    
    I have tested this change internally, and found that all functionality
    works (including server-side-rendering).
    
    I hope this attempt will go well.
    
    (All APIs and types are identical from the upstream react-loadable,
    which means not many code changes are required)
    
    - NOTE: because of the "minimumReleaseAge" setting,
      my `@seyoon20087/react-loadable` package's latest version was temporarily
      added in the "minimumReleaseAgeExclude" setting of pnpm-workspace.yaml.
    
    Signed-off-by: seyoon20087 <24852454+seyoon20087@users.noreply.github.com>

--- packages/docusaurus/src/client/serverEntry.tsx
+++ packages/docusaurus/src/client/serverEntry.tsx
@@ -28,7 +28,6 @@ const render: AppRenderer['render'] = async ({pathname}) => {
   const statefulBrokenLinks = createStatefulBrokenLinks();
 
   const app = (
-    // @ts-expect-error: we are migrating away from react-loadable anyways
     <Loadable.Capture report={(moduleName) => modules.add(moduleName)}>
       <HelmetProvider context={helmetContext}>
         <StaticRouter location={pathname} context={routerContext}>
