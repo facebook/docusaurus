babf8c29da7e253b3b9307bdf6a76431b634e664
Sébastien Lorber <slorber@users.noreply.github.com>
2026-08-13 15:17:07 +0100

    chore: add xslt + sh copyrights (#12361)

--- website/profileSamply.sh
+++ website/profileSamply.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 echo "This Samply profiling script doesn't work when it's run with Yarn, but you can run it manually instead"
 
