61abc65559f5f2cdcb7148971e03774b0aa3b90c
sebastien <lorber.sebastien@gmail.com>
2026-09-11 18:07:08 +0200

    improve git restore command

--- admin/scripts/test-release.sh
+++ admin/scripts/test-release.sh
@@ -57,7 +57,7 @@ npx --no-install lerna publish \
   --registry "$CUSTOM_REGISTRY_URL" "$NEW_VERSION"
 
 # Revert version changes
-# git diff --name-only -- '*.json' | sed 's, ,\\&,g' | xargs git checkout --
+git restore -- '**/package.json' pnpm-lock.yaml
 
 
 # The website is generated outside the repo to minimize chances of pnpm resolving the wrong version
